a9e5ad618719083dfb47d758df51c77cd4843b3b
ExceptionalError <43562640+ExceptionalError@users.noreply.github.com>
2019-10-12 16:04:00 +0200

    revert changes #117

--- pkg/ffmpeg/encoder_screenshot.go
+++ pkg/ffmpeg/encoder_screenshot.go
@@ -18,14 +18,14 @@ func (e *Encoder) Screenshot(probeResult VideoFile, options ScreenshotOptions) {
 		options.Quality = 1
 	}
 	args := []string{
-		"-v " + options.Verbosity,
-		fmt.Sprintf("-ss %v", options.Time),
+		"-v", options.Verbosity,
+		"-ss", fmt.Sprintf("%v", options.Time),
 		"-y",
-		"-i \"" + probeResult.Path + "\"",
-		"-vframes 1",
-		fmt.Sprintf("-q:v %v", options.Quality),
-		fmt.Sprintf("-vf scale=%v:-1", options.Width),
-		"-f image2",
+		"-i", probeResult.Path, // TODO: Wrap in quotes?
+		"-vframes", "1",
+		"-q:v", fmt.Sprintf("%v", options.Quality),
+		"-vf", fmt.Sprintf("scale=%v:-1", options.Width),
+		"-f", "image2",
 		options.OutputPath,
 	}
 	_, _ = e.run(probeResult, args)
