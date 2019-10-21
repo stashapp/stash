c144f54fa36ede1038f1196d53c60c52e0143005
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2019-10-22 10:12:21 +1100

    Add filtering of log entries by level

--- ui/v2/src/components/Settings/SettingsLogsPanel.tsx
+++ ui/v2/src/components/Settings/SettingsLogsPanel.tsx
@@ -1,5 +1,5 @@
 import {
-  H4,
+  H4, FormGroup, HTMLSelect,
 } from "@blueprintjs/core";
 import React, { FunctionComponent, useState, useEffect } from "react";
 import * as GQL from "../../core/generated-graphql";
@@ -30,23 +30,28 @@ class LogEntry {
   public time: string;
   public level: string;
   public message: string;
+  public id: string;
+
+  private static nextId: number = 0;
 
   public constructor(logEntry: GQL.LogEntryDataFragment) {
     this.time = convertTime(logEntry);
-    
-    // pad to maximum length of level enum
-    this.level = logEntry.level.padEnd(GQL.LogLevel.Progress.length);
-    
+    this.level = logEntry.level;
     this.message = logEntry.message;
+
+    var id = LogEntry.nextId++;
+    this.id = id.toString();
   }
 }
 
 export const SettingsLogsPanel: FunctionComponent<IProps> = (props: IProps) => {
-  const { data } = StashService.useLoggingSubscribe();
+  const { data, error } = StashService.useLoggingSubscribe();
   const { data: existingData } = StashService.useLogs();
   
   const [existingLogEntries, setExistingLogEntries] = useState<LogEntry[]>([]);
   const [logEntries, setLogEntries] = useState<LogEntry[]>([]);
+  const [logLevel, setLogLevel] = useState<string>("Info");
+  const [filteredLogEntries, setFilteredLogEntries] = useState<LogEntry[]>([]);
 
   useEffect(() => {
     if (!data) { return; }
@@ -66,7 +71,23 @@ export const SettingsLogsPanel: FunctionComponent<IProps> = (props: IProps) => {
 
     var convertedData = existingData.logs.map(convertLogEntry);
     setExistingLogEntries(convertedData);
-  }, [existingData])
+  }, [existingData]);
+
+  useEffect(() => {
+    // concatenate and filter the log entries
+    
+    var filteredEntries : LogEntry[] = [];
+    
+    if (logEntries) {
+      filteredEntries = filteredEntries.concat(logEntries.filter(filterByLogLevel));
+    }
+
+    if (existingLogEntries) {
+      filteredEntries = filteredEntries.concat(existingLogEntries.filter(filterByLogLevel));
+    }
+    
+    setFilteredLogEntries(filteredEntries);
+  }, [logLevel, logEntries, existingLogEntries]);
 
   function convertLogEntry(logEntry : GQL.LogEntryDataFragment) {
     return new LogEntry(logEntry);
@@ -76,23 +97,64 @@ export const SettingsLogsPanel: FunctionComponent<IProps> = (props: IProps) => {
     return level.toLowerCase().trim();
   }
 
-  function renderLogEntry(logEntry : LogEntry) {
+  interface ILogElementProps {
+    logEntry : LogEntry
+  }
+
+  function LogElement(props : ILogElementProps) {
+    // pad to maximum length of level enum
+    var level = props.logEntry.level.padEnd(GQL.LogLevel.Progress.length);
+
     return (
       <>
-        <span>{logEntry.time}</span>&nbsp;
-        <span className={levelClass(logEntry.level)}>{logEntry.level}</span>&nbsp;
-        <span>{logEntry.message}</span>
+        <span>{props.logEntry.time}</span>&nbsp;
+        <span className={levelClass(props.logEntry.level)}>{level}</span>&nbsp;
+        <span>{props.logEntry.message}</span>
         <br/>
       </>
     );
   }
 
+  function maybeRenderError() {
+    if (error) {
+      return (
+        <>
+        <span className={"error"}>Error connecting to log server: {error.message}</span><br/>
+        </>
+      );
+    }
+  }
+
+  const logLevels = ["Debug", "Info", "Warning", "Error"];
+
+  function filterByLogLevel(logEntry : LogEntry) {
+    if (logLevel == "Debug") {
+      return true;
+    }
+
+    var logLevelIndex = logLevels.indexOf(logLevel);
+    var levelIndex = logLevels.indexOf(logEntry.level);
+
+    return levelIndex >= logLevelIndex;
+  }
+
   return (
     <>
       <H4>Logs</H4>
+      <div>
+      <FormGroup inline={true} label="Log Level">
+        <HTMLSelect
+          options={logLevels}
+          onChange={(event) => setLogLevel(event.target.value)}
+          value={logLevel}
+        />
+        </FormGroup>
+      </div>
       <div className="logs">
-        {logEntries.map(renderLogEntry)}
-        {existingLogEntries.map(renderLogEntry)}
+        {maybeRenderError()}
+        {filteredLogEntries.map((logEntry) =>
+          <LogElement logEntry={logEntry} key={logEntry.id}/>
+        )}
       </div>
     </>
   );
