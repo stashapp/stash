a7c22e57f41723eef6cce06f1ff17b83bb5e7d8a
bill <48220860+bnkai@users.noreply.github.com>
2019-10-30 01:53:14 +0200

    Check if scenes are in the library when cleaning

--- pkg/manager/task_clean.go
+++ pkg/manager/task_clean.go
@@ -4,8 +4,11 @@ import (
 	"context"
 	"github.com/stashapp/stash/pkg/database"
 	"github.com/stashapp/stash/pkg/logger"
+	"github.com/stashapp/stash/pkg/manager/config"
 	"github.com/stashapp/stash/pkg/models"
 	"os"
+	"path/filepath"
+	"strings"
 	"sync"
 )
 
@@ -16,7 +19,7 @@ type CleanTask struct {
 func (t *CleanTask) Start(wg *sync.WaitGroup) {
 	defer wg.Done()
 
-	if t.fileExists(t.Scene.Path) {
+	if t.fileExists(t.Scene.Path) && t.pathInStash() {
 		logger.Debugf("File Found: %s", t.Scene.Path)
 	} else {
 		logger.Infof("File not found. Cleaning: %s", t.Scene.Path)
@@ -42,7 +45,7 @@ func (t *CleanTask) deleteScene(sceneID int) {
 		logger.Infof("Error deleting scene from database: %s", err.Error())
 		return
 	}
-	
+
 	DeleteGeneratedSceneFiles(scene)
 }
 
@@ -52,4 +55,21 @@ func (t *CleanTask) fileExists(filename string) bool {
 		return false
 	}
 	return !info.IsDir()
+}
+
+func (t *CleanTask) pathInStash() bool {
+	for _, path := range config.GetStashPaths() {
+
+		rel, error := filepath.Rel(path, filepath.Dir(t.Scene.Path))
+
+		if error == nil {
+			if !strings.HasPrefix(rel, ".."+string(filepath.Separator)) {
+				logger.Debugf("File %s belongs to stash path %s", t.Scene.Path, path)
+				return true
+			}
+		}
+
+	}
+	logger.Debugf("File %s is out from stash path", t.Scene.Path)
+	return false
 }
