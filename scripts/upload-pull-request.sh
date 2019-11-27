65b27556b9e6fe33f179d3904ef478c6147a55a8
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2019-11-28 10:15:28 +1100

    Fix clobbered script

--- scripts/upload-pull-request.sh
+++ scripts/upload-pull-request.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
 # assumes cross-compile.sh has already been run successfully
-function uploadFile(file) {
+uploadFile() 
+{
+    FILE=$1
     BASENAME="$(basename "${FILE}")"
-    curl --upload-file $FILE "https://transfer.sh/$BASENAME"
+    uploadedTo=`curl --upload-file $FILE "https://transfer.sh/$BASENAME"`
+    echo "$BASENAME uploaded to url: $uploadedTo"
 }
 
-uploadFile("dist/stash-osx")
-uploadFile("dist/stash-win.exe")
-uploadFile("dist/stash-linux")
+uploadFile "dist/stash-osx"
+uploadFile "dist/stash-win.exe"
+uploadFile "dist/stash-linux"
