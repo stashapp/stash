e5f2245cef2c8c070fe500644c3d947d5aeade55
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2019-12-13 05:57:13 +1100

    Add responsive menu (#257)

---
+++ ui/v2/src/components/Sidebar.tsx
@@ -0,0 +1,32 @@
+import {
+  MenuItem,
+  Menu,
+  IconName,
+} from "@blueprintjs/core";
+import React, { FunctionComponent } from "react";
+import { IMenuItem } from "../App";
+
+interface IProps {
+  className: string
+  menuItems: IMenuItem[]
+}
+
+export const Sidebar: FunctionComponent<IProps> = (props) => {
+  return (
+    <>
+      <div className={"sidebar" + props.className}>
+        <Menu large={true}>
+          {props.menuItems.map((i) => {
+            return (
+              <MenuItem
+                icon={i.icon}
+                text={i.text}
+                href={i.href}
+              />
+            )
+          })}
+        </Menu>
+      </div>
+    </>
+  );
+};
