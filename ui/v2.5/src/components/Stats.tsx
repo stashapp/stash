f5400256523f690484cdb2260a85101387124211
Infinite <infinitekittens@protonmail.com>
2020-02-29 22:24:44 +0100

    Update stats page #366

--- ui/v2.5/src/components/Stats.tsx
+++ ui/v2.5/src/components/Stats.tsx
@@ -12,69 +12,47 @@ export const Stats: React.FC = () => {
 
   return (
     <div className="mt-5">
-      <div className="col col-sm-8 m-sm-auto">
-        <nav className="col col-sm-8 m-sm-auto row">
-          <div className="flex-grow-1">
-            <div>
-              <p className="heading">
-                <FormattedMessage id="scenes" defaultMessage="Scenes" />
-              </p>
-              <p className="title">
-                <FormattedNumber value={data.stats.scene_count} />
-              </p>
-            </div>
-          </div>
-          <div className="flex-grow-1">
-            <div>
-              <p className="heading">
-                <FormattedMessage id="galleries" defaultMessage="Galleries" />
-              </p>
-              <p className="title">
-                <FormattedNumber value={data.stats.gallery_count} />
-              </p>
-            </div>
-          </div>
-          <div className="flex-grow-1">
-            <div>
-              <p className="heading">
-                <FormattedMessage id="performers" defaultMessage="Performers" />
-              </p>
-              <p className="title">
-                <FormattedNumber value={data.stats.performer_count} />
-              </p>
-            </div>
-          </div>
-          <div className="flex-grow-1">
-            <div>
-              <p className="heading">
-                <FormattedMessage id="studios" defaultMessage="Studios" />
-              </p>
-              <p className="title">
-                <FormattedNumber value={data.stats.studio_count} />
-              </p>
-            </div>
-          </div>
-          <div className="flex-grow-1">
-            <div>
-              <p className="heading">
-                <FormattedMessage id="tags" defaultMessage="Tags" />
-              </p>
-              <p className="title">
-                <FormattedNumber value={data.stats.tag_count} />
-              </p>
-            </div>
-          </div>
-        </nav>
-
-        <h5>
-          <FormattedMessage id="stats.notes" defaultMessage="Notes" />
-        </h5>
-        <em>
-          <FormattedMessage
-            id="stats.warning"
-            defaultMessage="This is still an early version, some things are still a work in progress."
-          />
-        </em>
+      <div className="col col-sm-8 m-sm-auto row stats">
+        <div className="flex-grow-1">
+          <p className="title">
+            <FormattedNumber value={data.stats.scene_count} />
+          </p>
+          <p className="heading">
+            <FormattedMessage id="scenes" defaultMessage="Scenes" />
+          </p>
+        </div>
+        <div className="flex-grow-1">
+          <p className="title">
+            <FormattedNumber value={data.stats.gallery_count} />
+          </p>
+          <p className="heading">
+            <FormattedMessage id="galleries" defaultMessage="Galleries" />
+          </p>
+        </div>
+        <div className="flex-grow-1">
+          <p className="title">
+            <FormattedNumber value={data.stats.performer_count} />
+          </p>
+          <p className="heading">
+            <FormattedMessage id="performers" defaultMessage="Performers" />
+          </p>
+        </div>
+        <div className="flex-grow-1">
+          <p className="title">
+            <FormattedNumber value={data.stats.studio_count} />
+          </p>
+          <p className="heading">
+            <FormattedMessage id="studios" defaultMessage="Studios" />
+          </p>
+        </div>
+        <div className="flex-grow-1">
+          <p className="title">
+            <FormattedNumber value={data.stats.tag_count} />
+          </p>
+          <p className="heading">
+            <FormattedMessage id="tags" defaultMessage="Tags" />
+          </p>
+        </div>
       </div>
     </div>
   );
