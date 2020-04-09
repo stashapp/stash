77be8879e1ec64e878b8970dbc305f1e93067ad8
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2020-04-09 10:32:17 +1000

    Fix clearing sometimes not firing onChange

--- ui/v2.5/src/utils/editabletext.tsx
+++ ui/v2.5/src/utils/editabletext.tsx
@@ -69,7 +69,7 @@ const renderInputGroup = (options: {
         className="text-input"
         readOnly={!options.isEditing}
         plaintext={!options.isEditing}
-        defaultValue={options.value}
+        value={options.value}
         placeholder={options.placeholder ?? options.title}
         onChange={(event: React.FormEvent<HTMLInputElement>) =>
           options.onChange(event.currentTarget.value)
