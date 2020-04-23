1b032107c225786988784ba32c9798e770627069
bnkai <48220860+bnkai@users.noreply.github.com>
2020-04-23 12:58:54 +0300

    Fix yarn extract error dure to library size

--- ui/v2.5/src/components/Stats.tsx
+++ ui/v2.5/src/components/Stats.tsx
@@ -15,10 +15,10 @@ export const Stats: React.FC = () => {
       <div className="col col-sm-8 m-sm-auto row stats">
         <div className="stats-element">
           <p className="title">
-            <FormattedMessage id="size" defaultMessage={data.stats.scene_size_count} />
+            {data.stats.scene_size_count}
           </p>
           <p className="heading">
-            <FormattedMessage id="library" defaultMessage="Library size" />
+            <FormattedMessage id="library-size" defaultMessage="Library size" />
           </p>
         </div>
         <div className="stats-element">
