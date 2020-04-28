6e83457d93ad3bbe39c2133f38f35c0a6130e1a1
bnkai <48220860+bnkai@users.noreply.github.com>
2020-04-28 21:59:00 +0300

    * add depth to the thumbs cache directory

--- pkg/manager/paths/paths_gallery.go
+++ pkg/manager/paths/paths_gallery.go
@@ -9,6 +9,8 @@ import (
 type galleryPaths struct{}
 
 const thumbDir = "gthumbs"
+const thumbDirDepth int = 1
+const thumbDirLength int = 2 // thumbDirDepth * thumbDirLength must be smaller than the length of checksum
 
 func newGalleryPaths() *galleryPaths {
 	return &galleryPaths{}
@@ -22,13 +24,25 @@ func GetGthumbCache() string {
 	return filepath.Join(config.GetCachePath(), thumbDir)
 }
 
+//return a string that can be added to filepath.Join to implement directory depth
+func getIntraDir(checksum string) string {
+	if thumbDirDepth == 0 {
+		return "" // filepath.Join ignores empty elements
+	}
+	intraDir := checksum[0:thumbDirLength]
+	for i := 1; i < thumbDirDepth; i++ {
+		intraDir = filepath.Join(intraDir, checksum[thumbDirLength*i:thumbDirLength*(i+1)])
+	}
+	return intraDir
+}
+
 func GetGthumbDir(checksum string) string {
-	return filepath.Join(config.GetCachePath(), thumbDir, checksum)
+	return filepath.Join(config.GetCachePath(), thumbDir, getIntraDir(checksum), checksum)
 }
 
 func GetGthumbPath(checksum string, index int, width int) string {
 	fname := fmt.Sprintf("%s_%d_%d.jpg", checksum, index, width)
-	return filepath.Join(config.GetCachePath(), thumbDir, checksum, fname)
+	return filepath.Join(config.GetCachePath(), thumbDir, getIntraDir(checksum), checksum, fname)
 }
 
 func (gp *galleryPaths) GetExtractedFilePath(checksum string, fileName string) string {
