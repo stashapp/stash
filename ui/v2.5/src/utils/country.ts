3e7cd9af681dda0536cec4a6f491dd7807989ddd
Infinite <infinitekittens@protonmail.com>
2020-04-29 16:57:09 +0200

    Add flags for performer countries

---
+++ ui/v2.5/src/utils/country.ts
@@ -0,0 +1,24 @@
+import Countries from "i18n-iso-countries";
+import english from "i18n-iso-countries/langs/en.json";
+
+Countries.registerLocale(english);
+
+const fuzzyDict: Record<string, string> = {
+  USA: "US",
+  "United States": "US",
+  America: "US",
+  American: "US",
+  England: "GB",
+  "United Kingdom": "GB",
+  Russia: "RU",
+};
+
+const getISOCode = (country: string | null | undefined) => {
+  if (!country) return null;
+
+  if (fuzzyDict[country]) return fuzzyDict[country];
+
+  return Countries.getAlpha2Code(country, "en");
+};
+
+export default getISOCode;
