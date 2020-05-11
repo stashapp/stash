50921cdbee688093b4b599f07025a1513c986938
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2020-05-11 14:52:23 +1000

    Add test for FindNameBySceneID

--- pkg/models/querybuilder_performer_test.go
+++ pkg/models/querybuilder_performer_test.go
@@ -34,10 +34,33 @@ func TestPerformerFindBySceneID(t *testing.T) {
 	assert.Equal(t, 0, len(performers))
 }
 
+func TestPerformerFindNameBySceneID(t *testing.T) {
+	pqb := models.NewPerformerQueryBuilder()
+	sceneID := sceneIDs[sceneIdxWithPerformer]
+
+	performers, err := pqb.FindNameBySceneID(sceneID, nil)
+
+	if err != nil {
+		t.Fatalf("Error finding performer: %s", err.Error())
+	}
+
+	assert.Equal(t, 1, len(performers))
+	performer := performers[0]
+
+	assert.Equal(t, getPerformerStringValue(performerIdxWithScene, "Name"), performer.Name.String)
+
+	performers, err = pqb.FindBySceneID(0, nil)
+
+	if err != nil {
+		t.Fatalf("Error finding performer: %s", err.Error())
+	}
+
+	assert.Equal(t, 0, len(performers))
+}
+
 // TODO Update
 // TODO Destroy
 // TODO Find
-// TODO FindNameBySceneID
 // TODO FindByNames
 // TODO Count
 // TODO All
