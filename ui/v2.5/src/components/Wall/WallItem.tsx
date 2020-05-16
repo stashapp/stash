b6ccbd5d20bb640afc742185150613fef3742d57
Infinite <infinitekittens@protonmail.com>
2020-05-16 12:36:31 +0200

    Update components

--- ui/v2.5/src/components/Wall/WallItem.tsx
+++ ui/v2.5/src/components/Wall/WallItem.tsx
@@ -1,16 +1,13 @@
-import _ from "lodash";
 import React, { useRef, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import * as GQL from "src/core/generated-graphql";
 import { useConfiguration } from "src/core/StashService";
-import { useVideoHover } from "src/hooks";
 import { TextUtils, NavUtils } from "src/utils";
+import cx from "classnames";
 
 interface IWallItemProps {
   scene?: GQL.SlimSceneDataFragment;
   sceneMarker?: GQL.SceneMarkerDataFragment;
-  origin?: string;
-  onOverlay: (show: boolean) => void;
   clickHandler?: (
     item: GQL.SlimSceneDataFragment | GQL.SceneMarkerDataFragment
   ) => void;
@@ -22,61 +19,91 @@ interface IPreviews {
   image?: string;
 }
 
-const Preview: React.FC<{previews?: IPreviews, config?: any}> = ({ previews, config }) => {
+const Preview: React.FC<{previews?: IPreviews, config?: GQL.ConfigDataFragment, active: boolean }> = ({ previews, config, active }) => {
+  const videoElement = useRef() as React.MutableRefObject<HTMLVideoElement>;
+
+  const previewType = config?.interface?.wallPlayback;
+  const soundOnPreview = config?.interface?.soundOnPreview ?? false;
+
+  useEffect(() => {
+    if (!videoElement.current) return;
+    videoElement.current.muted = !(soundOnPreview && active);
+    if (previewType !== "video") {
+      if (active)
+        videoElement.current.play();
+      else
+        videoElement.current.pause();
+    }
+  }, [videoElement, previewType, soundOnPreview, active]);
+
   if (!previews) return <div />;
 
-  const previewType = config?.data?.configuration?.interface?.wallPlayback;
-  if (previewType === "video")
-    return <video src={previews.video} poster={previews.image} autoPlay loop muted className="scene-wall-image" />;
-  if (previewType === "animation")
-    return <img alt="" className="scene-wall-image" src={previews.animation || previews.image} />
+  const image = <img alt="" className="wall-item-media" src={(previewType === "animation" && previews.animation) || previews.image} />;
+  const video =  (
+    <video
+      src={previews.video}
+      poster={previews.image}
+      autoPlay={previewType === "video"}
+      loop
+      muted
+      className={cx("wall-item-media", { "wall-item-preview": previewType !== "video" })}
+      ref={videoElement}
+    />
+  );
 
-  if (previews.image)
-    return <img alt="" className="scene-wall-image" src={previews.image} />
-  return <video src={previews.video} className="scene-wall-image" />;
+  if (previewType === "video") {
+    return video;
+  }
+  return (
+    <>
+      { image }
+      { video }
+    </>
+  );
 };
 
 export const WallItem: React.FC<IWallItemProps> = (props: IWallItemProps) => {
-  const [videoPath, setVideoPath] = useState<string>();
-  const [previews, setPreviews] = useState<IPreviews>();
-  const [screenshotPath, setScreenshotPath] = useState<string>("");
-  const [title, setTitle] = useState<string>("");
-  const [tags, setTags] = useState<JSX.Element[]>([]);
+  const [active, setActive] = useState(false);
+  const wallItem = useRef() as React.MutableRefObject<HTMLDivElement>;
   const config = useConfiguration();
-  const hoverHandler = useVideoHover({
-    resetOnMouseLeave: true,
+
+  const showTextContainer = config.data?.configuration.interface.wallShowTitle ?? true;
+
+  const previews = props.sceneMarker ? {
+    video: props.sceneMarker.stream,
+    animation: props.sceneMarker.preview
+  } : {
+    video: props.scene?.paths.preview ?? undefined,
+    animation: props.scene?.paths.webp ?? undefined,
+    image: props.scene?.paths.screenshot ?? undefined
+  };
+
+  const setInactive = () => (
+    setActive(false)
+  );
+  const toggleActive = (e: TransitionEvent) => {
+    if (e.propertyName === "transform" && e.elapsedTime === 0) {
+      const newActive = getComputedStyle(wallItem.current).transform !== 'none';
+      setActive(newActive);
+    }
+  };
+
+  useEffect(() => {
+    const { current } = wallItem;
+    current?.addEventListener('transitioncancel', setInactive);
+    current?.addEventListener('transitionstart', toggleActive);
+    return () => {
+      current?.removeEventListener('transitioncancel', setInactive);
+      current?.removeEventListener('transitionstart', toggleActive);
+    }
   });
-  const showTextContainer =
-    config.data?.configuration.interface.wallShowTitle ?? true;
 
-  function onMouseEnter() {
-    hoverHandler.onMouseEnter();
-    if (!videoPath || videoPath === "") {
-      if (props.sceneMarker) {
-        setVideoPath(props.sceneMarker.stream || "");
-      } else if (props.scene) {
-        setVideoPath(props.scene.paths.preview || "");
-      }
+  const clickHandler = () => {
+    if (props.scene) {
+      props?.clickHandler?.(props.scene);
     }
-    props.onOverlay(true);
-  }
-  const debouncedOnMouseEnter = useRef(_.debounce(onMouseEnter, 500));
-
-  function onMouseLeave() {
-    hoverHandler.onMouseLeave();
-    setVideoPath("");
-    debouncedOnMouseEnter.current.cancel();
-    props.onOverlay(false);
-  }
-
-  function onClick() {
-    if (props.clickHandler === undefined) {
-      return;
-    }
-    if (props.scene !== undefined) {
-      props.clickHandler(props.scene);
-    } else if (props.sceneMarker !== undefined) {
-      props.clickHandler(props.sceneMarker);
+    if (props.sceneMarker) {
+      props?.clickHandler?.(props.sceneMarker);
     }
   }
 
@@ -89,85 +116,32 @@ export const WallItem: React.FC<IWallItemProps> = (props: IWallItemProps) => {
     }
   }
 
-  function onTransitionEnd(event: React.TransitionEvent<HTMLDivElement>) {
-    const target = event.currentTarget;
-    if (target.classList.contains("double-scale") && target.parentElement) {
-      target.parentElement.style.zIndex = "10";
-    } else if (target.parentElement) {
-      target.parentElement.style.zIndex = "";
-    }
-  }
+  const renderText = () => {
+    if (!showTextContainer) return;
 
-  useEffect(() => {
-    if (props.sceneMarker) {
-      setPreviews({
-        video: props.sceneMarker.stream,
-        animation: props.sceneMarker.preview
-      });
-      setTitle(
-        `${props.sceneMarker!.title} - ${TextUtils.secondsToTimestamp(
-          props.sceneMarker.seconds
-        )}`
-      );
-      const thisTags = props.sceneMarker.tags.map((tag) => (
-        <span key={tag.id} className="wall-tag">
-          {tag.name}
-        </span>
-      ));
-      thisTags.unshift(
-        <span key={props.sceneMarker.primary_tag.id} className="wall-tag">
-          {props.sceneMarker.primary_tag.name}
-        </span>
-      );
-      setTags(thisTags);
-    } else if (props.scene) {
-      setPreviews({
-        video: props.scene.paths.preview ?? undefined,
-        animation: props.scene.paths.webp ?? undefined,
-        image: props.scene.paths.screenshot ?? undefined
-      });
-      setScreenshotPath(props.scene.paths.screenshot || "");
-      setTitle(props.scene.title || "");
-    }
-  }, [props.sceneMarker, props.scene]);
+    const title = props.sceneMarker
+      ? `${props.sceneMarker!.title} - ${TextUtils.secondsToTimestamp(props.sceneMarker.seconds)}`
+      : props.scene?.title ?? "";
+    const tags = props.sceneMarker ? [ props.sceneMarker.primary_tag, ...props.sceneMarker.tags] : [];
 
-  const className = ["scene-wall-item-container"];
-  if (hoverHandler.isHovering.current) {
-    className.push("double-scale");
-  }
-  const style: React.CSSProperties = {};
-  if (props.origin) {
-    style.transformOrigin = props.origin;
-  }
+    return (
+      <div className="wall-item-text">
+        <div>{title}</div>
+        { tags.map(tag => (
+          <span key={tag.id} className="wall-tag">
+            {tag.name}
+          </span>
+        ))}
+      </div>
+    );
+  };
+
   return (
     <div className="wall-item">
-      <div
-        className={className.join(" ")}
-        style={style}
-        onTransitionEnd={onTransitionEnd}
-        onMouseEnter={() => debouncedOnMouseEnter.current()}
-        onMouseMove={() => debouncedOnMouseEnter.current()}
-        onMouseLeave={onMouseLeave}
-      >
-        <Link onClick={onClick} to={linkSrc}>
-          <video
-            src={videoPath}
-            poster={screenshotPath}
-            className="scene-wall-video"
-            style={hoverHandler.isHovering.current ? {} : { display: "none" }}
-            autoPlay
-            loop
-            ref={hoverHandler.videoEl}
-          />
-          <Preview previews={previews} config={config} />
-          {showTextContainer ? (
-            <div className="scene-wall-item-text-container">
-              <div>{title}</div>
-              {tags}
-            </div>
-          ) : (
-            ""
-          )}
+      <div className="wall-item-container" ref={wallItem}>
+        <Link onClick={clickHandler} to={linkSrc}>
+          <Preview previews={previews} config={config.data?.configuration} active={active} />
+          { renderText }
         </Link>
       </div>
     </div>
--- ui/v2.5/src/components/Wall/WallPanel.tsx
+++ ui/v2.5/src/components/Wall/WallPanel.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import * as GQL from "src/core/generated-graphql";
 import { WallItem } from "./WallItem";
 
@@ -13,103 +13,28 @@ interface IWallPanelProps {
 export const WallPanel: React.FC<IWallPanelProps> = (
   props: IWallPanelProps
 ) => {
-  const [showOverlay, setShowOverlay] = useState<boolean>(false);
+  const scenes = (props.scenes ?? []).map(scene => (
+    <WallItem
+      key={scene.id}
+      scene={scene}
+      clickHandler={props.clickHandler}
+    />
+  ));
 
-  function onOverlay(show: boolean) {
-    setShowOverlay(show);
-  }
+  const sceneMarkers = (props.sceneMarkers ?? []).map(marker => (
+    <WallItem
+      key={marker.id}
+      sceneMarker={marker}
+      clickHandler={props.clickHandler}
+    />
+  ));
 
-  function getOrigin(index: number, rowSize: number, total: number): string {
-    const isAtStart = index % rowSize === 0;
-    const isAtEnd = index % rowSize === rowSize - 1;
-    const endRemaining = total % rowSize;
-
-    // First row
-    if (total === 1) {
-      return "top";
-    }
-    if (index === 0) {
-      return "top left";
-    }
-    if (index === rowSize - 1 || (total < rowSize && index === total - 1)) {
-      return "top right";
-    }
-    if (index < rowSize) {
-      return "top";
-    }
-
-    // Bottom row
-    if (isAtEnd && index === total - 1) {
-      return "bottom right";
-    }
-    if (isAtStart && index === total - rowSize) {
-      return "bottom left";
-    }
-    if (endRemaining !== 0 && index >= total - endRemaining) {
-      return "bottom";
-    }
-    if (endRemaining === 0 && index >= total - rowSize) {
-      return "bottom";
-    }
-
-    // Everything else
-    if (isAtStart) {
-      return "center left";
-    }
-    if (isAtEnd) {
-      return "center right";
-    }
-    return "center";
-  }
-
-  function maybeRenderScenes() {
-    if (props.scenes === undefined) {
-      return;
-    }
-    return props.scenes.map((scene, index) => {
-      const origin = getOrigin(index, 5, props.scenes!.length);
-      return (
-        <WallItem
-          key={scene.id}
-          scene={scene}
-          onOverlay={onOverlay}
-          clickHandler={props.clickHandler}
-          origin={origin}
-        />
-      );
-    });
-  }
-
-  function maybeRenderSceneMarkers() {
-    if (props.sceneMarkers === undefined) {
-      return;
-    }
-    return props.sceneMarkers.map((marker, index) => {
-      const origin = getOrigin(index, 5, props.sceneMarkers!.length);
-      return (
-        <WallItem
-          key={marker.id}
-          sceneMarker={marker}
-          onOverlay={onOverlay}
-          clickHandler={props.clickHandler}
-          origin={origin}
-        />
-      );
-    });
-  }
-
-  function render() {
-    const overlayClassName = showOverlay ? "visible" : "hidden";
-    return (
-      <>
-        <div className={`wall-overlay ${overlayClassName}`} />
-        <div className="wall row justify-content-center">
-          {maybeRenderScenes()}
-          {maybeRenderSceneMarkers()}
-        </div>
-      </>
-    );
-  }
-
-  return render();
+  return (
+    <div className="row">
+      <div className="wall row justify-content-center">
+        {scenes}
+        {sceneMarkers}
+      </div>
+    </div>
+  );
 };
