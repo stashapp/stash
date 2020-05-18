93c96adb14aca22c68b279a3a885717824ff39d1
Infinite <infinitekittens@protonmail.com>
2020-05-18 21:38:25 +0200

    Prune v0.2 changes

--- ui/v2.5/src/components/Changelog/versions/v020.tsx
+++ ui/v2.5/src/components/Changelog/versions/v020.tsx
@@ -31,14 +31,12 @@ const markup = `
 *  Support for localized text, dates and numbers.
 *  Replace Blueprint with react-bootstrap.
 *  Add image count to gallery list.
-*  Update prettier to v2.0.1 and enable for SCSS.
 *  Add library size to main stats page.
 *  Add slim endpoints for entities to speed up filters.
 *  Export performance optimization.
 *  Add random male performer image.
 *  Added various missing filters to performer page.
 *  Add index/total count to end of pagination buttons.
-*  Refactor build.
 *  Add flags for performer countries.
 *  Querybuilder integration tests.
 *  Overhaul look and feel of folder select.
@@ -48,17 +46,13 @@ const markup = `
 *  Update performer image in UI when it's replaced.
 *  Fix performer height filter.
 *  Fix error when viewing scenes related to objects with illegal characters in name.
-*  Fix to allow scene to be removed when attached to a movie.
 *  Make ethnicity freetext and fix freeones ethnicity panic.
-*  Fix to filter on movies from performer filter to movie filter.
 *  Delete marker preview on marker change or delete.
 *  Prefer modified performer image over scraped one.
-*  Don't redirect login to migrate page.
-*  Performer and Movie UI fixes and improvements.
 *  Include gender in performer scraper results.
 *  Include scene o-counter in import/export.
 *  Make image extension check in zip files case insensitive.
-*  Freeones scraper tweaks.
+*  Update built-in Freeones scraper for new API.
 
 `;
 
