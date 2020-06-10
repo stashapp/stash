b898004799b5859798eafebd9d104cab9e0fd6ee
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2020-06-10 12:33:45 +1000

    Add 0.3 changelog

---
+++ ui/v2.5/src/components/Changelog/versions/v030.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactMarkdown from "react-markdown";
+
+const markup = `
+### 🎨 Improvements
+*  Add reload scrapers button.
+
+`;
+
+export default () => <ReactMarkdown source={markup} />;
