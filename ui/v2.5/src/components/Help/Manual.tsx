43afbe2d8320d4368c60bf892aa2e4f65fe76135
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2020-06-26 17:04:09 +1000

    Add documentation

--- ui/v2.5/src/components/Help/Manual.tsx
+++ ui/v2.5/src/components/Help/Manual.tsx
@@ -10,6 +10,7 @@ import Galleries from "src/docs/en/Galleries.md";
 import Scraping from "src/docs/en/Scraping.md";
 import Contributing from "src/docs/en/Contributing.md";
 import SceneFilenameParser from "src/docs/en/SceneFilenameParser.md";
+import KeyboardShortcuts from "src/docs/en/KeyboardShortcuts.md";
 import Help from "src/docs/en/Help.md";
 import { Page } from "./Page";
 
@@ -32,7 +33,7 @@ export const Manual: React.FC<IManualProps> = ({ show, onClose }) => {
     },
     {
       key: "Interface.md",
-      title: "Interface",
+      title: "Interface Options",
       content: Interface,
     },
     {
@@ -67,6 +68,11 @@ export const Manual: React.FC<IManualProps> = ({ show, onClose }) => {
       key: "Scraping.md",
       title: "Metadata Scraping",
       content: Scraping,
+    },
+    {
+      key: "KeyboardShortcuts.md",
+      title: "Keyboard Shortcuts",
+      content: KeyboardShortcuts,
     },
     {
       key: "Contributing.md",
