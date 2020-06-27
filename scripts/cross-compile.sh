0e3d92ab50564ff41737055811547974dc801e82
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2020-06-27 14:23:20 +1000

    Fix environment setting

--- scripts/cross-compile.sh
+++ scripts/cross-compile.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-BUILD_DATE=`go run -mod=vendor scripts/getDate.go`; export BUILD_DATE
-GITHASH=`git rev-parse --short HEAD`; export GITHASH
-STASH_VERSION=`git describe --tags --exclude latest_develop`; export STASH_VERSION
+BUILD_DATE=`go run -mod=vendor scripts/getDate.go`
+GITHASH=`git rev-parse --short HEAD`
+STASH_VERSION=`git describe --tags --exclude latest_develop`
+SETENV="BUILD_DATE=\"$BUILD_DATE\" GITHASH=$GITHASH STASH_VERSION=\"$STASH_VERSION\""
 SETUP="export GO111MODULE=on; export CGO_ENABLED=1; make packr;"
-WINDOWS="echo '=== Building Windows binary ==='; GOOS=windows GOARCH=amd64 CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++ LDFLAGS=\"-extldflags '-static' \" OUTPUT=\"dist/stash-win.exe\" make build-release;"
-DARWIN="echo '=== Building OSX binary ==='; GOOS=darwin GOARCH=amd64 CC=o64-clang CXX=o64-clang++ OUTPUT=\"dist/stash-osx\" make build-release;"
-LINUX="echo '=== Building Linux binary ==='; OUTPUT=\"dist/stash-linux\" make build-release;"
-RASPPI="echo '=== Building Raspberry Pi binary ==='; GOOS=linux GOARCH=arm GOARM=5 CC=arm-linux-gnueabi-gcc OUTPUT=\"dist/stash-pi\" make build-release;"
+WINDOWS="echo '=== Building Windows binary ==='; $SETENV GOOS=windows GOARCH=amd64 CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++ LDFLAGS=\"-extldflags '-static' \" OUTPUT=\"dist/stash-win.exe\" make build-release;"
+DARWIN="echo '=== Building OSX binary ==='; $SETENV GOOS=darwin GOARCH=amd64 CC=o64-clang CXX=o64-clang++ OUTPUT=\"dist/stash-osx\" make build-release;"
+LINUX="echo '=== Building Linux binary ==='; $SETENV OUTPUT=\"dist/stash-linux\" make build-release;"
+RASPPI="echo '=== Building Raspberry Pi binary ==='; $SETENV GOOS=linux GOARCH=arm GOARM=5 CC=arm-linux-gnueabi-gcc OUTPUT=\"dist/stash-pi\" make build-release;"
 
 COMMAND="$SETUP $WINDOWS $DARWIN $LINUX $RASPPI"
 
