4241d7249675aad512f5931c47735218819fe4ed
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2020-06-29 10:16:52 +1000

    Add tag name uniqueness checks

--- pkg/api/resolver_mutation_tag.go
+++ pkg/api/resolver_mutation_tag.go
@@ -2,10 +2,12 @@ package api
 
 import (
 	"context"
+	"fmt"
 	"strconv"
 	"time"
 
 	"github.com/stashapp/stash/pkg/database"
+	"github.com/stashapp/stash/pkg/manager"
 	"github.com/stashapp/stash/pkg/models"
 	"github.com/stashapp/stash/pkg/utils"
 )
@@ -30,12 +32,19 @@ func (r *mutationResolver) TagCreate(ctx context.Context, input models.TagCreate
 		}
 	}
 
-	// Start the transaction and save the studio
+	// Start the transaction and save the tag
 	tx := database.DB.MustBeginTx(ctx, nil)
 	qb := models.NewTagQueryBuilder()
+
+	// ensure name is unique
+	if err := manager.EnsureTagNameUnique(newTag.Name, tx); err != nil {
+		tx.Rollback()
+		return nil, err
+	}
+
 	tag, err := qb.Create(newTag, tx)
 	if err != nil {
-		_ = tx.Rollback()
+		tx.Rollback()
 		return nil, err
 	}
 
@@ -78,6 +87,26 @@ func (r *mutationResolver) TagUpdate(ctx context.Context, input models.TagUpdate
 	// Start the transaction and save the tag
 	tx := database.DB.MustBeginTx(ctx, nil)
 	qb := models.NewTagQueryBuilder()
+
+	// ensure name is unique
+	existing, err := qb.Find(tagID, tx)
+	if err != nil {
+		tx.Rollback()
+		return nil, err
+	}
+
+	if existing == nil {
+		tx.Rollback()
+		return nil, fmt.Errorf("Tag with ID %d not found", tagID)
+	}
+
+	if existing.Name != updatedTag.Name {
+		if err := manager.EnsureTagNameUnique(updatedTag.Name, tx); err != nil {
+			tx.Rollback()
+			return nil, err
+		}
+	}
+
 	tag, err := qb.Update(updatedTag, tx)
 	if err != nil {
 		_ = tx.Rollback()
