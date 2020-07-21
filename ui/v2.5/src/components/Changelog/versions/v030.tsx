a70ed0dc6ec98aa5609b5b1bc16ed3de1c7c047a
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2020-07-21 13:47:41 +1000

    Update manual and changelog

--- ui/v2.5/src/components/Changelog/versions/v030.tsx
+++ ui/v2.5/src/components/Changelog/versions/v030.tsx
@@ -12,6 +12,7 @@ const markup = `
 *  Add support for parent/child studios.
 
 ### 🎨 Improvements
+*  Add mapped and fixed post-processing scraping options.
 *  Search for files which have low or upper case supported filename extensions.
 *  Add dialog when pasting movie images.
 *  Allow click and click-drag selection after selecting scene.
