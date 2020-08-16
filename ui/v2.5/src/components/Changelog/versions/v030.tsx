877b1cb7802204b102a174a926d4f7a0d336028b
JoeSmithStarkers <60503487+JoeSmithStarkers@users.noreply.github.com>
2020-08-17 09:21:58 +1000

    Preview generation fallback (#725)
    * Added preview generation fallback feature.
    When a preview generation fails (often for wmv/avi files), the new code tries with less stricted (no xerror) and more time consuming options (slow+fast seek).
    Fix a minor issue when stash downloads ffmpeg/ffprobe, but doesn't re-detect their paths.

--- ui/v2.5/src/components/Changelog/versions/v030.tsx
+++ ui/v2.5/src/components/Changelog/versions/v030.tsx
@@ -19,6 +19,7 @@ const markup = `
 *  Add support for parent/child studios.
 
 ### 🎨 Improvements
+*  Make preview generation more fault-tolerant.
 *  Allow clearing of images and querying on missing images.
 *  Allow free-editing of scene movie number.
 *  Allow adding performers and studios from selectors.
