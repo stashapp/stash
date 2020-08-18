5a9ad68835b293561d1935c61fa2d37f679f4263
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2020-08-18 16:04:50 +1000

    Graphql changes from #454

--- pkg/api/resolver_mutation_configure.go
+++ pkg/api/resolver_mutation_configure.go
@@ -130,6 +130,10 @@ func (r *mutationResolver) ConfigureGeneral(ctx context.Context, input models.Co
 		refreshScraperCache = true
 	}
 
+	if input.StashBoxes != nil {
+		config.Set(config.StashBoxes, input.StashBoxes)
+	}
+
 	if err := config.Write(); err != nil {
 		return makeConfigGeneralResult(), err
 	}
