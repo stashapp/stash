198b94cd19dabb58fd97d7df2df7addd1c5950b0
InfiniteTF <infinitekittens@protonmail.com>
2020-09-02 02:30:37 +0200

    Allow updating tag name capitalization (#781)

--- pkg/manager/tag.go
+++ pkg/manager/tag.go
@@ -7,18 +7,18 @@ import (
 	"github.com/stashapp/stash/pkg/models"
 )
 
-func EnsureTagNameUnique(name string, tx *sqlx.Tx) error {
+func EnsureTagNameUnique(tag models.Tag, tx *sqlx.Tx) error {
 	qb := models.NewTagQueryBuilder()
 
 	// ensure name is unique
-	sameNameTag, err := qb.FindByName(name, tx, true)
+	sameNameTag, err := qb.FindByName(tag.Name, tx, true)
 	if err != nil {
 		_ = tx.Rollback()
 		return err
 	}
 
-	if sameNameTag != nil {
-		return fmt.Errorf("Tag with name '%s' already exists", name)
+	if sameNameTag != nil && tag.ID != sameNameTag.ID {
+		return fmt.Errorf("Tag with name '%s' already exists", tag.Name)
 	}
 
 	return nil
