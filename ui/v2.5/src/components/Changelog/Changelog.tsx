d21c73e6eed59b764c8ee9f4904bc5b19bc3e211
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2020-09-16 09:54:24 +1000

    Add 0.4.0 changelog, convert to md (#803)
    * Turn page into shared component
    * Add v0.4.0 changelog markdown page
    * Rename version files
    * Markdown conversion

--- ui/v2.5/src/components/Changelog/Changelog.tsx
+++ ui/v2.5/src/components/Changelog/Changelog.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { useChangelogStorage } from "src/hooks";
 import Version from "./Version";
-import { V010, V011, V020, V021, V030 } from "./versions";
+import V010 from "./versions/v010.md";
+import V011 from "./versions/v011.md";
+import V020 from "./versions/v020.md";
+import V021 from "./versions/v021.md";
+import V030 from "./versions/v030.md";
+import V040 from "./versions/v040.md";
+import { MarkdownPage } from "../Shared/MarkdownPage";
 
 const Changelog: React.FC = () => {
   const [{ data, loading }, setOpenState] = useChangelogStorage();
@@ -30,13 +36,21 @@ const Changelog: React.FC = () => {
     <>
       <h1 className="mb-4">Changelog:</h1>
       <Version
-        version={stashVersion || "v0.3.0"}
+        version={stashVersion || "v0.4.0"}
         date={buildDate}
         openState={openState}
         setOpenState={setVersionOpenState}
         defaultOpen
       >
-        <V030 />
+        <MarkdownPage page={V040} />
+      </Version>
+      <Version
+        version="v0.3.0"
+        date="2020-09-02"
+        openState={openState}
+        setOpenState={setVersionOpenState}
+      >
+        <MarkdownPage page={V030} />
       </Version>
       <Version
         version="v0.2.1"
@@ -44,7 +58,7 @@ const Changelog: React.FC = () => {
         openState={openState}
         setOpenState={setVersionOpenState}
       >
-        <V021 />
+        <MarkdownPage page={V021} />
       </Version>
       <Version
         version="v0.2.0"
@@ -52,7 +66,7 @@ const Changelog: React.FC = () => {
         openState={openState}
         setOpenState={setVersionOpenState}
       >
-        <V020 />
+        <MarkdownPage page={V020} />
       </Version>
       <Version
         version="v0.1.1"
@@ -60,7 +74,7 @@ const Changelog: React.FC = () => {
         openState={openState}
         setOpenState={setVersionOpenState}
       >
-        <V011 />
+        <MarkdownPage page={V011} />
       </Version>
       <Version
         version="v0.1.0"
@@ -68,7 +82,7 @@ const Changelog: React.FC = () => {
         openState={openState}
         setOpenState={setVersionOpenState}
       >
-        <V010 />
+        <MarkdownPage page={V010} />
       </Version>
     </>
   );
