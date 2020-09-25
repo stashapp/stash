e357b9177a32410ecb6073fc09bbd2f82978eb36
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2020-09-25 13:15:49 +1000

    Show gallery card overlays

--- ui/v2.5/src/components/Galleries/GalleryCard.tsx
+++ ui/v2.5/src/components/Galleries/GalleryCard.tsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import * as GQL from "src/core/generated-graphql";
 import { FormattedPlural } from "react-intl";
 import { HoverPopover, Icon, TagLink } from "../Shared";
+import { useConfiguration } from "src/core/StashService";
 
 interface IProps {
   gallery: GQL.GalleryDataFragment;
@@ -14,6 +15,10 @@ interface IProps {
 }
 
 export const GalleryCard: React.FC<IProps> = (props) => {
+  const config = useConfiguration();
+  const showStudioAsText =
+    config?.data?.configuration.interface.showStudioAsText ?? false;
+    
   function maybeRenderScenePopoverButton() {
     if (!props.gallery.scene) return;
 
@@ -32,17 +37,100 @@ export const GalleryCard: React.FC<IProps> = (props) => {
     );
   }
 
+  function maybeRenderTagPopoverButton() {
+    if (props.gallery.tags.length <= 0) return;
+
+    const popoverContent = props.gallery.tags.map((tag) => (
+      <TagLink key={tag.id} tag={tag} />
+    ));
+
+    return (
+      <HoverPopover placement="bottom" content={popoverContent}>
+        <Button className="minimal">
+          <Icon icon="tag" />
+          <span>{props.gallery.tags.length}</span>
+        </Button>
+      </HoverPopover>
+    );
+  }
+
+  function maybeRenderPerformerPopoverButton() {
+    if (props.gallery.performers.length <= 0) return;
+
+    const popoverContent = props.gallery.performers.map((performer) => (
+      <div className="performer-tag-container row" key={performer.id}>
+        <Link
+          to={`/performers/${performer.id}`}
+          className="performer-tag col m-auto zoom-2"
+        >
+          <img
+            className="image-thumbnail"
+            alt={performer.name ?? ""}
+            src={performer.image_path ?? ""}
+          />
+        </Link>
+        <TagLink key={performer.id} performer={performer} className="d-block" />
+      </div>
+    ));
+
+    return (
+      <HoverPopover placement="bottom" content={popoverContent}>
+        <Button className="minimal">
+          <Icon icon="user" />
+          <span>{props.gallery.performers.length}</span>
+        </Button>
+      </HoverPopover>
+    );
+  }
+
+  function maybeRenderSceneStudioOverlay() {
+    if (!props.gallery.studio) return;
+
+    return (
+      <div className="scene-studio-overlay">
+        <Link to={`/studios/${props.gallery.studio.id}`}>
+          {showStudioAsText ? (
+            props.gallery.studio.name
+          ) : (
+            <img
+              className="image-thumbnail"
+              alt={props.gallery.studio.name}
+              src={props.gallery.studio.image_path ?? ""}
+            />
+          )}
+        </Link>
+      </div>
+    );
+  }
+
   function maybeRenderPopoverButtonGroup() {
-    if (props.gallery.scene) {
+    if (props.gallery.scene || props.gallery.performers.length > 0 || props.gallery.tags.length > 0) {
       return (
         <>
           <hr />
           <ButtonGroup className="card-popovers">
+            {maybeRenderTagPopoverButton()}
+            {maybeRenderPerformerPopoverButton()}
             {maybeRenderScenePopoverButton()}
           </ButtonGroup>
         </>
       );
     }
+  }
+
+  function maybeRenderRatingBanner() {
+    if (!props.gallery.rating) {
+      return;
+    }
+    return (
+      <div
+        className={`rating-banner ${
+          props.gallery.rating ? `rating-${props.gallery.rating}` : ""
+        }`}
+      >
+        RATING: {props.gallery.rating}
+      </div>
+    );
   }
 
   function handleImageClick(
@@ -91,22 +179,26 @@ export const GalleryCard: React.FC<IProps> = (props) => {
         }}
       />
 
-      <Link
-        to={`/galleries/${props.gallery.id}`}
-        className="gallery-card-header"
-        onClick={handleImageClick}
-        onDragStart={handleDrag}
-        onDragOver={handleDragOver}
-        draggable={props.selecting}
-      >
-        {props.gallery.cover ? (
-          <img
-            className="gallery-card-image"
-            alt={props.gallery.title ?? ""}
-            src={`${props.gallery.cover.paths.thumbnail}`}
-          />
-        ) : undefined}
-      </Link>
+      <div className="gallery-section">
+        <Link
+          to={`/galleries/${props.gallery.id}`}
+          className="gallery-card-header"
+          onClick={handleImageClick}
+          onDragStart={handleDrag}
+          onDragOver={handleDragOver}
+          draggable={props.selecting}
+        >
+          {props.gallery.cover ? (
+            <img
+              className="gallery-card-image"
+              alt={props.gallery.title ?? ""}
+              src={`${props.gallery.cover.paths.thumbnail}`}
+            />
+          ) : undefined}
+          {maybeRenderRatingBanner()}
+        </Link>
+        {maybeRenderSceneStudioOverlay()}
+      </div>
       <div className="card-section">
         <Link to={`/galleries/${props.gallery.id}`}>
           <h5 className="card-section-title">
