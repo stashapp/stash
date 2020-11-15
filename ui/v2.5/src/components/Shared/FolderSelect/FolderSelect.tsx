0a098b1d63830294f6e037e96c2c4186cb4fe53a
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2020-11-16 09:20:04 +1100

    Selective scan (#940)

--- ui/v2.5/src/components/Shared/FolderSelect/FolderSelect.tsx
+++ ui/v2.5/src/components/Shared/FolderSelect/FolderSelect.tsx
@@ -1,85 +1,83 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { FormattedMessage } from "react-intl";
-import { Button, InputGroup, Form, Modal } from "react-bootstrap";
+import { Button, InputGroup, Form } from "react-bootstrap";
 import { LoadingIndicator } from "src/components/Shared";
 import { useDirectory } from "src/core/StashService";
 
 interface IProps {
-  onClose: (directory?: string) => void;
+  currentDirectory: string;
+  setCurrentDirectory: (value: string) => void;
+  defaultDirectories?: string[];
+  appendButton?: JSX.Element;
 }
 
-export const FolderSelect: React.FC<IProps> = (props: IProps) => {
-  const [currentDirectory, setCurrentDirectory] = useState<string>("");
+export const FolderSelect: React.FC<IProps> = ({
+  currentDirectory,
+  setCurrentDirectory,
+  defaultDirectories,
+  appendButton,
+}) => {
   const { data, error, loading } = useDirectory(currentDirectory);
 
+  const selectableDirectories: string[] = currentDirectory
+    ? data?.directory.directories ?? defaultDirectories ?? []
+    : defaultDirectories ?? [];
+
   useEffect(() => {
-    if (currentDirectory === "" && data?.directory.path)
+    if (currentDirectory === "" && !defaultDirectories && data?.directory.path)
       setCurrentDirectory(data.directory.path);
-  }, [currentDirectory, data]);
+  }, [currentDirectory, setCurrentDirectory, data, defaultDirectories]);
 
-  const selectableDirectories: string[] = data?.directory.directories ?? [];
+  function goUp() {
+    if (defaultDirectories?.includes(currentDirectory)) {
+      setCurrentDirectory("");
+    } else if (data?.directory.parent) {
+      setCurrentDirectory(data.directory.parent);
+    }
+  }
 
-  const topDirectory = data?.directory?.parent ? (
-    <li className="folder-list-parent folder-list-item">
-      <Button
-        variant="link"
-        onClick={() =>
-          data.directory.parent && setCurrentDirectory(data.directory.parent)
-        }
-      >
-        <FormattedMessage defaultMessage="Up a directory" id="up-dir" />
-      </Button>
-    </li>
-  ) : null;
+  const topDirectory =
+    currentDirectory && data?.directory?.parent ? (
+      <li className="folder-list-parent folder-list-item">
+        <Button variant="link" onClick={() => goUp()}>
+          <FormattedMessage defaultMessage="Up a directory" id="up-dir" />
+        </Button>
+      </li>
+    ) : null;
 
   return (
-    <Modal show onHide={() => props.onClose()} title="">
-      <Modal.Header>Select Directory</Modal.Header>
-      <Modal.Body>
-        <div className="dialog-content">
-          {error ? <h1>{error.message}</h1> : ""}
-          <InputGroup>
-            <Form.Control
-              placeholder="File path"
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                setCurrentDirectory(e.currentTarget.value)
-              }
-              value={currentDirectory}
-              spellCheck={false}
-            />
-            <InputGroup.Append>
-              {!data || !data.directory || loading ? (
-                <LoadingIndicator inline />
-              ) : (
-                ""
-              )}
-            </InputGroup.Append>
-          </InputGroup>
-          <ul className="folder-list">
-            {topDirectory}
-            {selectableDirectories.map((path) => {
-              return (
-                <li key={path} className="folder-list-item">
-                  <Button
-                    variant="link"
-                    onClick={() => setCurrentDirectory(path)}
-                  >
-                    {path}
-                  </Button>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button
-          variant="success"
-          onClick={() => props.onClose(currentDirectory)}
-        >
-          Add
-        </Button>
-      </Modal.Footer>
-    </Modal>
+    <>
+      {error ? <h1>{error.message}</h1> : ""}
+      <InputGroup>
+        <Form.Control
+          placeholder="File path"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setCurrentDirectory(e.currentTarget.value)
+          }
+          value={currentDirectory}
+          spellCheck={false}
+        />
+        {appendButton ? (
+          <InputGroup.Append>{appendButton}</InputGroup.Append>
+        ) : undefined}
+        {!data || !data.directory || loading ? (
+          <InputGroup.Append>
+            <LoadingIndicator inline small message="" />
+          </InputGroup.Append>
+        ) : undefined}
+      </InputGroup>
+      <ul className="folder-list">
+        {topDirectory}
+        {selectableDirectories.map((path) => {
+          return (
+            <li key={path} className="folder-list-item">
+              <Button variant="link" onClick={() => setCurrentDirectory(path)}>
+                {path}
+              </Button>
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 };
