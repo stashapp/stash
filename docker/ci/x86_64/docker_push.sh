2419527a1d5a02f12b3451fa7fde712338d6e396
tweeticoats <tweeticoats@github.com>
2020-11-22 22:02:20 +1030

    This uses buildx to build docker containers. buildx is an alternate build method that allows you to build multiple platforms all at once. This will add support for building arm6, arm7, arm64 and x86_64 in parallel.

--- docker/ci/x86_64/docker_push.sh
+++ docker/ci/x86_64/docker_push.sh
@@ -3,7 +3,6 @@
 DOCKER_TAG=$1
 
 # must build the image from dist directory
-docker build -t stashapp/stash:$DOCKER_TAG -f ./docker/ci/x86_64/Dockerfile ./dist
+#echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v6,linux/arm/v7 --push --output type=registry,name=tweeticoats/stash:$DOCKER_TAG,push=true -f docker/ci/x86_64/Dockerfile dist/
 
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push stashapp/stash:$DOCKER_TAG
