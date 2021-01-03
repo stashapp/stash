20aaabd90ca910ad27016d24ed432ba2e14c3b87
tweeticoats <tweeticoats@github.com>
2021-01-03 23:49:03 +1030

    Switching to 18.04 as that seems to work

--- docker/ci/x86_64/docker_push.sh
+++ docker/ci/x86_64/docker_push.sh
@@ -3,6 +3,6 @@
 DOCKER_TAG=$1
 
 # must build the image from dist directory
-#echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v6,linux/arm/v7 --push --output type=registry,name=tweeticoats/stash:$DOCKER_TAG,push=true -f docker/ci/x86_64/Dockerfile dist/
 
