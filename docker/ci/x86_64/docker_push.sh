b7ea874fcce76414d56887fa8aeb57f980e59432
tweeticoats <tweeticoats@github.com>
2021-02-02 09:44:19 +1030

    Changing the docker container to use ubuntu and use the ubuntu ffmpeg and python

--- docker/ci/x86_64/docker_push.sh
+++ docker/ci/x86_64/docker_push.sh
@@ -4,5 +4,5 @@ DOCKER_TAG=$1
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 # must build the image from dist directory
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v6,linux/arm/v7 --push --output type=registry,name=stashapp/stash:$DOCKER_TAG,push=true -f docker/ci/x86_64/Dockerfile dist/
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 --push --output type=image,name=stashapp/stash:$DOCKER_TAG,push=true -f docker/ci/x86_64/Dockerfile dist/
 
