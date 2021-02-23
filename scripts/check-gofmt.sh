51ee41284b19b86ddce5a8253932f12a9f941638
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2021-02-24 08:18:08 +1100

    Final touches

--- scripts/check-gofmt.sh
+++ scripts/check-gofmt.sh
@@ -28,7 +28,7 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-gofiles=./pkg/...
+gofiles=./pkg/
 [ "$OS" = "Windows_NT" ] && gofiles=.\\pkg\\
 
 unformatted=$(gofmt -l $gofiles)
