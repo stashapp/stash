e5bd5aedd7c8462bc739f51cae032f05b42555d0
Infinite <infinitekittens@protonmail.com>
2021-02-28 18:24:08 +0000

    yarn format

--- ui/v2.5/src/components/Settings/SettingsDuplicatePanel.tsx
+++ ui/v2.5/src/components/Settings/SettingsDuplicatePanel.tsx
@@ -4,14 +4,24 @@ import { Link } from "react-router-dom";
 import { FormattedNumber } from "react-intl";
 
 import * as GQL from "src/core/generated-graphql";
-import { LoadingIndicator, ErrorMessage, HoverPopover } from "src/components/Shared";
+import {
+  LoadingIndicator,
+  ErrorMessage,
+  HoverPopover,
+} from "src/components/Shared";
 import { TextUtils } from "src/utils";
 import { DeleteScenesDialog } from "src/components/Scenes/DeleteScenesDialog";
 
 export const SettingsDuplicatePanel: React.FC = () => {
   const [distance, setDistance] = useState(0);
-  const { data, loading, refetch } = GQL.useFindDuplicateScenesQuery({ fetchPolicy: "no-cache", variables: { distance } });
-  const [deletingScene, setDeletingScene] = useState<GQL.SlimSceneDataFragment | null>(null);
+  const { data, loading, refetch } = GQL.useFindDuplicateScenesQuery({
+    fetchPolicy: "no-cache",
+    variables: { distance },
+  });
+  const [
+    deletingScene,
+    setDeletingScene,
+  ] = useState<GQL.SlimSceneDataFragment | null>(null);
 
   if (loading) return <LoadingIndicator />;
   if (!data) return <ErrorMessage error="Error searching for duplicates." />;
@@ -19,8 +29,7 @@ export const SettingsDuplicatePanel: React.FC = () => {
 
   function onDeleteDialogClosed(deleted: boolean) {
     setDeletingScene(null);
-    if (deleted)
-      refetch();
+    if (deleted) refetch();
   }
 
   const renderFilesize = (filesize: string | null | undefined) => {
@@ -36,52 +45,66 @@ export const SettingsDuplicatePanel: React.FC = () => {
         maximumFractionDigits={2}
       />
     );
-  }
+  };
 
   return (
     <div className="container">
-      { deletingScene && (
-        <DeleteScenesDialog selected={[deletingScene]} onClose={onDeleteDialogClosed} />
+      {deletingScene && (
+        <DeleteScenesDialog
+          selected={[deletingScene]}
+          onClose={onDeleteDialogClosed}
+        />
       )}
-        <h4>Duplicate Scenes</h4>
-        <Form.Group>
-          <Row noGutters>
-            <Form.Label>Search Accuracy</Form.Label>
-            <Col xs={2}>
-              <Form.Control
-                as="select"
-                onChange={e => setDistance(Number.parseInt(e.currentTarget.value, 10) || 0)}
-                value={distance}
-                className="ml-4"
-              >
-                <option value={0}>Accurate</option>
-                <option value={1}>High</option>
-                <option value={2}>Medium</option>
-                <option value={3}>Low</option>
-              </Form.Control>
-            </Col>
-          </Row>
-          <Form.Text>Levels below &ldquo;Accurate&rdquo; take longer to calculate and can yield larger amounts of false positives.</Form.Text>
-        </Form.Group>
-        <h6>{ scenes.length } sets of duplicates found.</h6>
-        <Table striped>
-          <thead>
-            <tr>
-              <th> </th>
-              <th>Title</th>
-              <th>Duration</th>
-              <th>Filesize</th>
-              <th>Resolution</th>
-              <th>Bitrate</th>
-              <th>Codec</th>
-              <th>Delete</th>
-            </tr>
-          </thead>
-          <tbody>
-            {scenes.map((group) => group.map((scene, i) => (
-              <tr className={i === 0 ? 'duplicate-group' : ''}>
+      <h4>Duplicate Scenes</h4>
+      <Form.Group>
+        <Row noGutters>
+          <Form.Label>Search Accuracy</Form.Label>
+          <Col xs={2}>
+            <Form.Control
+              as="select"
+              onChange={(e) =>
+                setDistance(Number.parseInt(e.currentTarget.value, 10) || 0)
+              }
+              value={distance}
+              className="ml-4"
+            >
+              <option value={0}>Accurate</option>
+              <option value={1}>High</option>
+              <option value={2}>Medium</option>
+              <option value={3}>Low</option>
+            </Form.Control>
+          </Col>
+        </Row>
+        <Form.Text>
+          Levels below &ldquo;Accurate&rdquo; take longer to calculate and can
+          yield larger amounts of false positives.
+        </Form.Text>
+      </Form.Group>
+      <h6>{scenes.length} sets of duplicates found.</h6>
+      <Table striped>
+        <thead>
+          <tr>
+            <th> </th>
+            <th>Title</th>
+            <th>Duration</th>
+            <th>Filesize</th>
+            <th>Resolution</th>
+            <th>Bitrate</th>
+            <th>Codec</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {scenes.map((group) =>
+            group.map((scene, i) => (
+              <tr className={i === 0 ? "duplicate-group" : ""}>
                 <td>
-                  <HoverPopover content={<img src={scene.paths.sprite ?? ""} alt="" width={600} />} placement="right">
+                  <HoverPopover
+                    content={
+                      <img src={scene.paths.sprite ?? ""} alt="" width={600} />
+                    }
+                    placement="right"
+                  >
                     <img src={scene.paths.sprite ?? ""} alt="" width={100} />
                   </HoverPopover>
                 </td>
@@ -94,9 +117,7 @@ export const SettingsDuplicatePanel: React.FC = () => {
                   {scene.file.duration &&
                     TextUtils.secondsToTimestamp(scene.file.duration)}
                 </td>
-                <td>
-                  {renderFilesize(scene.file.size)}
-                </td>
+                <td>{renderFilesize(scene.file.size)}</td>
                 <td>{`${scene.file.width}x${scene.file.height}`}</td>
                 <td>
                   <FormattedNumber
@@ -116,11 +137,14 @@ export const SettingsDuplicatePanel: React.FC = () => {
                   </Button>
                 </td>
               </tr>
-            )))}
-          </tbody>
-        </Table>
+            ))
+          )}
+        </tbody>
+      </Table>
       {scenes.length === 0 && (
-        <h4 className="text-center mt-4">No duplicates found. Make sure the phash task has been run.</h4>
+        <h4 className="text-center mt-4">
+          No duplicates found. Make sure the phash task has been run.
+        </h4>
       )}
     </div>
   );
