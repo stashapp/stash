cb3016f22349e39412d07c293bf07adf383ebd89
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2021-03-10 17:40:36 +1100

    Add performer image from URL

--- pkg/utils/image.go
+++ pkg/utils/image.go
@@ -4,10 +4,65 @@ import (
 	"crypto/md5"
 	"encoding/base64"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"regexp"
 	"strings"
+	"time"
 )
+
+// Timeout to get the image. Includes transfer time. May want to make this
+// configurable at some point.
+const imageGetTimeout = time.Second * 30
+
+const base64RE = `^data:.+\/(.+);base64,(.*)$`
+
+// ProcessImageInput transforms an image string either from a base64 encoded
+// string, or from a URL, and returns the image as a byte slice
+func ProcessImageInput(imageInput string) ([]byte, error) {
+	regex := regexp.MustCompile(base64RE)
+	if regex.MatchString(imageInput) {
+		_, d, err := ProcessBase64Image(imageInput)
+		return d, err
+	}
+
+	// assume input is a URL. Read it.
+	return ReadImageFromURL(imageInput)
+}
+
+// ReadImageFromURL returns image data from a URL
+func ReadImageFromURL(url string) ([]byte, error) {
+	client := &http.Client{
+		Timeout: imageGetTimeout,
+	}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	// assume is a URL for now
+
+	// set the host of the URL as the referer
+	if req.URL.Scheme != "" {
+		req.Header.Set("Referer", req.URL.Scheme+"://"+req.Host+"/")
+	}
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
 
 // ProcessBase64Image transforms a base64 encoded string from a form post and returns the MD5 hash of the data and the
 // image itself as a byte slice.
@@ -16,7 +71,7 @@ func ProcessBase64Image(imageString string) (string, []byte, error) {
 		return "", nil, fmt.Errorf("empty image string")
 	}
 
-	regex := regexp.MustCompile(`^data:.+\/(.+);base64,(.*)$`)
+	regex := regexp.MustCompile(base64RE)
 	matches := regex.FindStringSubmatch(imageString)
 	var encodedString string
 	if len(matches) > 2 {
--- ui/v2.5/src/components/Shared/ImageInput.tsx
+++ ui/v2.5/src/components/Shared/ImageInput.tsx
@@ -1,10 +1,14 @@
-import React from "react";
-import { Button, Form } from "react-bootstrap";
+import React, { useState } from "react";
+import { Button, Col, Form, OverlayTrigger, Popover, Row } from "react-bootstrap";
+import { FormUtils } from "src/utils";
+import { Modal } from ".";
+import Icon from "./Icon";
 
 interface IImageInput {
   isEditing: boolean;
   text?: string;
   onImageChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onImageURL?: (url: string) => void;
   acceptSVG?: boolean;
 }
 
@@ -12,18 +16,108 @@ export const ImageInput: React.FC<IImageInput> = ({
   isEditing,
   text,
   onImageChange,
+  onImageURL,
   acceptSVG = false,
 }) => {
+  const [isShowDialog, setIsShowDialog] = useState(false);
+  const [url, setURL] = useState("");
+
   if (!isEditing) return <div />;
 
+  if (!onImageURL) {
+    // just return the file input
+    return (
+      <Form.Label className="image-input">
+        <Button variant="secondary">{text ?? "Browse for image..."}</Button>
+        <Form.Control
+          type="file"
+          onChange={onImageChange}
+          accept={`.jpg,.jpeg,.png${acceptSVG ? ",.svg" : ""}`}
+        />
+      </Form.Label>
+    )
+  }
+
+  function onConfirmURL() {
+    if (!onImageURL) {
+      return;
+    }
+
+    setIsShowDialog(false);
+    onImageURL(url);
+  }
+
+  function renderDialog() {
+    return (
+      <Modal
+        show={!!isShowDialog}
+        onHide={() => setIsShowDialog(false)}
+        header="Image URL"
+        accept={{ onClick: onConfirmURL, text: "Confirm" }}
+      >
+        <div className="dialog-content">
+          <Form.Group controlId="url" as={Row}>
+            <Form.Label column xs={3}>
+              URL
+            </Form.Label>
+            <Col xs={9}>
+              <Form.Control
+                className="text-input"
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                  setURL(event.currentTarget.value)
+                }
+                value={url}
+                placeholder="URL"
+              />
+            </Col>
+          </Form.Group>
+        </div>
+      </Modal>
+    );
+  }
+
+  const popover = (
+    <Popover id="set-image-popover">
+      <Popover.Content>
+        <>
+          <div>
+            <Form.Label className="image-input">
+              <Button variant="secondary">
+                <span className="fa-icon">
+                  <Icon icon="file" />
+                </span>
+                <span>
+                  From file...
+                </span>
+              </Button>
+              <Form.Control
+                type="file"
+                onChange={onImageChange}
+                accept={`.jpg,.jpeg,.png${acceptSVG ? ",.svg" : ""}`}
+              />
+            </Form.Label>
+          </div>
+          <div>
+            <Button className="minimal" onClick={() => setIsShowDialog(true)}>
+              <span className="fa-icon">
+                <Icon icon="link" />
+              </span>
+              <span>From URL...</span>
+            </Button>
+          </div>
+        </>
+      </Popover.Content>
+    </Popover>
+  );
+
   return (
-    <Form.Label className="image-input">
-      <Button variant="secondary">{text ?? "Browse for image..."}</Button>
-      <Form.Control
-        type="file"
-        onChange={onImageChange}
-        accept={`.jpg,.jpeg,.png${acceptSVG ? ",.svg" : ""}`}
-      />
-    </Form.Label>
+    <>
+      {renderDialog()}
+      <OverlayTrigger trigger="click" placement="top" overlay={popover} rootClose>
+        <Button variant="secondary" className="mr-2">
+        {text ?? "Set image..."}
+        </Button>
+      </OverlayTrigger>
+    </>
   );
 };
