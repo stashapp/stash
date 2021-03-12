0eebe897886fb0af8e7deaead7682c5115a6976b
Infinite <infinitekittens@protonmail.com>
2021-03-12 09:37:54 +0000

    Fix image url

--- pkg/api/urlbuilders/image.go
+++ pkg/api/urlbuilders/image.go
@@ -20,9 +20,9 @@ func NewImageURLBuilder(baseURL string, image *models.Image) ImageURLBuilder {
 }
 
 func (b ImageURLBuilder) GetImageURL() string {
-	return b.BaseURL + "/image/" + b.ImageID + "/image" + b.UpdatedAt
+	return b.BaseURL + "/image/" + b.ImageID + "/image?" + b.UpdatedAt
 }
 
 func (b ImageURLBuilder) GetThumbnailURL() string {
-	return b.BaseURL + "/image/" + b.ImageID + "/thumbnail" + b.UpdatedAt
+	return b.BaseURL + "/image/" + b.ImageID + "/thumbnail?" + b.UpdatedAt
 }
