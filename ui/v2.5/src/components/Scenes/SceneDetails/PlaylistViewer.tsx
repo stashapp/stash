b663d13a38ab3a23652c36b1e4c5bba157538b72
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2021-03-17 17:09:25 +1100

    Add controls

--- ui/v2.5/src/components/Scenes/SceneDetails/PlaylistViewer.tsx
+++ ui/v2.5/src/components/Scenes/SceneDetails/PlaylistViewer.tsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import cx from "classnames";
 import * as GQL from "src/core/generated-graphql";
 import { TextUtils } from "src/utils";
+import { Button } from "react-bootstrap";
+import { Icon } from "src/components/Shared";
 
 export interface IPlaylistViewer {
   scenes?: GQL.SlimSceneDataFragment[];
@@ -38,6 +40,19 @@ export const PlaylistViewer: React.FC<IPlaylistViewer> = ({
 
   return (
     <div id="playlist-viewer">
+      <div id="playlist-controls" className="d-flex justify-content-end">
+        <div>
+          <Button className="minimal" variant="secondary" >
+            <Icon icon="step-backward" />
+          </Button>
+          <Button className="minimal" variant="secondary">
+            <Icon icon="step-forward" />
+          </Button>
+          <Button className="minimal" variant="secondary">
+            <Icon icon="random" />
+          </Button>
+        </div>
+      </div>
       <ol>{(scenes ?? []).map(renderPlaylistEntry)}</ol>
     </div>
   );
