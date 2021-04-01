3fed142dd4d6f90ecd183dbcfefe4ef13c6a36cb
peolic <66393006+peolic@users.noreply.github.com>
2021-04-01 16:37:08 +0300

    Parse date string manually

--- ui/v2.5/src/utils/text.ts
+++ ui/v2.5/src/utils/text.ts
@@ -69,17 +69,27 @@ const fileNameFromPath = (path: string) => {
   return path.replace(/^.*[\\/]/, "");
 };
 
+const stringToDate = (dateString: string) => {
+  if (!dateString) return null;
+
+  const parts = dateString.split("-");
+  // Invalid date string
+  if (parts.length !== 3) return null;
+
+  const year = Number(parts[0]);
+  const monthIndex = Math.max(0, Number(parts[1]) - 1);
+  const day = Number(parts[2]);
+
+  return new Date(year, monthIndex, day, 0, 0, 0, 0);
+};
+
 const getAge = (dateString?: string | null, fromDateString?: string) => {
   if (!dateString) return 0;
 
-  const birthdate = new Date(dateString);
-  const fromDate = fromDateString ? new Date(fromDateString) : new Date();
+  const birthdate = stringToDate(dateString);
+  const fromDate = fromDateString ? stringToDate(fromDateString) : new Date();
 
-  // Set time to midnight to force system timezone instead of UTC
-  birthdate.setHours(0, 0, 0, 0);
-  if (fromDateString) {
-    fromDate.setHours(0, 0, 0, 0);
-  }
+  if (!birthdate || !fromDate) return 0;
 
   let age = fromDate.getFullYear() - birthdate.getFullYear();
   if (
@@ -182,6 +192,7 @@ const TextUtils = {
   fileSizeFractionalDigits,
   secondsToTimestamp,
   fileNameFromPath,
+  stringToDate,
   age: getAge,
   bitRate,
   resolution,
