3869e9e0fb827bd3af48642b010620792f54613e
DamJa <julien0221@outlook.com>
2021-04-02 21:40:16 +0100

    fixed whitespace not trimmed query string

--- ui/v2.5/src/models/list-filter/filter.ts
+++ ui/v2.5/src/models/list-filter/filter.ts
@@ -338,7 +338,7 @@ export class ListFilterModel {
       this.displayMode = Number.parseInt(params.disp, 10);
     }
     if (params.q) {
-      this.searchTerm = params.q;
+      this.searchTerm = params.q.trim();
     }
     if (params.p) {
       this.currentPage = Number.parseInt(params.p, 10);
