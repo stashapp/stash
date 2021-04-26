c60e44983c7f96db4b704eea27913e54c946383b
Infinite <infinitekittens@protonmail.com>
2021-04-26 09:27:57 +0000

    Simplify ffmpeg runner

--- pkg/ffmpeg/encoder.go
+++ pkg/ffmpeg/encoder.go
@@ -1,8 +1,7 @@
 package ffmpeg
 
 import (
-	"fmt"
-	"io/ioutil"
+	"bytes"
 	"os"
 	"os/exec"
 	"strings"
@@ -62,7 +61,7 @@ func KillRunningEncoders(path string) {
 
 	for _, process := range processes {
 		// assume it worked, don't check for error
-		fmt.Printf("Killing encoder process for file: %s", path)
+		logger.Infof("Killing encoder process for file: %s", path)
 		process.Kill()
 
 		// wait for the process to die before returning
@@ -85,55 +84,22 @@ func KillRunningEncoders(path string) {
 func (e *Encoder) run(probeResult VideoFile, args []string) (string, error) {
 	cmd := exec.Command(e.Path, args...)
 
-	stderr, err := cmd.StderrPipe()
-	if err != nil {
-		logger.Error("FFMPEG stderr not available: " + err.Error())
-	}
+	var stdout, stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
 
-	stdout, err := cmd.StdoutPipe()
-	if nil != err {
-		logger.Error("FFMPEG stdout not available: " + err.Error())
-	}
-
-	if err = cmd.Start(); err != nil {
+	if err := cmd.Start(); err != nil {
 		return "", err
 	}
 
-	buf := make([]byte, 80)
-	lastProgress := 0.0
-	var errBuilder strings.Builder
-	for {
-		n, err := stderr.Read(buf)
-		if n > 0 {
-			data := string(buf[0:n])
-			time := GetTimeFromRegex(data)
-			if time > 0 && probeResult.Duration > 0 {
-				progress := time / probeResult.Duration
-
-				if progress > lastProgress+0.01 {
-					logger.Infof("Progress %.2f", progress)
-					lastProgress = progress
-				}
-			}
-
-			errBuilder.WriteString(data)
-		}
-		if err != nil {
-			break
-		}
-	}
-
-	stdoutData, _ := ioutil.ReadAll(stdout)
-	stdoutString := string(stdoutData)
-
 	registerRunningEncoder(probeResult.Path, cmd.Process)
-	err = waitAndDeregister(probeResult.Path, cmd)
+	err := waitAndDeregister(probeResult.Path, cmd)
 
 	if err != nil {
 		// error message should be in the stderr stream
-		logger.Errorf("ffmpeg error when running command <%s>: %s", strings.Join(cmd.Args, " "), errBuilder.String())
-		return stdoutString, err
+		logger.Errorf("ffmpeg error when running command <%s>: %s", strings.Join(cmd.Args, " "), stderr.String())
+		return stdout.String(), err
 	}
 
-	return stdoutString, nil
+	return stdout.String(), nil
 }
