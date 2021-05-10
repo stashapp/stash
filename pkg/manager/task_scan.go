d1634b17cde7f9fe2bd231f0f5dd76cfb5613d22
UnluckyChemical765 <83972682+UnluckyChemical765@users.noreply.github.com>
2021-05-10 13:51:51 -0700

    Look for funscript during scan
    This is stored in the Scene record and used to drive UI changes for funscript support.
    
    Currently, that's limited to a funscript link in the Scene's file info.

--- pkg/manager/task_scan.go
+++ pkg/manager/task_scan.go
@@ -295,6 +295,12 @@ func (t *ScanTask) getFileModTime() (time.Time, error) {
 	return ret, nil
 }
 
+func (t *ScanTask) getInteractive() bool {
+	_, err := os.Stat(utils.GetFunscriptPath(t.FilePath))
+	return err == nil
+
+}
+
 func (t *ScanTask) isFileModified(fileModTime time.Time, modTime models.NullSQLiteTimestamp) bool {
 	return !modTime.Timestamp.Equal(fileModTime)
 }
@@ -376,6 +382,7 @@ func (t *ScanTask) scanScene() *models.Scene {
 	if err != nil {
 		return logError(err)
 	}
+	interactive := t.getInteractive()
 
 	if s != nil {
 		// if file mod time is not set, set it now
@@ -484,6 +491,20 @@ func (t *ScanTask) scanScene() *models.Scene {
 			}
 		}
 
+		if s.Interactive != interactive {
+			if err := t.TxnManager.WithTxn(context.TODO(), func(r models.Repository) error {
+				qb := r.Scene()
+				scenePartial := models.ScenePartial{
+					ID:   s.ID,
+					Interactive: &interactive,
+				}
+				_, err := qb.Update(scenePartial)
+				return err
+			}); err != nil {
+				return logError(err)
+			}
+		}
+
 		return nil
 	}
 
@@ -551,6 +572,7 @@ func (t *ScanTask) scanScene() *models.Scene {
 			scenePartial := models.ScenePartial{
 				ID:   s.ID,
 				Path: &t.FilePath,
+				Interactive: &interactive,
 			}
 			if err := t.TxnManager.WithTxn(context.TODO(), func(r models.Repository) error {
 				_, err := r.Scene().Update(scenePartial)
@@ -582,6 +604,7 @@ func (t *ScanTask) scanScene() *models.Scene {
 			},
 			CreatedAt: models.SQLiteTimestamp{Timestamp: currentTime},
 			UpdatedAt: models.SQLiteTimestamp{Timestamp: currentTime},
+			Interactive: interactive,
 		}
 
 		if t.UseFileMetadata {
