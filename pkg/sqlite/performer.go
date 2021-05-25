0cd24a9893b487c4c12c95baa733817e752a21a1
peolic <66393006+peolic@users.noreply.github.com>
2021-05-25 03:35:52 +0300

    deprecate instead

--- pkg/sqlite/performer.go
+++ pkg/sqlite/performer.go
@@ -336,6 +336,9 @@ func performerIsMissingCriterionHandler(qb *performerQueryBuilder, isMissing *st
 	return func(f *filterBuilder) {
 		if isMissing != nil && *isMissing != "" {
 			switch *isMissing {
+			case "scenes": // Deprecated: use `scene_count == 0` filter instead
+				f.addJoin(performersScenesTable, "scenes_join", "scenes_join.performer_id = performers.id")
+				f.addWhere("scenes_join.scene_id IS NULL")
 			case "image":
 				f.addJoin(performersImageTable, "image_join", "image_join.performer_id = performers.id")
 				f.addWhere("image_join.performer_id IS NULL")
