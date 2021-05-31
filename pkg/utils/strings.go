4cda4212ef7c28f41e1ae8c51d011a23393e24b3
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2021-05-31 14:10:03 +1000

    Detect case sensitive file moves

--- pkg/utils/strings.go
+++ pkg/utils/strings.go
@@ -1,8 +1,10 @@
 package utils
 
 import (
+	"fmt"
 	"math/rand"
 	"time"
+	"unicode"
 )
 
 var characters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
@@ -14,4 +16,22 @@ func RandomSequence(n int) string {
 		b[i] = characters[rand.Intn(len(characters))]
 	}
 	return string(b)
+}
+
+// FlipCaseSingle flips the case ( lower<->upper ) of a single char from the string s
+// If the string cannot be flipped, the original string value and an error are returned
+func FlipCaseSingle(s string) (string, error) {
+	rr := []rune(s)
+	for i, r := range rr {
+		if unicode.IsLetter(r) { // look for a letter  to flip
+			if unicode.IsUpper(r) {
+				rr[i] = unicode.ToLower(r)
+				return string(rr), nil
+			}
+			rr[i] = unicode.ToUpper(r)
+			return string(rr), nil
+		}
+
+	}
+	return s, fmt.Errorf("could not case flip string %s", s)
 }
