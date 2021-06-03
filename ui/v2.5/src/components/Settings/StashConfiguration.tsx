9fd958480114fefe8533643f56469194dc09442b
Still Hsu <dev@stillu.cc>
2021-06-04 00:19:44 +0800

    Add "add" action strings

--- ui/v2.5/src/components/Settings/StashConfiguration.tsx
+++ ui/v2.5/src/components/Settings/StashConfiguration.tsx
@@ -124,7 +124,7 @@ export const StashConfiguration: React.FC<IStashConfigurationProps> = ({
           variant="secondary"
           onClick={() => setIsDisplayingDialog(true)}
         >
-          Add Directory
+          <FormattedMessage id="actions.add_directory" />
         </Button>
       </Form.Group>
     </>
