58c6a062ddba8ee6213a1659a7fef06f36d9b2c0
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2021-06-16 11:52:21 +1000

    Prevent UI crash on bad hierarchical input value

--- ui/v2.5/src/models/list-filter/criteria/criterion.ts
+++ ui/v2.5/src/models/list-filter/criteria/criterion.ts
@@ -341,14 +341,14 @@ export abstract class IHierarchicalLabeledIdCriterion extends Criterion<IHierarc
 
   protected toCriterionInput(): HierarchicalMultiCriterionInput {
     return {
-      value: this.value.items.map((v) => v.id),
+      value: (this.value.items ?? []).map((v) => v.id),
       modifier: this.modifier,
       depth: this.value.depth,
     };
   }
 
   public getLabelValue(): string {
-    const labels = this.value.items.map((v) => v.label).join(", ");
+    const labels = (this.value.items ?? []).map((v) => v.label).join(", ");
 
     if (this.value.depth === 0) {
       return labels;
