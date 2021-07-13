0826120bfbd8838eca8b4ec597612fce401eef02
Phasetime <87231024+Phasetime@users.noreply.github.com>
2021-07-14 01:27:50 +0200

    initial translation pass

--- ui/v2.5/src/locales/index.ts
+++ ui/v2.5/src/locales/index.ts
@@ -1,8 +1,10 @@
+import deDE from "./de-DE.json";
 import enGB from "./en-GB.json";
 import enUS from "./en-US.json";
 import zhTW from "./zh-TW.json";
 
 export default {
+  deDE,
   enGB,
   enUS,
   zhTW,
