39b6152378bfe545d4224e37deac722206001d3d
Felipe Fernandes <felipefernandes.sjc@hotmail.com>
2021-07-16 10:00:44 -0300

    Portuguese translation

--- ui/v2.5/src/locales/index.ts
+++ ui/v2.5/src/locales/index.ts
@@ -1,9 +1,11 @@
 import enGB from "./en-GB.json";
 import enUS from "./en-US.json";
+import ptBR from "./pt-BR.json";
 import zhTW from "./zh-TW.json";
 
 export default {
   enGB,
   enUS,
+  ptBR,
   zhTW,
 };
