5e4a9651e51d2a9610292cb140ad256b30ea546b
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2021-08-02 11:37:13 +1000

    Refactor UI code

--- ui/v2.5/src/models/list-filter/criteria/resolution.ts
+++ ui/v2.5/src/models/list-filter/criteria/resolution.ts
@@ -1,80 +1,20 @@
 import {
   ResolutionCriterionInput,
-  ResolutionEnum,
   CriterionModifier,
 } from "src/core/generated-graphql";
+import { stringToResolution, resolutionStrings } from "src/utils/resolution";
 import { CriterionType } from "../types";
 import { CriterionOption, StringCriterion } from "./criterion";
 
 abstract class AbstractResolutionCriterion extends StringCriterion {
   protected toCriterionInput(): ResolutionCriterionInput | undefined {
-    switch (this.value) {
-      case "144p":
-        return {
-          value: ResolutionEnum.VeryLow,
-          modifier: this.modifier,
-        };
-      case "240p":
-        return {
-          value: ResolutionEnum.Low,
-          modifier: this.modifier,
-        };
-      case "360p":
-        return {
-          value: ResolutionEnum.R360P,
-          modifier: this.modifier,
-        };
-      case "480p":
-        return {
-          value: ResolutionEnum.Standard,
-          modifier: this.modifier,
-        };
-      case "540p":
-        return {
-          value: ResolutionEnum.WebHd,
-          modifier: this.modifier,
-        };
-      case "720p":
-        return {
-          value: ResolutionEnum.StandardHd,
-          modifier: this.modifier,
-        };
-      case "1080p":
-        return {
-          value: ResolutionEnum.FullHd,
-          modifier: this.modifier,
-        };
-      case "1440p":
-        return {
-          value: ResolutionEnum.QuadHd,
-          modifier: this.modifier,
-        };
-      case "1920p":
-        return {
-          value: ResolutionEnum.VrHd,
-          modifier: this.modifier,
-        };
-      case "4k":
-        return {
-          value: ResolutionEnum.FourK,
-          modifier: this.modifier,
-        };
-      case "5k":
-        return {
-          value: ResolutionEnum.FiveK,
-          modifier: this.modifier,
-        };
-      case "6k":
-        return {
-          value: ResolutionEnum.SixK,
-          modifier: this.modifier,
-        };
-      case "8k":
-        return {
-          value: ResolutionEnum.EightK,
-          modifier: this.modifier,
-        };
-      // no default
+    const value = stringToResolution(this.value);
+
+    if (value !== undefined) {
+      return {
+        value,
+        modifier: this.modifier,
+      };
     }
   }
 }
@@ -91,20 +31,7 @@ class ResolutionCriterionOptionType extends CriterionOption {
         CriterionModifier.GreaterThan,
         CriterionModifier.LessThan,
       ],
-      options: [
-        "144p",
-        "240p",
-        "360p",
-        "480p",
-        "540p",
-        "720p",
-        "1080p",
-        "1440p",
-        "4k",
-        "5k",
-        "6k",
-        "8k",
-      ],
+      options: resolutionStrings,
     });
   }
 }
