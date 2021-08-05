e5dd962b00e0934f77b961601a9020667ea64e6b
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2021-08-05 13:47:25 +1000

    Initialise box files at startup

--- pkg/api/images.go
+++ pkg/api/images.go
@@ -4,35 +4,50 @@ import (
 	"strings"
 
 	"github.com/gobuffalo/packr/v2"
+	"github.com/stashapp/stash/pkg/logger"
+	"github.com/stashapp/stash/pkg/manager/config"
 	"github.com/stashapp/stash/pkg/utils"
 )
 
-var performerBox *packr.Box
-var performerBoxMale *packr.Box
-var performerBoxCustom *packr.Box
+type imageBox struct {
+	box   *packr.Box
+	files []string
+}
+
+func newImageBox(box *packr.Box) *imageBox {
+	return &imageBox{
+		box:   box,
+		files: box.List(),
+	}
+}
+
+var performerBox *imageBox
+var performerBoxMale *imageBox
+var performerBoxCustom *imageBox
 
 func initialiseImages() {
-	performerBox = packr.New("Performer Box", "../../static/performer")
-	performerBoxMale = packr.New("Male Performer Box", "../../static/performer_male")
+	performerBox = newImageBox(packr.New("Performer Box", "../../static/performer"))
+	performerBoxMale = newImageBox(packr.New("Male Performer Box", "../../static/performer_male"))
+	initialiseCustomImages()
+}
+
+func initialiseCustomImages() {
+	customPath := config.GetInstance().GetCustomPerformerImageLocation()
+	if customPath != "" {
+		logger.Debugf("Loading custom performer images from %s", customPath)
+		// We need to set performerBoxCustom at runtime, as this is a custom path, and store it in a pointer.
+		performerBoxCustom = newImageBox(packr.New("Custom Performer Box", config.GetInstance().GetCustomPerformerImageLocation()))
+	} else {
+		performerBoxCustom = nil
+	}
 }
 
 func getRandomPerformerImageUsingName(name, gender, customPath string) ([]byte, error) {
-	var box *packr.Box
+	var box *imageBox
 
 	// If we have a custom path, we should return a new box in the given path.
-	if customPath != "" {
-		if performerBoxCustom != nil {
-			box = performerBoxCustom
-		} else {
-			// We need to set performerBoxCustom at runtime, as this is a custom path, and store it in a pointer.
-			newBox := packr.New("Custom Performer Box", customPath)
-
-			// only grab from this box if it is not empty
-			if len(newBox.List()) > 0 {
-				performerBoxCustom = newBox
-				box = newBox
-			}
-		}
+	if performerBoxCustom != nil && len(performerBoxCustom.files) > 0 {
+		box = performerBoxCustom
 	}
 
 	if box == nil {
@@ -46,7 +61,7 @@ func getRandomPerformerImageUsingName(name, gender, customPath string) ([]byte, 
 		}
 	}
 
-	imageFiles := box.List()
+	imageFiles := box.files
 	index := utils.IntFromString(name) % uint64(len(imageFiles))
-	return box.Find(imageFiles[index])
+	return box.box.Find(imageFiles[index])
 }
