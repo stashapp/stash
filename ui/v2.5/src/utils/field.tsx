63ee5440ebcf4a432bf42ba219aa82a06d17dac7
gitgiggety <gitgiggety@outlook.com>
2021-08-06 17:19:28 +0200

    Rebuild SceneFileInfoPanel
    Completely rebuild the SceneFileInfoPanel to make use of the `TextField`
    and `URLField` components. Using these components means that the URLs
    automatically get `target="_blank" rel="noopener noreferrer"`.
    Furhermore this should also improve the styling a bit, as described in
    the previous commit.

--- ui/v2.5/src/utils/field.tsx
+++ ui/v2.5/src/utils/field.tsx
@@ -1,20 +1,37 @@
 import React from "react";
 import { FormattedMessage } from "react-intl";
+import { TruncatedText } from "../components/Shared";
 
 interface ITextField {
   id?: string;
   name?: string;
+  abbr?: string | null;
   value?: string | null;
+  truncate?: boolean | null;
 }
 
-export const TextField: React.FC<ITextField> = ({ id, name, value }) => {
-  if (!value) {
+export const TextField: React.FC<ITextField> = ({
+  id,
+  name,
+  value,
+  abbr,
+  truncate,
+  children,
+}) => {
+  if (!value && !children) {
     return null;
   }
+
+  const message = (
+    <>{id ? <FormattedMessage id={id} defaultMessage={name} /> : name}:</>
+  );
+
   return (
     <>
-      <dt>{id ? <FormattedMessage id={id} defaultMessage={name} /> : name}:</dt>
-      <dd>{value ?? undefined}</dd>
+      <dt>{abbr ? <abbr title={abbr}>{message}</abbr> : message}</dt>
+      <dd>
+        {value ? truncate ? <TruncatedText text={value} /> : value : children}
+      </dd>
     </>
   );
 };
@@ -22,21 +39,44 @@ export const TextField: React.FC<ITextField> = ({ id, name, value }) => {
 interface IURLField {
   id?: string;
   name?: string;
+  abbr?: string | null;
   value?: string | null;
   url?: string | null;
+  truncate?: boolean | null;
 }
 
-export const URLField: React.FC<IURLField> = ({ id, name, value, url }) => {
-  if (!value) {
+export const URLField: React.FC<IURLField> = ({
+  id,
+  name,
+  value,
+  url,
+  abbr,
+  truncate,
+  children,
+}) => {
+  if (!value && !children) {
     return null;
   }
+
+  const message = (
+    <>{id ? <FormattedMessage id={id} defaultMessage={name} /> : name}:</>
+  );
+
   return (
     <>
-      <dt>{id ? <FormattedMessage id={id} defaultMessage={name} /> : name}:</dt>
+      <dt>{abbr ? <abbr title={abbr}>{message}</abbr> : message}</dt>
       <dd>
         {url ? (
           <a href={url} target="_blank" rel="noopener noreferrer">
-            {value}
+            {value ? (
+              truncate ? (
+                <TruncatedText text={value} />
+              ) : (
+                value
+              )
+            ) : (
+              children
+            )}
           </a>
         ) : undefined}
       </dd>
