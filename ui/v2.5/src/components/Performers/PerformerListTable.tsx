4c144db5107302cb90adf6d4dd3ad1544d335060
gitgiggety <79809426+gitgiggety@users.noreply.github.com>
2021-08-26 01:41:18 +0200

    Performers listing improvements (#1671)
    * Add sorting on image and gallery count to performers
    * Make performer table headers translatable
    * Add image and gallery count to performers table
    * Make sure list table container fits the table
    
    Make the table container minimally as wide as the table. This fixes the
    table "overflowing" to the left and right and the left not being
    accessible.
    
    * Remove unnecessary truncation in tables
    
    IMO there is no need to truncate the title in the scenes table and the
    name in the performers table. This because both tables also contain an
    image which means that multiple lines should be possible without really
    extending the height of the row. Furthermore both tables contain other
    values which might be way longer and also aren't wrapped (like tags and
    performers for scenes, and aliases for performers).

--- ui/v2.5/src/components/Performers/PerformerListTable.tsx
+++ ui/v2.5/src/components/Performers/PerformerListTable.tsx
@@ -1,10 +1,11 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
 
 import React from "react";
+import { useIntl } from "react-intl";
 import { Button, Table } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import * as GQL from "src/core/generated-graphql";
-import { Icon, TruncatedText } from "src/components/Shared";
+import { Icon } from "src/components/Shared";
 import { NavUtils } from "src/utils";
 
 interface IPerformerListTableProps {
@@ -14,6 +15,8 @@ interface IPerformerListTableProps {
 export const PerformerListTable: React.FC<IPerformerListTableProps> = (
   props: IPerformerListTableProps
 ) => {
+  const intl = useIntl();
+
   const renderPerformerRow = (performer: GQL.PerformerDataFragment) => (
     <tr key={performer.id}>
       <td>
@@ -27,9 +30,7 @@ export const PerformerListTable: React.FC<IPerformerListTableProps> = (
       </td>
       <td className="text-left">
         <Link to={`/performers/${performer.id}`}>
-          <h5>
-            <TruncatedText text={performer.name} />
-          </h5>
+          <h5>{performer.name}</h5>
         </Link>
       </td>
       <td>{performer.aliases ? performer.aliases : ""}</td>
@@ -45,6 +46,16 @@ export const PerformerListTable: React.FC<IPerformerListTableProps> = (
           <h6>{performer.scene_count}</h6>
         </Link>
       </td>
+      <td>
+        <Link to={NavUtils.makePerformerImagesUrl(performer)}>
+          <h6>{performer.image_count}</h6>
+        </Link>
+      </td>
+      <td>
+        <Link to={NavUtils.makePerformerGalleriesUrl(performer)}>
+          <h6>{performer.gallery_count}</h6>
+        </Link>
+      </td>
       <td>{performer.birthdate}</td>
       <td>{performer.height}</td>
     </tr>
@@ -56,12 +67,14 @@ export const PerformerListTable: React.FC<IPerformerListTableProps> = (
         <thead>
           <tr>
             <th />
-            <th>Name</th>
-            <th>Aliases</th>
-            <th>Favourite</th>
-            <th>Scene Count</th>
-            <th>Birthdate</th>
-            <th>Height</th>
+            <th>{intl.formatMessage({ id: "name" })}</th>
+            <th>{intl.formatMessage({ id: "aliases" })}</th>
+            <th>{intl.formatMessage({ id: "favourite" })}</th>
+            <th>{intl.formatMessage({ id: "scene_count" })}</th>
+            <th>{intl.formatMessage({ id: "image_count" })}</th>
+            <th>{intl.formatMessage({ id: "gallery_count" })}</th>
+            <th>{intl.formatMessage({ id: "birthdate" })}</th>
+            <th>{intl.formatMessage({ id: "height" })}</th>
           </tr>
         </thead>
         <tbody>{props.performers.map(renderPerformerRow)}</tbody>
