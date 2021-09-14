11690a0f70dbad63a3b46db301bedc2017628f31
SmallCoccinelle <89733524+SmallCoccinelle@users.noreply.github.com>
2021-09-14 13:10:40 +0200

    Remove utils.Btoi
    There are no users of this utility function

--- pkg/utils/boolean.go
+++ pkg/utils/boolean.go
@@ -1,13 +1,5 @@
 package utils
 
-// Btoi transforms a boolean to an int.  1 for true, false otherwise
-func Btoi(b bool) int {
-	if b {
-		return 1
-	}
-	return 0
-}
-
 // IsTrue returns true if the bool pointer is not nil and true.
 func IsTrue(b *bool) bool {
 	return b != nil && *b
