66f92c5dccaed02ef0a6caa2669ad0e890dcf929
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2021-09-17 09:23:56 +1000

    Fix autotag tag alias (#1713)
    * Add unit tests for tag alias
    * Fix tag alias autotag
    * Don't autotag non-path galleries
    * Add studio alias tests

--- pkg/autotag/gallery_test.go
+++ pkg/autotag/gallery_test.go
@@ -74,13 +74,7 @@ func TestGalleryStudios(t *testing.T) {
 
 	assert := assert.New(t)
 
-	for _, test := range testTables {
-		mockStudioReader := &mocks.StudioReaderWriter{}
-		mockGalleryReader := &mocks.GalleryReaderWriter{}
-
-		mockStudioReader.On("QueryForAutoTag", mock.Anything).Return([]*models.Studio{&studio, &reversedStudio}, nil).Once()
-		mockStudioReader.On("GetAliases", mock.Anything).Return([]string{}, nil).Maybe()
-
+	doTest := func(mockStudioReader *mocks.StudioReaderWriter, mockGalleryReader *mocks.GalleryReaderWriter, test pathTestTable) {
 		if test.Matches {
 			mockGalleryReader.On("Find", galleryID).Return(&models.Gallery{}, nil).Once()
 			expectedStudioID := models.NullInt64(studioID)
@@ -99,6 +93,33 @@ func TestGalleryStudios(t *testing.T) {
 		assert.Nil(err)
 		mockStudioReader.AssertExpectations(t)
 		mockGalleryReader.AssertExpectations(t)
+	}
+
+	for _, test := range testTables {
+		mockStudioReader := &mocks.StudioReaderWriter{}
+		mockGalleryReader := &mocks.GalleryReaderWriter{}
+
+		mockStudioReader.On("QueryForAutoTag", mock.Anything).Return([]*models.Studio{&studio, &reversedStudio}, nil).Once()
+		mockStudioReader.On("GetAliases", mock.Anything).Return([]string{}, nil).Maybe()
+
+		doTest(mockStudioReader, mockGalleryReader, test)
+	}
+
+	// test against aliases
+	const unmatchedName = "unmatched"
+	studio.Name.String = unmatchedName
+
+	for _, test := range testTables {
+		mockStudioReader := &mocks.StudioReaderWriter{}
+		mockGalleryReader := &mocks.GalleryReaderWriter{}
+
+		mockStudioReader.On("QueryForAutoTag", mock.Anything).Return([]*models.Studio{&studio, &reversedStudio}, nil).Once()
+		mockStudioReader.On("GetAliases", studioID).Return([]string{
+			studioName,
+		}, nil).Once()
+		mockStudioReader.On("GetAliases", reversedStudioID).Return([]string{}, nil).Once()
+
+		doTest(mockStudioReader, mockGalleryReader, test)
 	}
 }
 
@@ -122,12 +143,7 @@ func TestGalleryTags(t *testing.T) {
 
 	assert := assert.New(t)
 
-	for _, test := range testTables {
-		mockTagReader := &mocks.TagReaderWriter{}
-		mockGalleryReader := &mocks.GalleryReaderWriter{}
-
-		mockTagReader.On("QueryForAutoTag", mock.Anything).Return([]*models.Tag{&tag, &reversedTag}, nil).Once()
-
+	doTest := func(mockTagReader *mocks.TagReaderWriter, mockGalleryReader *mocks.GalleryReaderWriter, test pathTestTable) {
 		if test.Matches {
 			mockGalleryReader.On("GetTagIDs", galleryID).Return(nil, nil).Once()
 			mockGalleryReader.On("UpdateTags", galleryID, []int{tagID}).Return(nil).Once()
@@ -142,5 +158,31 @@ func TestGalleryTags(t *testing.T) {
 		assert.Nil(err)
 		mockTagReader.AssertExpectations(t)
 		mockGalleryReader.AssertExpectations(t)
+	}
+
+	for _, test := range testTables {
+		mockTagReader := &mocks.TagReaderWriter{}
+		mockGalleryReader := &mocks.GalleryReaderWriter{}
+
+		mockTagReader.On("QueryForAutoTag", mock.Anything).Return([]*models.Tag{&tag, &reversedTag}, nil).Once()
+		mockTagReader.On("GetAliases", mock.Anything).Return([]string{}, nil).Maybe()
+
+		doTest(mockTagReader, mockGalleryReader, test)
+	}
+
+	const unmatchedName = "unmatched"
+	tag.Name = unmatchedName
+
+	for _, test := range testTables {
+		mockTagReader := &mocks.TagReaderWriter{}
+		mockGalleryReader := &mocks.GalleryReaderWriter{}
+
+		mockTagReader.On("QueryForAutoTag", mock.Anything).Return([]*models.Tag{&tag, &reversedTag}, nil).Once()
+		mockTagReader.On("GetAliases", tagID).Return([]string{
+			tagName,
+		}, nil).Once()
+		mockTagReader.On("GetAliases", reversedTagID).Return([]string{}, nil).Once()
+
+		doTest(mockTagReader, mockGalleryReader, test)
 	}
 }
