dac4ba8a99b4ceed364f2567b52fe3102ed02329
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2021-09-22 10:31:11 +1000

    Remove remaining packr references

--- scripts/cross-compile.sh
+++ scripts/cross-compile.sh
@@ -8,8 +8,7 @@ GITHASH=`git rev-parse --short HEAD`
 STASH_VERSION=`git describe --tags --exclude latest_develop`
 
 SETENV="BUILD_DATE=\"$BUILD_DATE\" GITHASH=$GITHASH STASH_VERSION=\"$STASH_VERSION\""
-SETUP="export CGO_ENABLED=1; set -e; echo '=== Running packr ==='; make packr;"
-SETUP_FAST="export CGO_ENABLED=1;"
+SETUP="export CGO_ENABLED=1;"
 WINDOWS="echo '=== Building Windows binary ==='; $SETENV make cross-compile-windows;"
 DARWIN="echo '=== Building OSX binary ==='; $SETENV make cross-compile-osx-intel;"
 DARWIN-ARM64="echo '=== Building OSX (arm64) binary ==='; $SETENV make cross-compile-osx-applesilicon;"
@@ -18,14 +17,6 @@ LINUX_ARM64v8="echo '=== Building Linux (armv8/arm64) binary ==='; $SETENV make 
 LINUX_ARM32v7="echo '=== Building Linux (armv7/armhf) binary ==='; $SETENV make cross-compile-linux-arm32v7;"
 LINUX_ARM32v6="echo '=== Building Linux (armv6 | Raspberry Pi 1) binary ==='; $SETENV make cross-compile-pi;"
 BUILD_COMPLETE="echo '=== Build complete ==='"
-
-# if build target ends with -fast then use prebuilt packr2. eg amd64-fast or all-fast
-FAST=`echo "$1" | cut -d - -f 2`
-if [ "$FAST" == "fast" ]
-then
-  echo "Building without Packr2"
-  SETUP=$SETUP_FAST
-fi
 
 BUILD=`echo "$1" | cut -d - -f 1`
 if [ "$BUILD" == "windows" ]
