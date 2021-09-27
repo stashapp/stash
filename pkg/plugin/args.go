028f6b30d7d130782e9a7409545bd5c5379d4699
SmallCoccinelle <89733524+SmallCoccinelle@users.noreply.github.com>
2021-09-27 16:40:19 +0200

    Enable the exportloopref linter
    There are two places in the code with these warnings. Fix them while
    enabling the linter.

--- pkg/plugin/args.go
+++ pkg/plugin/args.go
@@ -17,6 +17,7 @@ func findArg(args []*models.PluginArgInput, name string) *models.PluginArgInput 
 func applyDefaultArgs(args []*models.PluginArgInput, defaultArgs map[string]string) []*models.PluginArgInput {
 	for k, v := range defaultArgs {
 		if arg := findArg(args, k); arg == nil {
+			v := v // Copy v, because it's being exported out of the loop
 			args = append(args, &models.PluginArgInput{
 				Key: k,
 				Value: &models.PluginValueInput{
