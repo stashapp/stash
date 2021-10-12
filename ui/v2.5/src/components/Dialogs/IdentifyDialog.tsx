257fecb0c444ecfceea4529e7a61b7bbbd51e9a1
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2021-10-12 15:59:54 +1100

    Support rearranging source list

--- ui/v2.5/src/components/Dialogs/IdentifyDialog.tsx
+++ ui/v2.5/src/components/Dialogs/IdentifyDialog.tsx
@@ -60,7 +60,7 @@ const OptionsEditor: React.FC<IOptionsEditor> = ({
   function cycleState(existingState: boolean | undefined) {
     if (existingState) {
       return undefined;
-    } 
+    }
     if (existingState === undefined) {
       return false;
     }
@@ -214,26 +214,76 @@ const SourcesList: React.FC<ISourcesList> = ({
   editSource,
   canAdd,
 }) => {
+  const [tempSources, setTempSources] = useState(sources);
+  const [dragIndex, setDragIndex] = useState<number | undefined>();
+  const [mouseOverIndex, setMouseOverIndex] = useState<number | undefined>();
+
+  useEffect(() => {
+    setTempSources([...sources]);
+  }, [sources]);
+
   function removeSource(index: number) {
     const newSources = [...sources];
     newSources.splice(index, 1);
     setSources(newSources);
   }
 
+  function onDragStart(event: React.DragEvent<HTMLElement>, index: number) {
+    event.dataTransfer.setData("text/plain", "");
+    event.dataTransfer.setDragImage(new Image(), 0, 0);
+    event.dataTransfer.effectAllowed = "move";
+    // event.dataTransfer.dropEffect = "move";
+    setDragIndex(index);
+  }
+
+  function onDragOver(event: React.DragEvent<HTMLElement>, index?: number) {
+    if (dragIndex !== undefined && index !== undefined && index !== dragIndex) {
+      const newSources = [...tempSources];
+      const moved = newSources.splice(dragIndex, 1);
+      newSources.splice(index, 0, moved[0]);
+      setTempSources(newSources);
+      setDragIndex(index);
+    }
+
+    event.dataTransfer.dropEffect = "move";
+    event.preventDefault();
+  }
+
+  function onDragOverDefault(event: React.DragEvent<HTMLDivElement>) {
+    event.dataTransfer.dropEffect = "move";
+    event.preventDefault();
+  }
+
+  function onDrop() {
+    // assume we've already set the temp source list
+    // feed it up
+    setSources(tempSources);
+    setDragIndex(undefined);
+    setMouseOverIndex(undefined);
+  }
+
   return (
-    <Form.Group className="scraper-sources">
+    <Form.Group className="scraper-sources" onDragOver={onDragOverDefault}>
       <h5>
         <FormattedMessage id="config.tasks.identify.sources" />
       </h5>
       <ListGroup as="ul" className="scraper-source-list">
-        {sources.map((s, index) => (
+        {tempSources.map((s, index) => (
           <ListGroup.Item
             as="li"
             key={s.id}
             className="d-flex justify-content-between"
+            draggable={mouseOverIndex === index}
+            onDragStart={(e) => onDragStart(e, index)}
+            onDragEnter={(e) => onDragOver(e, index)}
+            onDrop={() => onDrop()}
           >
             <div>
-              <Button className="minimal text-muted drag-handle">
+              <Button
+                className="minimal text-muted drag-handle"
+                onMouseEnter={() => setMouseOverIndex(index)}
+                onMouseLeave={() => setMouseOverIndex(undefined)}
+              >
                 <Icon icon="grip-lines" />
               </Button>
               {s.displayName}
