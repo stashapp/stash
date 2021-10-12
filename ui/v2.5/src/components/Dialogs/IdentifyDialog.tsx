58f3aa2a64991921698775a0e7ed13629357bc2a
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2021-10-13 08:23:45 +1100

    WIP

--- ui/v2.5/src/components/Dialogs/IdentifyDialog.tsx
+++ ui/v2.5/src/components/Dialogs/IdentifyDialog.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useMemo } from "react";
-import { Form, Button, ListGroup } from "react-bootstrap";
+import { Form, Button, ListGroup, Row, Col } from "react-bootstrap";
 import {
   mutateMetadataIdentify,
   useConfiguration,
@@ -20,29 +20,249 @@ interface IScraperSource {
 }
 
 interface IFieldOptionsEditor {
+  availableFields: string[];
   options: GQL.IdentifyFieldOptions;
-  setOptions: (o: GQL.IdentifyFieldOptions) => void;
+  editOptions: (o?: GQL.IdentifyFieldOptions) => void;
+  removeField: () => void;
+  editing: boolean;
 }
 
-const FieldOptionsEditor: React.FC<IFieldOptionsEditor> = ({}) => <div></div>;
+const FieldOptionsEditor: React.FC<IFieldOptionsEditor> = ({availableFields, options, removeField, editOptions, editing}) => {
+  const intl = useIntl();
+
+  const [localOptions, setLocalOptions] = useState(options);
+
+  useEffect(() => {
+    setLocalOptions(options);
+  }, [options]);
+
+  function renderFieldSelect() {
+    return (
+      <Form.Group>
+        <Form.Label>
+          Field
+        </Form.Label>
+        <Form.Control
+          disabled={!editing}
+          className="w-auto input-control"
+          as="select"
+          value={localOptions.field}
+          onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
+            setLocalOptions({...localOptions, field: e.currentTarget.value})
+          }
+        >
+          {availableFields.map((f) => (
+            <option key={f} value={f}>
+              {f}
+            </option>
+          ))}
+        </Form.Control>
+      </Form.Group>
+    );
+  }
+
+  function renderStrategySelect() {
+    const strategyStrings = Object.keys(GQL.IdentifyFieldStrategy);
+
+    return (
+      <Form.Group>
+        <Form.Label>
+          Strategy
+        </Form.Label>
+        <Form.Control
+          disabled={!editing}
+          className="w-auto input-control"
+          as="select"
+          value={localOptions.strategy}
+          onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
+            setLocalOptions({...localOptions, strategy: e.currentTarget.value as GQL.IdentifyFieldStrategy})
+          }
+        >
+          {strategyStrings.map((f) => (
+            <option key={f} value={f}>
+              {f}
+            </option>
+          ))}
+        </Form.Control>
+      </Form.Group>
+    )
+  }
+
+  function maybeRenderCreateMissing() {
+    const createMissingFields = ["studio", "performers", "tags"];
+
+    if (createMissingFields.includes(localOptions.field)) {
+      return (
+        <ThreeStateCheckbox
+          value={localOptions.createMissing === null ? undefined : localOptions.createMissing}
+          setValue={(v) => setLocalOptions({...localOptions, createMissing: v})}
+          label={intl.formatMessage({id: "config.tasks.identify.create_missing"})}
+        />
+      )
+    }
+  }
+
+  function render() {
+    return (
+      <Row className="mx-2 align-items-center">
+        <Col sm={3}>
+          {renderFieldSelect()}
+        </Col>
+        <Col sm={3}>
+          {renderStrategySelect()}
+        </Col>
+        <Col sm={3}>
+          {maybeRenderCreateMissing()}
+        </Col>
+        
+        <div className="col-3 d-flex justify-content-end">
+          {editing ? (
+            <>
+              <Button
+                className="minimal text-success"
+                onClick={() => editOptions(localOptions)}
+              >
+                <Icon icon="check" />
+              </Button>
+              <Button
+                className="minimal text-danger"
+                onClick={() => editOptions()}
+              >
+                <Icon icon="times" />
+              </Button>
+            </>
+          ) : (
+            <Button
+              className="minimal text-danger"
+              onClick={() => removeField()}
+            >
+              <Icon icon="minus" />
+            </Button>
+          )}
+        </div>
+      </Row>
+    )
+  }
+  
+  return (
+    <ListGroup.Item
+      as="li"
+      key={options.field}
+    >
+      {render()}
+    </ListGroup.Item>
+  )
+};
 
 interface IFieldOptionsList {
   fieldOptions?: GQL.IdentifyFieldOptions[];
   setFieldOptions: (o: GQL.IdentifyFieldOptions[]) => void;
+  setEditingField: (v: boolean) => void;
 }
 
-const FieldOptionsList: React.FC<IFieldOptionsList> = ({}) => <div></div>;
+const FieldOptionsList: React.FC<IFieldOptionsList> = ({fieldOptions, setFieldOptions, setEditingField}) => {
+  const [localFieldOptions, setLocalFieldOptions] = useState<GQL.IdentifyFieldOptions[]>([]);
+  const [editField, setEditField] = useState<GQL.IdentifyFieldOptions | undefined>();
+
+  useEffect(() => {
+    if (fieldOptions) {
+      setLocalFieldOptions([...fieldOptions]);
+    }
+  }, [fieldOptions]);
+
+  const allFields = [
+    "title",
+    "date",
+    "details",
+    "url",
+    "studio",
+    "performers",
+    "tags",
+    "stash_ids",
+  ];
+
+  const availableFields = useMemo(() => {
+    return allFields.filter(f => !localFieldOptions?.some(o => o !== editField && o.field === f));
+  }, [localFieldOptions]);
+
+  function onAdd() {
+    const newOptions = [ ...localFieldOptions ];
+    const newOption = {
+      field: availableFields[0],
+      strategy: GQL.IdentifyFieldStrategy.Ignore,
+    };
+    newOptions.push(newOption);
+    setLocalFieldOptions(newOptions);
+    setEditField(newOption);
+    setEditingField(true);
+  }
+
+  function handleEditOptions(o?: GQL.IdentifyFieldOptions) {
+    if (!o) {
+      if (localFieldOptions.length > (fieldOptions?.length ?? 0)) {
+        // must be new field option. remove it
+        const newOptions = [ ...localFieldOptions ];
+        newOptions.pop();
+        setLocalFieldOptions(newOptions);
+      }
+    } else {
+      const newOptions = [ ...localFieldOptions ];
+      newOptions.splice(newOptions.indexOf(editField!), 1, o);
+      setFieldOptions(newOptions);
+    }
+
+    setEditField(undefined);
+    setEditingField(false);
+  }
+
+  function removeField(index: number) {
+    const newOptions = [ ...localFieldOptions ];
+    newOptions.splice(index, 1);
+    setFieldOptions(newOptions);
+  }
+
+  return (
+    <Form.Group className="scraper-sources">
+      <h5>
+        <FormattedMessage id="config.tasks.identify.field_options" />
+      </h5>
+      <ListGroup as="ul" className="scraper-source-list">
+        {localFieldOptions?.map((s, index) => (
+          <FieldOptionsEditor 
+            availableFields={availableFields}
+            options={s} 
+            removeField={() => removeField(index)} 
+            editOptions={handleEditOptions} 
+            editing={s === editField}
+          />
+        ))}
+      </ListGroup>
+      {!editField && availableFields.length > 0 ? (
+        <div className="text-right">
+          <Button
+            className="minimal add-scraper-source-button"
+            onClick={() => onAdd()}
+          >
+            <Icon icon="plus" />
+          </Button>
+        </div>
+      ): undefined}
+    </Form.Group>
+  )
+};
 
 interface IOptionsEditor {
   options: GQL.IdentifyMetadataOptionsInput;
   setOptions: (s: GQL.IdentifyMetadataOptionsInput) => void;
   source?: IScraperSource;
+  setEditingField: (v: boolean) => void;
 }
 
 const OptionsEditor: React.FC<IOptionsEditor> = ({
   options,
   setOptions: setOptionsState,
   source,
+  setEditingField,
 }) => {
   const intl = useIntl();
 
@@ -66,52 +286,55 @@ const OptionsEditor: React.FC<IOptionsEditor> = ({
           values={{ source: source?.displayName }}
         />
       </h5>
-      <ThreeStateCheckbox
-        value={
-          options.includeMalePerformers === null
-            ? undefined
-            : options.includeMalePerformers
-        }
-        setValue={(v) =>
-          setOptions({
-            includeMalePerformers: v,
-          })
-        }
-        label={intl.formatMessage({
-          id: "config.tasks.identify.include_male_performers",
-        })}
-        {...checkboxProps}
-      />
-      <ThreeStateCheckbox
-        value={
-          options.setCoverImage === null ? undefined : options.setCoverImage
-        }
-        setValue={(v) =>
-          setOptions({
-            setCoverImage: v,
-          })
-        }
-        label={intl.formatMessage({
-          id: "config.tasks.identify.set_cover_images",
-        })}
-        {...checkboxProps}
-      />
-      <ThreeStateCheckbox
-        value={options.setOrganized === null ? undefined : options.setOrganized}
-        setValue={(v) =>
-          setOptions({
-            setOrganized: v,
-          })
-        }
-        label={intl.formatMessage({
-          id: "config.tasks.identify.set_organized",
-        })}
-        {...checkboxProps}
-      />
+      <Form.Group>
+        <ThreeStateCheckbox
+          value={
+            options.includeMalePerformers === null
+              ? undefined
+              : options.includeMalePerformers
+          }
+          setValue={(v) =>
+            setOptions({
+              includeMalePerformers: v,
+            })
+          }
+          label={intl.formatMessage({
+            id: "config.tasks.identify.include_male_performers",
+          })}
+          {...checkboxProps}
+        />
+        <ThreeStateCheckbox
+          value={
+            options.setCoverImage === null ? undefined : options.setCoverImage
+          }
+          setValue={(v) =>
+            setOptions({
+              setCoverImage: v,
+            })
+          }
+          label={intl.formatMessage({
+            id: "config.tasks.identify.set_cover_images",
+          })}
+          {...checkboxProps}
+        />
+        <ThreeStateCheckbox
+          value={options.setOrganized === null ? undefined : options.setOrganized}
+          setValue={(v) =>
+            setOptions({
+              setOrganized: v,
+            })
+          }
+          label={intl.formatMessage({
+            id: "config.tasks.identify.set_organized",
+          })}
+          {...checkboxProps}
+        />
+      </Form.Group>
 
       <FieldOptionsList
         fieldOptions={options.fieldOptions ?? undefined}
         setFieldOptions={(o) => setOptions({ fieldOptions: o })}
+        setEditingField={setEditingField}
       />
 
       {!source && (
@@ -139,6 +362,7 @@ const SourcesEditor: React.FC<ISourceEditor> = ({
   saveSource,
 }) => {
   const [source, setSource] = useState<IScraperSource>(initialSource);
+  const [editingField, setEditingField] = useState(false);
 
   const intl = useIntl();
 
@@ -182,7 +406,7 @@ const SourcesEditor: React.FC<ISourceEditor> = ({
         text: intl.formatMessage({ id: "actions.cancel" }),
         variant: "secondary",
       }}
-      disabled={!source.scraper_id && !source.stash_box_endpoint}
+      disabled={(!source.scraper_id && !source.stash_box_endpoint) || editingField}
     >
       <Form>
         {isNew && (
@@ -208,6 +432,7 @@ const SourcesEditor: React.FC<ISourceEditor> = ({
           options={source.options ?? {}}
           setOptions={(o) => setSource({ ...source, options: o })}
           source={source}
+          setEditingField={(v) => setEditingField(v)}
         />
       </Form>
     </Modal>
@@ -352,6 +577,7 @@ export const IdentifyDialog: React.FC<IIdentifyDialogProps> = ({
     IScraperSource | undefined
   >();
   const [animation, setAnimation] = useState(true);
+  const [editingField, setEditingField] = useState(false);
 
   const intl = useIntl();
   const Toast = useToast();
@@ -493,7 +719,7 @@ export const IdentifyDialog: React.FC<IIdentifyDialogProps> = ({
 
   return (
     <Modal
-      modalProps={{ animation }}
+      modalProps={{ animation, size: "lg" }}
       show
       icon="cogs"
       header={intl.formatMessage({ id: "actions.identify" })}
@@ -506,6 +732,7 @@ export const IdentifyDialog: React.FC<IIdentifyDialogProps> = ({
         text: intl.formatMessage({ id: "actions.cancel" }),
         variant: "secondary",
       }}
+      disabled={editingField}
     >
       <Form>
         <SourcesList
@@ -514,7 +741,11 @@ export const IdentifyDialog: React.FC<IIdentifyDialogProps> = ({
           editSource={onEditSource}
           canAdd={sources.length < allSources.length}
         />
-        <OptionsEditor options={options} setOptions={(o) => setOptions(o)} />
+        <OptionsEditor 
+          options={options} 
+          setOptions={(o) => setOptions(o)} 
+          setEditingField={(v) => setEditingField(v)} 
+        />
       </Form>
     </Modal>
   );
