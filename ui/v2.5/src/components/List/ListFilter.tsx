95e11d4aa8f61c6531cf774e2b3bbc76dd33f89f
7dJx1qP <38586902+7dJx1qP@users.noreply.github.com>
2021-10-14 23:42:32 -0400

    Add clear button to list filter search input (#1845)
    * add clear button to list filter search input

--- ui/v2.5/src/components/List/ListFilter.tsx
+++ ui/v2.5/src/components/List/ListFilter.tsx
@@ -1,5 +1,6 @@
 import _, { debounce } from "lodash";
 import React, { HTMLAttributes, useEffect, useRef, useState } from "react";
+import cx from "classnames";
 import Mousetrap from "mousetrap";
 import { SortDirectionEnum } from "src/core/generated-graphql";
 import {
@@ -44,6 +45,9 @@ export const ListFilter: React.FC<IListFilterProps> = ({
 }) => {
   const [customPageSizeShowing, setCustomPageSizeShowing] = useState(false);
   const [queryRef, setQueryFocus] = useFocus();
+  const [queryClearShowing, setQueryClearShowing] = useState(
+    !!filter.searchTerm
+  );
   const perPageSelect = useRef(null);
   const [perPageInput, perPageFocus] = useFocus();
 
@@ -99,6 +103,14 @@ export const ListFilter: React.FC<IListFilterProps> = ({
 
   function onChangeQuery(event: React.FormEvent<HTMLInputElement>) {
     searchCallback(event.currentTarget.value);
+    setQueryClearShowing(!!event.currentTarget.value);
+  }
+
+  function onClearQuery() {
+    queryRef.current.value = "";
+    searchCallback("");
+    setQueryFocus();
+    setQueryClearShowing(false);
   }
 
   function onChangeSortDirection() {
@@ -217,7 +229,17 @@ export const ListFilter: React.FC<IListFilterProps> = ({
               onInput={onChangeQuery}
               className="query-text-field bg-secondary text-white border-secondary"
             />
-
+            <Button
+              variant="secondary"
+              onClick={onClearQuery}
+              title={intl.formatMessage({ id: "actions.clear" })}
+              className={cx(
+                "query-text-field-clear",
+                queryClearShowing ? "" : "d-none"
+              )}
+            >
+              <Icon icon="times" />
+            </Button>
             <InputGroup.Append>
               <OverlayTrigger
                 placement="top"
