a564a7eb757f45ece437f8ea150ac16515c76746
Infinite <infinitekittens@protonmail.com>
2021-10-23 15:08:10 +0000

    Split performerCreate page into separate page

--- ui/v2.5/src/components/Performers/Performers.tsx
+++ ui/v2.5/src/components/Performers/Performers.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import { PersistanceLevel } from "src/hooks/ListHook";
-import { Performer } from "./PerformerDetails/Performer";
+import Performer from "./PerformerDetails/Performer";
+import PerformerCreate from "./PerformerDetails/PerformerCreate";
 import { PerformerList } from "./PerformerList";
 
 const Performers = () => (
@@ -13,6 +14,7 @@ const Performers = () => (
         <PerformerList persistState={PersistanceLevel.ALL} {...props} />
       )}
     />
+    <Route path="/performers/new" component={PerformerCreate} />
     <Route path="/performers/:id/:tab?" component={Performer} />
   </Switch>
 );
