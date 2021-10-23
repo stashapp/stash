ff6039f52d76f9ce96e0ee86f50e94117557838a
Kermie <kermie@isinthe.house>
2021-10-23 03:21:12 -0700

    Italian translation metadata

--- ui/v2.5/src/locales/index.ts
+++ ui/v2.5/src/locales/index.ts
@@ -2,6 +2,7 @@ import deDE from "./de-DE.json";
 import enGB from "./en-GB.json";
 import enUS from "./en-US.json";
 import ptBR from "./pt-BR.json";
+import itIT from "./it-IT.json";
 import svSE from "./sv-SE.json";
 import zhTW from "./zh-TW.json";
 import zhCN from "./zh-CN.json";
@@ -11,6 +12,7 @@ export default {
   enGB,
   enUS,
   ptBR,
+  itIT,
   svSE,
   zhTW,
   zhCN,
