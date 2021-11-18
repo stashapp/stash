9733753f84fd0c1f349618a76410933be3de13b2
Infinite <infinitekittens@protonmail.com>
2021-11-18 21:06:06 +0000

    format

--- ui/v2.5/src/utils/hamming.ts
+++ ui/v2.5/src/utils/hamming.ts
@@ -1,21 +1,19 @@
-const hexToBinary = (hex: string) => (
-	hex.split('').map(i => (
-		parseInt(i, 16).toString(2).padStart(4, '0')
-	)).join('')
-);
+const hexToBinary = (hex: string) =>
+  hex
+    .split("")
+    .map((i) => parseInt(i, 16).toString(2).padStart(4, "0"))
+    .join("");
 
-export const distance = (a: string, b: string): number|undefined=> {
-  if (a.length !== b.length)
-    return;
+export const distance = (a: string, b: string): number | undefined => {
+  if (a.length !== b.length) return;
 
   const aBinary = hexToBinary(a);
   const bBinary = hexToBinary(b);
 
   let counter = 0;
   for (let i = 0; i < aBinary.length; i++) {
-    if (aBinary[i] !== bBinary[i])
-      counter++;
+    if (aBinary[i] !== bBinary[i]) counter++;
   }
 
   return counter;
-}
+};
