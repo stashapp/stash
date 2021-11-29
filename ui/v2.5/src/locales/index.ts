8680dde44d676383af8a4980993d8134743d5ebb
Kermie <kermie@isinthe.house>
2021-11-28 17:20:09 -0800

    Add Finnish Locale

--- ui/v2.5/src/locales/index.ts
+++ ui/v2.5/src/locales/index.ts
@@ -5,6 +5,7 @@ import esES from "./es-ES.json";
 import ptBR from "./pt-BR.json";
 import frFR from "./fr-FR.json";
 import itIT from "./it-IT.json";
+import fiFI from "./fi-FI.json";
 import svSE from "./sv-SE.json";
 import zhTW from "./zh-TW.json";
 import zhCN from "./zh-CN.json";
