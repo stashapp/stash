4c725e136c5311009eb5079ade96b66363dc1bb4
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2021-11-30 10:58:48 +1100

    Full width settings page

--- ui/v2.5/src/components/Settings/Settings.tsx
+++ ui/v2.5/src/components/Settings/Settings.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import queryString from "query-string";
-import { Card, Tab, Nav, Row, Col } from "react-bootstrap";
+import { Tab, Nav, Row, Col } from "react-bootstrap";
 import { useHistory, useLocation } from "react-router-dom";
 import { FormattedMessage, useIntl } from "react-intl";
 import { Helmet } from "react-helmet";
@@ -27,98 +27,101 @@ export const Settings: React.FC = () => {
     id: "settings",
   })} ${TITLE_SUFFIX}`;
   return (
-    <Card className="col col-lg-9 mx-auto">
+    <Tab.Container
+      activeKey={defaultTab}
+      id="configuration-tabs"
+      onSelect={(tab) => tab && onSelect(tab)}
+    >
       <Helmet
         defaultTitle={title_template}
         titleTemplate={`%s | ${title_template}`}
       />
-      <Tab.Container
-        activeKey={defaultTab}
-        id="configuration-tabs"
-        onSelect={(tab) => tab && onSelect(tab)}
-      >
-        <Row>
-          <Col sm={3} md={2}>
-            <Nav variant="pills" className="flex-column">
-              <Nav.Item>
-                <Nav.Link eventKey="configuration">
-                  <FormattedMessage id="configuration" />
-                </Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="interface">
-                  <FormattedMessage id="config.categories.interface" />
-                </Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="tasks">
-                  <FormattedMessage id="config.categories.tasks" />
-                </Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="dlna">DLNA</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="tools">
-                  <FormattedMessage id="config.categories.tools" />
-                </Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="scraping">
-                  <FormattedMessage id="config.categories.scraping" />
-                </Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="plugins">
-                  <FormattedMessage id="config.categories.plugins" />
-                </Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="logs">
-                  <FormattedMessage id="config.categories.logs" />
-                </Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="about">
-                  <FormattedMessage id="config.categories.about" />
-                </Nav.Link>
-              </Nav.Item>
-              <hr className="d-sm-none" />
-            </Nav>
-          </Col>
-          <Col sm={9} md={10}>
-            <Tab.Content>
-              <Tab.Pane eventKey="configuration">
-                <SettingsConfigurationPanel />
-              </Tab.Pane>
-              <Tab.Pane eventKey="interface">
-                <SettingsInterfacePanel />
-              </Tab.Pane>
-              <Tab.Pane eventKey="tasks">
-                <SettingsTasksPanel />
-              </Tab.Pane>
-              <Tab.Pane eventKey="dlna" unmountOnExit>
-                <SettingsDLNAPanel />
-              </Tab.Pane>
-              <Tab.Pane eventKey="tools" unmountOnExit>
-                <SettingsToolsPanel />
-              </Tab.Pane>
-              <Tab.Pane eventKey="scraping" unmountOnExit>
-                <SettingsScrapingPanel />
-              </Tab.Pane>
-              <Tab.Pane eventKey="plugins" unmountOnExit>
-                <SettingsPluginsPanel />
-              </Tab.Pane>
-              <Tab.Pane eventKey="logs" unmountOnExit>
-                <SettingsLogsPanel />
-              </Tab.Pane>
-              <Tab.Pane eventKey="about" unmountOnExit>
-                <SettingsAboutPanel />
-              </Tab.Pane>
-            </Tab.Content>
-          </Col>
-        </Row>
-      </Tab.Container>
-    </Card>
+      <Row>
+        <Col id="settings-menu-container" sm={3} md={2} xl={1}>
+          <Nav variant="pills" className="flex-column">
+            <Nav.Item>
+              <Nav.Link eventKey="configuration">
+                <FormattedMessage id="configuration" />
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="interface">
+                <FormattedMessage id="config.categories.interface" />
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="tasks">
+                <FormattedMessage id="config.categories.tasks" />
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="dlna">DLNA</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="tools">
+                <FormattedMessage id="config.categories.tools" />
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="scraping">
+                <FormattedMessage id="config.categories.scraping" />
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="plugins">
+                <FormattedMessage id="config.categories.plugins" />
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="logs">
+                <FormattedMessage id="config.categories.logs" />
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="about">
+                <FormattedMessage id="config.categories.about" />
+              </Nav.Link>
+            </Nav.Item>
+            <hr className="d-sm-none" />
+          </Nav>
+        </Col>
+        <Col
+          id="settings-container"
+          sm={{ offset: 3 }}
+          md={{ offset: 2 }}
+          xl={{ offset: 1 }}
+        >
+          <Tab.Content className="mx-auto">
+            <Tab.Pane eventKey="configuration">
+              <SettingsConfigurationPanel />
+            </Tab.Pane>
+            <Tab.Pane eventKey="interface">
+              <SettingsInterfacePanel />
+            </Tab.Pane>
+            <Tab.Pane eventKey="tasks">
+              <SettingsTasksPanel />
+            </Tab.Pane>
+            <Tab.Pane eventKey="dlna" unmountOnExit>
+              <SettingsDLNAPanel />
+            </Tab.Pane>
+            <Tab.Pane eventKey="tools" unmountOnExit>
+              <SettingsToolsPanel />
+            </Tab.Pane>
+            <Tab.Pane eventKey="scraping" unmountOnExit>
+              <SettingsScrapingPanel />
+            </Tab.Pane>
+            <Tab.Pane eventKey="plugins" unmountOnExit>
+              <SettingsPluginsPanel />
+            </Tab.Pane>
+            <Tab.Pane eventKey="logs" unmountOnExit>
+              <SettingsLogsPanel />
+            </Tab.Pane>
+            <Tab.Pane eventKey="about" unmountOnExit>
+              <SettingsAboutPanel />
+            </Tab.Pane>
+          </Tab.Content>
+        </Col>
+      </Row>
+    </Tab.Container>
   );
 };
