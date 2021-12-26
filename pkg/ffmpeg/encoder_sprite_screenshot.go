b525156df47283fab5e7b11215da819a2d4c2c40
bnkai <48220860+bnkai@users.noreply.github.com>
2021-12-26 12:41:41 +0200

    Fix scrubber sprite creation for small files

--- pkg/ffmpeg/encoder_sprite_screenshot.go
+++ pkg/ffmpeg/encoder_sprite_screenshot.go
@@ -8,6 +8,7 @@ import (
 
 type SpriteScreenshotOptions struct {
 	Time  float64
+	Frame int
 	Width int
 }
 
@@ -18,6 +19,34 @@ func (e *Encoder) SpriteScreenshot(probeResult VideoFile, options SpriteScreensh
 		"-i", probeResult.Path,
 		"-vframes", "1",
 		"-vf", fmt.Sprintf("scale=%v:-1", options.Width),
+		"-c:v", "bmp",
+		"-f", "rawvideo",
+		"-",
+	}
+	data, err := e.run(probeResult.Path, args, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	reader := strings.NewReader(data)
+
+	img, _, err := image.Decode(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	return img, err
+}
+
+// SpriteScreenshotSlow uses the select filter to get a single frame from a videofile instead of seeking
+// It is very slow and should only be used for files with very small duration in secs /  frame count
+func (e *Encoder) SpriteScreenshotSlow(probeResult VideoFile, options SpriteScreenshotOptions) (image.Image, error) {
+	args := []string{
+		"-v", "error",
+		"-i", probeResult.Path,
+		"-vsync", "0", // do not create/drop frames
+		"-vframes", "1",
+		"-vf", fmt.Sprintf("select=eq(n\\,%d),scale=%v:-1", options.Frame, options.Width), // keep only frame number options.Frame
 		"-c:v", "bmp",
 		"-f", "rawvideo",
 		"-",
