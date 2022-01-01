f3a9ef9dddfe1cea2bff69ffbec9abb03d5e3a76
chetbrinko <henryevans@protonmail.com>
2022-01-01 11:58:00 +0100

    Add support for the Handy APIv2
    Docs: https://staging.handyfeeling.com/api/handy/v2/docs/
    Ref: https://github.com/defucilis/thehandy
    
    Update axios to 0.24.0 due to a security update

--- ui/v2.5/src/utils/interactive.ts
+++ ui/v2.5/src/utils/interactive.ts
@@ -1,7 +1,10 @@
-import Handy from "thehandy";
+import { Handy, HandyUtils } from "thehandy";
+import { HandyMode, HsspSetupResult } from "thehandy/lib/types";
 
 interface IFunscript {
   actions: Array<IAction>;
+  inverted: boolean;
+  range: number;
 }
 
 interface IAction {
@@ -9,13 +12,38 @@ interface IAction {
   pos: number;
 }
 
-// Copied from handy-js-sdk under MIT license, with modifications. (It's not published to npm)
-// Converting to CSV first instead of uploading Funscripts will reduce uploaded file size.
+// Utility function to convert one range of values to another
+function convertRange(
+  value: number,
+  fromLow: number,
+  fromHigh: number,
+  toLow: number,
+  toHigh: number
+) {
+  return ((value - fromLow) * (toHigh - toLow)) / (fromHigh - fromLow) + toLow;
+}
+
+// Converting to CSV first instead of uploading Funscripts is required
+// Reference for Funscript format:
+// https://pkg.go.dev/github.com/funjack/launchcontrol/protocol/funscript
 function convertFunscriptToCSV(funscript: IFunscript) {
   const lineTerminator = "\r\n";
   if (funscript?.actions?.length > 0) {
     return funscript.actions.reduce((prev: string, curr: IAction) => {
-      return `${prev}${curr.at},${curr.pos}${lineTerminator}`;
+      var { pos } = curr;
+      // If it's inverted in the Funscript, we flip it because
+      // the Handy doesn't have inverted support
+      if (funscript.inverted === true) {
+        pos = convertRange(curr.pos, 0, 100, 100, 0);
+      }
+      // in APIv2; the Handy maintains it's own slide range
+      // (ref: https://staging.handyfeeling.com/api/handy/v2/docs/#/SLIDE )
+      // so if a range is specified in the Funscript, we convert it to the
+      // full range and let the Handy's settings take precedence
+      if (funscript.range) {
+        pos = convertRange(curr.pos, 0, funscript.range, 0, 100);
+      }
+      return `${prev}${curr.at},${pos}${lineTerminator}`;
     }, `#Created by stash.app ${new Date().toUTCString()}\n`);
   }
   throw new Error("Not a valid funscript");
@@ -45,40 +73,36 @@ export class Interactive {
     if (!(this._handy.connectionKey && funscriptPath)) {
       return;
     }
-
-    if (!this._handy.serverTimeOffset) {
-      const cachedOffset = localStorage.getItem("serverTimeOffset");
-      if (cachedOffset !== null) {
-        this._handy.serverTimeOffset = parseInt(cachedOffset, 10);
-      } else {
-        // One time sync to get server time offset
-        await this._handy.getServerTimeOffset();
-        localStorage.setItem(
-          "serverTimeOffset",
-          this._handy.serverTimeOffset.toString()
-        );
-      }
-    }
+    // Calibrates the latency between the browser client and the Handy server's
+    // This is done before a script upload to ensure a synchronized experience
+    await this._handy.getServerTimeOffset();
 
     const csv = await fetch(funscriptPath)
       .then((response) => response.json())
       .then((json) => convertFunscriptToCSV(json));
     const fileName = `${Math.round(Math.random() * 100000000)}.csv`;
     const csvFile = new File([csv], fileName);
-    const tempURL = await this._handy
-      .uploadCsv(csvFile)
-      .then((response) => response.url);
+    const tempURL = await HandyUtils.uploadCsv(csvFile).then(
+      (response) => response.url
+    );
+
+    await this._handy.setMode(HandyMode.hssp);
+
     this._connected = await this._handy
-      .syncPrepare(encodeURIComponent(tempURL), fileName, csvFile.size)
-      .then((response) => response.connected);
+      .setHsspSetup(tempURL)
+      .then((result) => result === HsspSetupResult.downloaded);
   }
 
   async play(position: number) {
     if (!this._connected) {
       return;
     }
+
     this._playing = await this._handy
-      .syncPlay(true, Math.round(position * 1000 + this._scriptOffset))
+      .setHsspPlay(
+        Math.round(position * 1000 + this._scriptOffset),
+        this._handy.estimatedServerTimeOffset + Date.now() // our guess of the Handy server's UNIX epoch time
+      )
       .then(() => true);
   }
 
@@ -86,7 +110,7 @@ export class Interactive {
     if (!this._connected) {
       return;
     }
-    this._playing = await this._handy.syncPlay(false).then(() => false);
+    this._playing = await this._handy.setHsspStop().then(() => false);
   }
 
   async ensurePlaying(position: number) {
