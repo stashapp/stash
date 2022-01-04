500606685bf285bb3a46bf6e791c815198f90875
Kermie <kermie@isinthe.house>
2022-01-03 19:40:25 -0800

    Fix lints

--- ui/v2.5/src/components/Performers/GenderIcon.tsx
+++ ui/v2.5/src/components/Performers/GenderIcon.tsx
@@ -3,7 +3,6 @@ import {
   faVenus,
   faTransgenderAlt,
   faMars,
-  faVenusMars,
 } from "@fortawesome/free-solid-svg-icons";
 import * as GQL from "src/core/generated-graphql";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
