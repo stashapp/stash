51d25e2256d42be38634e08df5505cb09c3c4a0a
Kermie <kermie@isinthe.house>
2022-01-07 17:14:24 -0800

    Allow stash to be iframed

--- pkg/api/server.go
+++ pkg/api/server.go
@@ -364,7 +364,6 @@ func SecurityHeadersMiddleware(next http.Handler) http.Handler {
 
 		w.Header().Set("Referrer-Policy", "same-origin")
 		w.Header().Set("X-Content-Type-Options", "nosniff")
-		w.Header().Set("X-Frame-Options", "DENY")
 		w.Header().Set("X-XSS-Protection", "1")
 		w.Header().Set("Content-Security-Policy", cspDirectives)
 
