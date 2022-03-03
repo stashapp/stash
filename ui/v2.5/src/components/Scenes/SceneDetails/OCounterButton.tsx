0737ca953d74770284ba19f44a15227960494417
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2022-03-03 11:39:03 +1100

    Add title, rating, o-counter to image lightbox (#2274)

--- ui/v2.5/src/components/Scenes/SceneDetails/OCounterButton.tsx
+++ ui/v2.5/src/components/Scenes/SceneDetails/OCounterButton.tsx
@@ -1,34 +1,45 @@
-import React from "react";
-import {
-  Button,
-  ButtonGroup,
-  Dropdown,
-  DropdownButton,
-  Spinner,
-} from "react-bootstrap";
+import React, { useState } from "react";
+import { Button, ButtonGroup, Dropdown, DropdownButton } from "react-bootstrap";
 import { useIntl } from "react-intl";
-import { Icon, SweatDrops } from "src/components/Shared";
+import { Icon, LoadingIndicator, SweatDrops } from "src/components/Shared";
 
 export interface IOCounterButtonProps {
-  loading: boolean;
   value: number;
-  onIncrement: () => void;
-  onDecrement: () => void;
-  onReset: () => void;
-  onMenuOpened?: () => void;
-  onMenuClosed?: () => void;
+  onIncrement: () => Promise<void>;
+  onDecrement: () => Promise<void>;
+  onReset: () => Promise<void>;
 }
 
 export const OCounterButton: React.FC<IOCounterButtonProps> = (
   props: IOCounterButtonProps
 ) => {
   const intl = useIntl();
-  if (props.loading) return <Spinner animation="border" role="status" />;
+  const [loading, setLoading] = useState(false);
+
+  async function increment() {
+    setLoading(true);
+    await props.onIncrement();
+    setLoading(false);
+  }
+
+  async function decrement() {
+    setLoading(true);
+    await props.onDecrement();
+    setLoading(false);
+  }
+
+  async function reset() {
+    setLoading(true);
+    await props.onReset();
+    setLoading(false);
+  }
+
+  if (loading) return <LoadingIndicator message="" inline small />;
 
   const renderButton = () => (
     <Button
       className="minimal pr-1"
-      onClick={props.onIncrement}
+      onClick={increment}
       variant="secondary"
       title={intl.formatMessage({ id: "o_counter" })}
     >
@@ -46,11 +57,11 @@ export const OCounterButton: React.FC<IOCounterButtonProps> = (
           variant="secondary"
           className="pl-0 show-carat"
         >
-          <Dropdown.Item onClick={props.onDecrement}>
+          <Dropdown.Item onClick={decrement}>
             <Icon icon="minus" />
             <span>Decrement</span>
           </Dropdown.Item>
-          <Dropdown.Item onClick={props.onReset}>
+          <Dropdown.Item onClick={reset}>
             <Icon icon="ban" />
             <span>Reset</span>
           </Dropdown.Item>
