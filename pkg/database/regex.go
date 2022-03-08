d22f22e65ea810ff3df6bf8ec7919452f54c5f8a
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2022-03-08 14:59:49 +1100

    Document regex cache size

--- pkg/database/regex.go
+++ pkg/database/regex.go
@@ -6,6 +6,12 @@ import (
 	lru "github.com/hashicorp/golang-lru"
 )
 
+// size of the regex LRU cache in elements.
+// A small number number was chosen because it's most likely use is for a
+// single query - this function gets called for every row in the (filtered)
+// results. It's likely to only need no more than 1 or 2 in any given query.
+// After that point, it's just sitting in the cache and is unlikely to be used
+// again.
 const regexCacheSize = 10
 
 var regexCache *lru.Cache
