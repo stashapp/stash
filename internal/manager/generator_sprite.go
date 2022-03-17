e1d1429ea82cc8c2fee334c1849a916f093ba098
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2022-03-17 13:16:30 +1100

    Add missing lockmanager instance

--- internal/manager/generator_sprite.go
+++ internal/manager/generator_sprite.go
@@ -75,8 +75,9 @@ func NewSpriteGenerator(videoFile ffmpeg.VideoFile, videoChecksum string, imageO
 		SlowSeek:        slowSeek,
 		Columns:         cols,
 		g: &generate.Generator{
-			Encoder:    instance.FFMPEG,
-			ScenePaths: instance.Paths.Scene,
+			Encoder:     instance.FFMPEG,
+			LockManager: instance.ReadLockManager,
+			ScenePaths:  instance.Paths.Scene,
 		},
 	}, nil
 }
