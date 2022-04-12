102389f87dafb4125e90a2aaa07598a6961d1196
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2022-04-13 09:49:51 +1000

    Add txn management to sqlite

--- pkg/sqlite/transaction.go
+++ pkg/sqlite/transaction.go
@@ -2,7 +2,6 @@ package sqlite
 
 import (
 	"context"
-	"database/sql"
 	"errors"
 	"fmt"
 
@@ -11,12 +10,51 @@ import (
 	"github.com/stashapp/stash/pkg/models"
 )
 
-type dbi interface {
-	Get(dest interface{}, query string, args ...interface{}) error
-	Select(dest interface{}, query string, args ...interface{}) error
-	Queryx(query string, args ...interface{}) (*sqlx.Rows, error)
-	NamedExec(query string, arg interface{}) (sql.Result, error)
-	Exec(query string, args ...interface{}) (sql.Result, error)
+type key int
+
+const (
+	txnKey key = iota + 1
+)
+
+type Database struct {
+	DB *sqlx.DB
+}
+
+func (db *Database) Begin(ctx context.Context) (context.Context, error) {
+	if tx, _ := getTx(ctx); tx != nil {
+		return nil, fmt.Errorf("already in transaction")
+	}
+
+	tx, err := db.DB.BeginTxx(ctx, nil)
+	if err != nil {
+		return nil, fmt.Errorf("beginning transaction: %w", err)
+	}
+
+	return context.WithValue(ctx, txnKey, tx), nil
+}
+
+func (db *Database) Commit(ctx context.Context) error {
+	tx, err := getTx(ctx)
+	if err != nil {
+		return err
+	}
+	return tx.Commit()
+}
+
+func (db *Database) Rollback(ctx context.Context) error {
+	tx, err := getTx(ctx)
+	if err != nil {
+		return err
+	}
+	return tx.Rollback()
+}
+
+func getTx(ctx context.Context) (*sqlx.Tx, error) {
+	tx, ok := ctx.Value(txnKey).(*sqlx.Tx)
+	if !ok || tx == nil {
+		return nil, fmt.Errorf("not in transaction")
+	}
+	return tx, nil
 }
 
 type transaction struct {
