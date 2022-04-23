d1dedca012c9441c4eafba4e20d45221b80d496f
cj13 <tedabed@gmail.com>
2022-04-23 12:36:04 -0600

    removed caption index

--- pkg/database/migrations/31_scenes_captions.up.sql
+++ pkg/database/migrations/31_scenes_captions.up.sql
@@ -5,5 +5,3 @@ CREATE TABLE `scene_captions` (
   `caption_type` varchar(255) NOT NULL,
   foreign key(`scene_id`) references `scenes`(`id`) on delete CASCADE
 );
-
-CREATE UNIQUE INDEX `scene_captions_caption_unique` on `scene_captions` (`path`);
