92dbf25341a7b4c6332fdae0473c174b7cb5be10
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2022-05-05 16:07:30 +1000

    Ensure one caption per scene/language

--- pkg/database/migrations/31_scenes_captions.up.sql
+++ pkg/database/migrations/31_scenes_captions.up.sql
@@ -3,5 +3,6 @@ CREATE TABLE `scene_captions` (
   `language_code` varchar(255) NOT NULL,
   `filename` varchar(255) NOT NULL,
   `caption_type` varchar(255) NOT NULL,
+  primary key (`scene_id`, `language_code`),
   foreign key(`scene_id`) references `scenes`(`id`) on delete CASCADE
 );
