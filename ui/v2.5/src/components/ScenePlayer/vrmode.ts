68ede843412138e79469b4c85f7e26d65533e0fe
pickleahead <buttercup49358749@gmail.com>
2022-06-14 18:11:18 +0200

    don't use videojs-vr in vr browsers

--- ui/v2.5/src/components/ScenePlayer/vrmode.ts
+++ ui/v2.5/src/components/ScenePlayer/vrmode.ts
@@ -5,128 +5,134 @@ import * as GQL from "../../core/generated-graphql";
 import { projectionStrings } from "../../utils/projection";
 import { stereoModeStrings } from "../../utils/stereoMode";
 
+function isVrDevice() {
+  return navigator.userAgent.match(/oculusbrowser|\svr\s/i);
+}
+
 const vrmode = function (this: VideoJsPlayer, scene: GQL.SceneDataFragment) {
   const player = this;
   let projection;
   let stereo_mode;
   let lowercaseSceneTags = scene.tags.map((tag) => tag.name.toLowerCase());
 
-  if (
-    scene.projection &&
-    scene.projection !== "AUTO" &&
-    projectionStrings.includes(scene.projection)
-  ) {
-    ({ projection } = scene);
-  } else {
+  if (!isVrDevice()) {
     if (
-      lowercaseSceneTags.filter((tag) =>
-        /^vr$|virtual reality|^vr porn|^3d$/.exec(tag)
-      ).length > 0
+      scene.projection &&
+      scene.projection !== "AUTO" &&
+      projectionStrings.includes(scene.projection)
     ) {
-      if (
-        lowercaseSceneTags.filter((tag) => /^dome|^180$|^180\D$/.exec(tag))
-          .length > 0
-      ) {
-        projection = "DOME";
-      } else if (
-        lowercaseSceneTags.filter((tag) =>
-          /^sphere$|^equirectangular$|^360$|^360[\D\-_]/.exec(tag)
-        ).length > 0
-      ) {
-        projection = "SPHERE";
-      } else if (
-        lowercaseSceneTags.filter((tag) => /^mkx200$/.exec(tag)).length > 0
-      ) {
-        projection = "MKX200";
-      } else if (
-        lowercaseSceneTags.filter((tag) => /^cube$/.exec(tag)).length > 0
-      ) {
-        projection = "CUBE";
-      } else if (
-        lowercaseSceneTags.filter((tag) =>
-          /^eac$|^eac[\-_]|^equi[\-_\s]?angular[\-_\s]?cube[\-_\s]?/.exec(tag)
-        ).length > 0
-      ) {
-        projection = "EAC";
-      } else if (
-        lowercaseSceneTags.filter((tag) => /^rf52$/.exec(tag)).length > 0
-      ) {
-        projection = "RF52";
-      } else if (
-        lowercaseSceneTags.filter((tag) => /^fisheye$/.exec(tag)).length > 0
-      ) {
-        projection = "FISHEYE";
-      }else {
-        projection = "DOME";
-      }
-    }
-  }
-
-  if (
-    scene.stereo_mode &&
-    scene.stereo_mode !== "AUTO" &&
-    stereoModeStrings.includes(scene.stereo_mode)
-  ) {
-    ({ stereo_mode } = scene);
-  } else {
-    if (
-      lowercaseSceneTags.filter((tag) =>
-        /^vr$|virtual reality|^vr porn|^3d$/.exec(tag)
-      ).length > 0
-    ) {
+      ({ projection } = scene);
+    } else {
       if (
         lowercaseSceneTags.filter((tag) =>
-          /^mono$|^monoscopic$|^[0-9]{1,3}[\D\-_]mono$/.exec(tag)
+          /^vr$|virtual reality|^vr porn|^3d$/.exec(tag)
         ).length > 0
       ) {
-        stereo_mode = "MONO";
-      } else if (
-        lowercaseSceneTags.filter((tag) =>
-          /^stereo$|^stereoscopic|^[0-9]{1,3}[\D\-_]lr$|^[0-9]{1,3}[\D\-_]left[\D\-_]right$/.exec(
-            tag
-          )
-        ).length > 0
-      ) {
-        stereo_mode = "LEFT_RIGHT";
-      } else if (
-        lowercaseSceneTags.filter((tag) =>
-          /^top[\D\-_]bottom$|^tb$|^[0-9]{1,3}[\D\-_]tb$|^[0-9]{1,3}[\D\-_]top[\D\-_]bottom$/.exec(
-            tag
-          )
-        ).length > 0
-      ) {
-        stereo_mode = "TOP_BOTTOM";
-      } else {
-        stereo_mode = "LEFT_RIGHT";
+        if (
+          lowercaseSceneTags.filter((tag) => /^dome|^180$|^180\D$/.exec(tag))
+            .length > 0
+        ) {
+          projection = "DOME";
+        } else if (
+          lowercaseSceneTags.filter((tag) =>
+            /^sphere$|^equirectangular$|^360$|^360[\D\-_]/.exec(tag)
+          ).length > 0
+        ) {
+          projection = "SPHERE";
+        } else if (
+          lowercaseSceneTags.filter((tag) => /^mkx200$/.exec(tag)).length > 0
+        ) {
+          projection = "MKX200";
+        } else if (
+          lowercaseSceneTags.filter((tag) => /^cube$/.exec(tag)).length > 0
+        ) {
+          projection = "CUBE";
+        } else if (
+          lowercaseSceneTags.filter((tag) =>
+            /^eac$|^eac[\-_]|^equi[\-_\s]?angular[\-_\s]?cube[\-_\s]?/.exec(tag)
+          ).length > 0
+        ) {
+          projection = "EAC";
+        } else if (
+          lowercaseSceneTags.filter((tag) => /^rf52$/.exec(tag)).length > 0
+        ) {
+          projection = "RF52";
+        } else if (
+          lowercaseSceneTags.filter((tag) => /^fisheye$/.exec(tag)).length > 0
+        ) {
+          projection = "FISHEYE";
+        } else {
+          projection = "DOME";
+        }
       }
     }
-  }
 
-  switch (projection) {
-    case "SPHERE":
-      switch (stereo_mode) {
-        case "TOP_BOTTOM":
-          player.vr({ projection: "360_TB" });
-          break;
-        case "LEFT_RIGHT":
-          player.vr({ projection: "360_LR" });
-          break;
-        default:
-          player.vr({ projection: "360" });
+    if (
+      scene.stereo_mode &&
+      scene.stereo_mode !== "AUTO" &&
+      stereoModeStrings.includes(scene.stereo_mode)
+    ) {
+      ({ stereo_mode } = scene);
+    } else {
+      if (
+        lowercaseSceneTags.filter((tag) =>
+          /^vr$|virtual reality|^vr porn|^3d$/.exec(tag)
+        ).length > 0
+      ) {
+        if (
+          lowercaseSceneTags.filter((tag) =>
+            /^mono$|^monoscopic$|^[0-9]{1,3}[\D\-_]mono$/.exec(tag)
+          ).length > 0
+        ) {
+          stereo_mode = "MONO";
+        } else if (
+          lowercaseSceneTags.filter((tag) =>
+            /^stereo$|^stereoscopic|^[0-9]{1,3}[\D\-_]lr$|^[0-9]{1,3}[\D\-_]left[\D\-_]right$/.exec(
+              tag
+            )
+          ).length > 0
+        ) {
+          stereo_mode = "LEFT_RIGHT";
+        } else if (
+          lowercaseSceneTags.filter((tag) =>
+            /^top[\D\-_]bottom$|^tb$|^[0-9]{1,3}[\D\-_]tb$|^[0-9]{1,3}[\D\-_]top[\D\-_]bottom$/.exec(
+              tag
+            )
+          ).length > 0
+        ) {
+          stereo_mode = "TOP_BOTTOM";
+        } else {
+          stereo_mode = "LEFT_RIGHT";
+        }
       }
-      break;
-    case "DOME":
-    case "MKX200":
-    case "RF52":
-    case "FISHEYE":
-      switch (stereo_mode) {
-        case "LEFT_RIGHT":
-          player.vr({ projection: "180_LR" });
-          break;
-        default:
-          player.vr({ projection: "180_MONO" });
-      }
-      break;
+    }
+
+    switch (projection) {
+      case "SPHERE":
+        switch (stereo_mode) {
+          case "TOP_BOTTOM":
+            player.vr({ projection: "360_TB" });
+            break;
+          case "LEFT_RIGHT":
+            player.vr({ projection: "360_LR" });
+            break;
+          default:
+            player.vr({ projection: "360" });
+        }
+        break;
+      case "DOME":
+      case "MKX200":
+      case "RF52":
+      case "FISHEYE":
+        switch (stereo_mode) {
+          case "LEFT_RIGHT":
+            player.vr({ projection: "180_LR" });
+            break;
+          default:
+            player.vr({ projection: "180_MONO" });
+        }
+        break;
+    }
   }
 };
 
