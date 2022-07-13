8036afee1af7b90dab01c099da192cccd4fa0269
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2022-07-13 03:22:13 +0000

    Add warnings in release and migration notes

--- ui/v2.5/src/docs/en/MigrationNotes/index.ts
+++ ui/v2.5/src/docs/en/MigrationNotes/index.ts
@@ -1,9 +1,7 @@
-// import migration32 from "./32.md";
+import migration32 from "./32.md";
 
-// type Module = typeof migration32;
+type Module = typeof migration32;
 
-// replace any with module once we add migration notes
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const migrationNotes: Record<number, any> = {
-  // 32: migration31,
+export const migrationNotes: Record<number, Module> = {
+  32: migration32,
 };
