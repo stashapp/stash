b097e892415780521177c98855c462bc3c2d3973
Hijack Hornet <northriver@laposte.net>
2022-08-17 18:07:09 +0200

    Experiment with locals

--- ui/v2.5/src/locales/index.ts
+++ ui/v2.5/src/locales/index.ts
@@ -18,6 +18,7 @@ export const localeLoader = {
   plPL: () => import("./pl-PL.json"),
   daDK: () => import("./da-DK.json"),
   koKR: () => import("./ko-KR.json"),
+  hentaiH: () => import("./hentai-H.json"),
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
 } as { [key: string]: any };
 
