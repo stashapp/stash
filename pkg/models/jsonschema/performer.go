97a47a6b352ee8e6641546f2536bdac6351ff63c
skier233 <benchy222@gmail.com>
2022-09-08 09:34:11 -0400

    more merge fixes

--- pkg/models/jsonschema/performer.go
+++ pkg/models/jsonschema/performer.go
@@ -1,168 +1,69 @@
-package models
+package jsonschema
 
 import (
-	"context"
 	"fmt"
-	"io"
-	"strconv"
+	"os"
+
+	jsoniter "github.com/json-iterator/go"
+	"github.com/stashapp/stash/pkg/fsutil"
+	"github.com/stashapp/stash/pkg/models"
+	"github.com/stashapp/stash/pkg/models/json"
 )
 
-type GenderEnum string
-
-const (
-	GenderEnumMale              GenderEnum = "MALE"
-	GenderEnumFemale            GenderEnum = "FEMALE"
-	GenderEnumTransgenderMale   GenderEnum = "TRANSGENDER_MALE"
-	GenderEnumTransgenderFemale GenderEnum = "TRANSGENDER_FEMALE"
-	GenderEnumIntersex          GenderEnum = "INTERSEX"
-	GenderEnumNonBinary         GenderEnum = "NON_BINARY"
-)
-
-var AllGenderEnum = []GenderEnum{
-	GenderEnumMale,
-	GenderEnumFemale,
-	GenderEnumTransgenderMale,
-	GenderEnumTransgenderFemale,
-	GenderEnumIntersex,
-	GenderEnumNonBinary,
+type Performer struct {
+	Name          string           `json:"name,omitempty"`
+	Gender        string           `json:"gender,omitempty"`
+	URL           string           `json:"url,omitempty"`
+	Twitter       string           `json:"twitter,omitempty"`
+	Instagram     string           `json:"instagram,omitempty"`
+	Birthdate     string           `json:"birthdate,omitempty"`
+	Ethnicity     string           `json:"ethnicity,omitempty"`
+	Country       string           `json:"country,omitempty"`
+	EyeColor      string           `json:"eye_color,omitempty"`
+	Height        string           `json:"height,omitempty"`
+	Measurements  string           `json:"measurements,omitempty"`
+	FakeTits      string           `json:"fake_tits,omitempty"`
+	CareerLength  string           `json:"career_length,omitempty"`
+	Tattoos       string           `json:"tattoos,omitempty"`
+	Piercings     string           `json:"piercings,omitempty"`
+	Aliases       string           `json:"aliases,omitempty"`
+	Favorite      bool             `json:"favorite,omitempty"`
+	Tags          []string         `json:"tags,omitempty"`
+	Image         string           `json:"image,omitempty"`
+	CreatedAt     json.JSONTime    `json:"created_at,omitempty"`
+	UpdatedAt     json.JSONTime    `json:"updated_at,omitempty"`
+	Rating        int              `json:"rating,omitempty"`
+	Details       string           `json:"details,omitempty"`
+	DeathDate     string           `json:"death_date,omitempty"`
+	HairColor     string           `json:"hair_color,omitempty"`
+	Weight        int              `json:"weight,omitempty"`
+	StashIDs      []models.StashID `json:"stash_ids,omitempty"`
+	IgnoreAutoTag bool             `json:"ignore_auto_tag,omitempty"`
 }
 
-func (e GenderEnum) IsValid() bool {
-	switch e {
-	case GenderEnumMale, GenderEnumFemale, GenderEnumTransgenderMale, GenderEnumTransgenderFemale, GenderEnumIntersex, GenderEnumNonBinary:
-		return true
+func (s Performer) Filename() string {
+	return fsutil.SanitiseBasename(s.Name) + ".json"
+}
+
+func LoadPerformerFile(filePath string) (*Performer, error) {
+	var performer Performer
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
 	}
-	return false
-}
-
-func (e GenderEnum) String() string {
-	return string(e)
-}
-
-func (e *GenderEnum) UnmarshalGQL(v interface{}) error {
-	str, ok := v.(string)
-	if !ok {
-		return fmt.Errorf("enums must be strings")
+	defer file.Close()
+	var json = jsoniter.ConfigCompatibleWithStandardLibrary
+	jsonParser := json.NewDecoder(file)
+	err = jsonParser.Decode(&performer)
+	if err != nil {
+		return nil, err
 	}
+	return &performer, nil
+}
 
-	*e = GenderEnum(str)
-	if !e.IsValid() {
-		return fmt.Errorf("%s is not a valid GenderEnum", str)
+func SavePerformerFile(filePath string, performer *Performer) error {
+	if performer == nil {
+		return fmt.Errorf("performer must not be nil")
 	}
-	return nil
-}
-
-func (e GenderEnum) MarshalGQL(w io.Writer) {
-	fmt.Fprint(w, strconv.Quote(e.String()))
-}
-
-type GenderCriterionInput struct {
-	Value    *GenderEnum       `json:"value"`
-	Modifier CriterionModifier `json:"modifier"`
-}
-
-type PerformerFilterType struct {
-	And     *PerformerFilterType  `json:"AND"`
-	Or      *PerformerFilterType  `json:"OR"`
-	Not     *PerformerFilterType  `json:"NOT"`
-	Name    *StringCriterionInput `json:"name"`
-	Details *StringCriterionInput `json:"details"`
-	// Filter by favorite
-	FilterFavorites *bool `json:"filter_favorites"`
-	// Filter by birth year
-	BirthYear *IntCriterionInput `json:"birth_year"`
-	// Filter by age
-	Age *IntCriterionInput `json:"age"`
-	// Filter by ethnicity
-	Ethnicity *StringCriterionInput `json:"ethnicity"`
-	// Filter by country
-	Country *StringCriterionInput `json:"country"`
-	// Filter by eye color
-	EyeColor *StringCriterionInput `json:"eye_color"`
-	// Filter by height
-	Height *StringCriterionInput `json:"height"`
-	// Filter by measurements
-	Measurements *StringCriterionInput `json:"measurements"`
-	// Filter by fake tits value
-	FakeTits *StringCriterionInput `json:"fake_tits"`
-	// Filter by career length
-	CareerLength *StringCriterionInput `json:"career_length"`
-	// Filter by tattoos
-	Tattoos *StringCriterionInput `json:"tattoos"`
-	// Filter by piercings
-	Piercings *StringCriterionInput `json:"piercings"`
-	// Filter by aliases
-	Aliases *StringCriterionInput `json:"aliases"`
-	// Filter by gender
-	Gender *GenderCriterionInput `json:"gender"`
-	// Filter to only include performers missing this property
-	IsMissing *string `json:"is_missing"`
-	// Filter to only include performers with these tags
-	Tags *HierarchicalMultiCriterionInput `json:"tags"`
-	// Filter by tag count
-	TagCount *IntCriterionInput `json:"tag_count"`
-	// Filter by scene count
-	SceneCount *IntCriterionInput `json:"scene_count"`
-	// Filter by image count
-	ImageCount *IntCriterionInput `json:"image_count"`
-	// Filter by gallery count
-	GalleryCount *IntCriterionInput `json:"gallery_count"`
-	// Filter by StashID
-	StashID *StringCriterionInput `json:"stash_id"`
-	// Filter by rating
-	Rating *IntCriterionInput `json:"rating"`
-	// Filter by url
-	URL *StringCriterionInput `json:"url"`
-	// Filter by hair color
-	HairColor *StringCriterionInput `json:"hair_color"`
-	// Filter by weight
-	Weight *IntCriterionInput `json:"weight"`
-	// Filter by death year
-	DeathYear *IntCriterionInput `json:"death_year"`
-	// Filter by studios where performer appears in scene/image/gallery
-	Studios *HierarchicalMultiCriterionInput `json:"studios"`
-	// Filter by autotag ignore value
-	IgnoreAutoTag *bool `json:"ignore_auto_tag"`
-}
-
-type PerformerFinder interface {
-	FindMany(ctx context.Context, ids []int) ([]*Performer, error)
-}
-
-type PerformerReader interface {
-	Find(ctx context.Context, id int) (*Performer, error)
-	PerformerFinder
-	FindBySceneID(ctx context.Context, sceneID int) ([]*Performer, error)
-	FindNamesBySceneID(ctx context.Context, sceneID int) ([]*Performer, error)
-	FindByImageID(ctx context.Context, imageID int) ([]*Performer, error)
-	FindByGalleryID(ctx context.Context, galleryID int) ([]*Performer, error)
-	FindByNames(ctx context.Context, names []string, nocase bool) ([]*Performer, error)
-	FindByStashID(ctx context.Context, stashID StashID) ([]*Performer, error)
-	FindByStashIDStatus(ctx context.Context, hasStashID bool, stashboxEndpoint string) ([]*Performer, error)
-	CountByTagID(ctx context.Context, tagID int) (int, error)
-	Count(ctx context.Context) (int, error)
-	All(ctx context.Context) ([]*Performer, error)
-	// TODO - this interface is temporary until the filter schema can fully
-	// support the query needed
-	QueryForAutoTag(ctx context.Context, words []string) ([]*Performer, error)
-	Query(ctx context.Context, performerFilter *PerformerFilterType, findFilter *FindFilterType) ([]*Performer, int, error)
-	GetImage(ctx context.Context, performerID int) ([]byte, error)
-	StashIDLoader
-	GetTagIDs(ctx context.Context, performerID int) ([]int, error)
-}
-
-type PerformerWriter interface {
-	Create(ctx context.Context, newPerformer Performer) (*Performer, error)
-	Update(ctx context.Context, updatedPerformer PerformerPartial) (*Performer, error)
-	UpdateFull(ctx context.Context, updatedPerformer Performer) (*Performer, error)
-	Destroy(ctx context.Context, id int) error
-	UpdateImage(ctx context.Context, performerID int, image []byte) error
-	DestroyImage(ctx context.Context, performerID int) error
-	UpdateStashIDs(ctx context.Context, performerID int, stashIDs []StashID) error
-	UpdateTags(ctx context.Context, performerID int, tagIDs []int) error
-}
-
-type PerformerReaderWriter interface {
-	PerformerReader
-	PerformerWriter
+	return marshalToFile(filePath, performer)
 }
--- pkg/models/jsonschema/studio.go
+++ pkg/models/jsonschema/studio.go
@@ -1,69 +1,52 @@
-package models
+package jsonschema
 
-import "context"
+import (
+	"fmt"
+	"os"
 
-type StudioFilterType struct {
-	And     *StudioFilterType     `json:"AND"`
-	Or      *StudioFilterType     `json:"OR"`
-	Not     *StudioFilterType     `json:"NOT"`
-	Name    *StringCriterionInput `json:"name"`
-	Details *StringCriterionInput `json:"details"`
-	// Filter to only include studios with this parent studio
-	Parents *MultiCriterionInput `json:"parents"`
-	// Filter by StashID
-	StashID *StringCriterionInput `json:"stash_id"`
-	// Filter to only include studios missing this property
-	IsMissing *string `json:"is_missing"`
-	// Filter by rating
-	Rating *IntCriterionInput `json:"rating"`
-	// Filter by scene count
-	SceneCount *IntCriterionInput `json:"scene_count"`
-	// Filter by image count
-	ImageCount *IntCriterionInput `json:"image_count"`
-	// Filter by gallery count
-	GalleryCount *IntCriterionInput `json:"gallery_count"`
-	// Filter by url
-	URL *StringCriterionInput `json:"url"`
-	// Filter by studio aliases
-	Aliases *StringCriterionInput `json:"aliases"`
-	// Filter by autotag ignore value
-	IgnoreAutoTag *bool `json:"ignore_auto_tag"`
+	jsoniter "github.com/json-iterator/go"
+	"github.com/stashapp/stash/pkg/fsutil"
+	"github.com/stashapp/stash/pkg/models"
+	"github.com/stashapp/stash/pkg/models/json"
+)
+
+type Studio struct {
+	Name          string           `json:"name,omitempty"`
+	URL           string           `json:"url,omitempty"`
+	ParentStudio  string           `json:"parent_studio,omitempty"`
+	Image         string           `json:"image,omitempty"`
+	CreatedAt     json.JSONTime    `json:"created_at,omitempty"`
+	UpdatedAt     json.JSONTime    `json:"updated_at,omitempty"`
+	Rating        int              `json:"rating,omitempty"`
+	Details       string           `json:"details,omitempty"`
+	Aliases       []string         `json:"aliases,omitempty"`
+	StashIDs      []models.StashID `json:"stash_ids,omitempty"`
+	IgnoreAutoTag bool             `json:"ignore_auto_tag,omitempty"`
 }
 
-type StudioFinder interface {
-	FindMany(ctx context.Context, ids []int) ([]*Studio, error)
+func (s Studio) Filename() string {
+	return fsutil.SanitiseBasename(s.Name) + ".json"
 }
 
-type StudioReader interface {
-	Find(ctx context.Context, id int) (*Studio, error)
-	StudioFinder
-	FindChildren(ctx context.Context, id int) ([]*Studio, error)
-	FindByName(ctx context.Context, name string, nocase bool) (*Studio, error)
-	FindByStashID(ctx context.Context, stashID StashID) ([]*Studio, error)
-	Count(ctx context.Context) (int, error)
-	All(ctx context.Context) ([]*Studio, error)
-	// TODO - this interface is temporary until the filter schema can fully
-	// support the query needed
-	QueryForAutoTag(ctx context.Context, words []string) ([]*Studio, error)
-	Query(ctx context.Context, studioFilter *StudioFilterType, findFilter *FindFilterType) ([]*Studio, int, error)
-	GetImage(ctx context.Context, studioID int) ([]byte, error)
-	HasImage(ctx context.Context, studioID int) (bool, error)
-	StashIDLoader
-	GetAliases(ctx context.Context, studioID int) ([]string, error)
+func LoadStudioFile(filePath string) (*Studio, error) {
+	var studio Studio
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	var json = jsoniter.ConfigCompatibleWithStandardLibrary
+	jsonParser := json.NewDecoder(file)
+	err = jsonParser.Decode(&studio)
+	if err != nil {
+		return nil, err
+	}
+	return &studio, nil
 }
 
-type StudioWriter interface {
-	Create(ctx context.Context, newStudio Studio) (*Studio, error)
-	Update(ctx context.Context, updatedStudio StudioPartial) (*Studio, error)
-	UpdateFull(ctx context.Context, updatedStudio Studio) (*Studio, error)
-	Destroy(ctx context.Context, id int) error
-	UpdateImage(ctx context.Context, studioID int, image []byte) error
-	DestroyImage(ctx context.Context, studioID int) error
-	UpdateStashIDs(ctx context.Context, studioID int, stashIDs []StashID) error
-	UpdateAliases(ctx context.Context, studioID int, aliases []string) error
-}
-
-type StudioReaderWriter interface {
-	StudioReader
-	StudioWriter
+func SaveStudioFile(filePath string, studio *Studio) error {
+	if studio == nil {
+		return fmt.Errorf("studio must not be nil")
+	}
+	return marshalToFile(filePath, studio)
 }
