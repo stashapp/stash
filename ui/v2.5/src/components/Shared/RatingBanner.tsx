55db1eb817a0a0f07164f71673d5e508410cdd6d
skier233 <benchy222@gmail.com>
2022-09-08 18:58:33 -0400

    add 20 options to gradient

--- ui/v2.5/src/components/Shared/RatingBanner.tsx
+++ ui/v2.5/src/components/Shared/RatingBanner.tsx
@@ -7,7 +7,7 @@ interface IProps {
 
 export const RatingBanner: React.FC<IProps> = ({ rating }) =>
   rating ? (
-    <div className={`rating-banner rating-${Math.trunc(rating / 10)}`}>
+    <div className={`rating-banner rating-${Math.trunc(rating / 5)}`}>
       <FormattedMessage id="rating" />: {rating}
     </div>
   ) : (
