e25a9fe67a9baa5ca6fc1114125ed289130b8321
DingDongSoLong4 <99329275+DingDongSoLong4@users.noreply.github.com>
2022-11-12 03:08:24 +0200

    Update vite

--- ui/v2.5/src/globals.d.ts
+++ ui/v2.5/src/globals.d.ts
@@ -7,16 +7,11 @@ declare module "*.md" {
   export default src;
 }
 
-/* eslint-disable  @typescript-eslint/naming-convention */
-interface ImportMetaEnv extends Readonly<Record<string, string>> {
+/* eslint-disable-next-line  @typescript-eslint/naming-convention */
+interface ImportMetaEnv {
   readonly VITE_APP_GITHASH?: string;
   readonly VITE_APP_STASH_VERSION?: string;
   readonly VITE_APP_DATE?: string;
   readonly VITE_APP_PLATFORM_PORT?: string;
   readonly VITE_APP_HTTPS?: string;
 }
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
-/* eslint-enable  @typescript-eslint/no-unused-vars */
