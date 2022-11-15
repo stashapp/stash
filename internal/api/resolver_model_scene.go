dc820e29af36c698755db3dfb3a36ebd96a48dc7
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2022-11-16 09:45:45 +1100

    Revert removal of chapters_vtt from ScenePathsType (#3128)

--- internal/api/resolver_model_scene.go
+++ internal/api/resolver_model_scene.go
@@ -186,6 +186,7 @@ func (r *sceneResolver) Paths(ctx context.Context, obj *models.Scene) (*ScenePat
 	webpPath := builder.GetStreamPreviewImageURL()
 	vttPath := builder.GetSpriteVTTURL()
 	spritePath := builder.GetSpriteURL()
+	chaptersVttPath := builder.GetChaptersVTTURL()
 	funscriptPath := builder.GetFunscriptURL()
 	captionBasePath := builder.GetCaptionURL()
 	interactiveHeatmap := builder.GetInteractiveHeatmapURL()
@@ -196,6 +197,7 @@ func (r *sceneResolver) Paths(ctx context.Context, obj *models.Scene) (*ScenePat
 		Stream:             &streamPath,
 		Webp:               &webpPath,
 		Vtt:                &vttPath,
+		ChaptersVtt:        &chaptersVttPath,
 		Sprite:             &spritePath,
 		Funscript:          &funscriptPath,
 		InteractiveHeatmap: &interactiveHeatmap,
