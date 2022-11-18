420c6fa9d757accdb8353d65215f2834220fb73e
DingDongSoLong4 <99329275+DingDongSoLong4@users.noreply.github.com>
2022-11-18 05:39:30 +0200

    Fix duplicate log messages (#3116)

--- ui/v2.5/src/core/StashService.ts
+++ ui/v2.5/src/core/StashService.ts
@@ -985,6 +985,12 @@ export const useLogs = () =>
     fetchPolicy: "no-cache",
   });
 
+export const queryLogs = () =>
+  client.query<GQL.LogsQuery>({
+    query: GQL.LogsDocument,
+    fetchPolicy: "no-cache",
+  });
+
 export const useJobQueue = () =>
   GQL.useJobQueueQuery({
     fetchPolicy: "no-cache",
