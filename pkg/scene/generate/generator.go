f8291235bd41d4fedd34d9ba0958671235d23c98
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2022-11-21 16:41:28 +1100

    Check for empty file in generateFile

--- pkg/scene/generate/generator.go
+++ pkg/scene/generate/generator.go
@@ -83,6 +83,16 @@ func (g Generator) generateFile(lockCtx *fsutil.LockContext, p Paths, pattern st
 		return err
 	}
 
+	// check if generated empty file
+	stat, err := os.Stat(tmpFn)
+	if err != nil {
+		return fmt.Errorf("error getting file stat: %w", err)
+	}
+
+	if stat.Size() == 0 {
+		return fmt.Errorf("ffmpeg command produced no output")
+	}
+
 	if err := fsutil.SafeMove(tmpFn, output); err != nil {
 		return fmt.Errorf("moving %s to %s", tmpFn, output)
 	}
