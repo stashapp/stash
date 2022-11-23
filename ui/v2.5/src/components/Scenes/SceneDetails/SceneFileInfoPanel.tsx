307233311891f695ad8279e4f2b86c0a226ba180
puc9 <51006296+puc9@users.noreply.github.com>
2022-11-22 22:30:51 -0800

    Adding info about duplicate files in various places in the UI (#3054)
    * Add counter to File Info where file count > 1
    * Add file modification time to File Info panel
    * Remove duplicate intl keys
    * Add file count to duplicate checker
    Co-authored-by: WithoutPants <53250216+WithoutPants@users.noreply.github.com>

--- ui/v2.5/src/components/Scenes/SceneDetails/SceneFileInfoPanel.tsx
+++ ui/v2.5/src/components/Scenes/SceneDetails/SceneFileInfoPanel.tsx
@@ -1,6 +1,11 @@
 import React, { useMemo, useState } from "react";
 import { Accordion, Button, Card } from "react-bootstrap";
-import { FormattedMessage, FormattedNumber, useIntl } from "react-intl";
+import {
+  FormattedMessage,
+  FormattedNumber,
+  FormattedTime,
+  useIntl,
+} from "react-intl";
 import { useHistory } from "react-router-dom";
 import { TruncatedText } from "src/components/Shared";
 import DeleteFilesDialog from "src/components/Shared/DeleteFilesDialog";
@@ -87,6 +92,13 @@ const FileInfoPanel: React.FC<IFileInfoPanelProps> = (
           truncate
         />
         {renderFileSize()}
+        <TextField id="file_mod_time">
+          <FormattedTime
+            dateStyle="medium"
+            timeStyle="medium"
+            value={props.file.mod_time ?? 0}
+          />
+        </TextField>
         <TextField
           id="duration"
           value={TextUtils.secondsToTimestamp(props.file.duration ?? 0)}
