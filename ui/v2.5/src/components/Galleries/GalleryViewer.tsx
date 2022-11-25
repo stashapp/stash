57ad12e43b121ad134f8a7696acc2d915057cde5
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2022-11-25 11:14:50 +1100

    Improve gallery performance (#3183)
    * Improve cover resolver performance
    * Deprecate and remove usage of slow gallery images

--- ui/v2.5/src/components/Galleries/GalleryViewer.tsx
+++ ui/v2.5/src/components/Galleries/GalleryViewer.tsx
@@ -1,17 +1,49 @@
-import React from "react";
-import { useFindGallery } from "src/core/StashService";
+import React, { useMemo } from "react";
 import { useLightbox } from "src/hooks";
 import { LoadingIndicator } from "src/components/Shared";
 import "flexbin/flexbin.css";
+import {
+  CriterionModifier,
+  useFindImagesQuery,
+} from "src/core/generated-graphql";
 
 interface IProps {
   galleryId: string;
 }
 
 export const GalleryViewer: React.FC<IProps> = ({ galleryId }) => {
-  const { data, loading } = useFindGallery(galleryId);
-  const images = data?.findGallery?.images ?? [];
-  const showLightbox = useLightbox({ images, showNavigation: false });
+  // TODO - add paging - don't load all images at once
+  const pageSize = -1;
+
+  const currentFilter = useMemo(() => {
+    return {
+      per_page: pageSize,
+      sort: "path",
+    };
+  }, [pageSize]);
+
+  const { data, loading } = useFindImagesQuery({
+    variables: {
+      filter: currentFilter,
+      image_filter: {
+        galleries: {
+          modifier: CriterionModifier.Includes,
+          value: [galleryId],
+        },
+      },
+    },
+  });
+
+  const images = useMemo(() => data?.findImages?.images ?? [], [data]);
+
+  const lightboxState = useMemo(() => {
+    return {
+      images,
+      showNavigation: false,
+    };
+  }, [images]);
+
+  const showLightbox = useLightbox(lightboxState);
 
   if (loading) return <LoadingIndicator />;
 
