35019a6fa80e42cebac9d38d8c7b712df8c0bcde
CJ <72030708+Teda1@users.noreply.github.com>
2023-04-03 21:54:13 -0500

    ran prettier

--- ui/v2.5/src/components/Scenes/SceneDetails/SceneVideoFilterPanel.tsx
+++ ui/v2.5/src/components/Scenes/SceneDetails/SceneVideoFilterPanel.tsx
@@ -110,9 +110,10 @@ export const SceneVideoFilterPanel: React.FC<ISceneVideoFilterPanelProps> = (
 
   function updateVideoStyle() {
     const playerVideoContainer = document.getElementById(VIDEO_PLAYER_ID);
-    const videoElements = playerVideoContainer?.getElementsByTagName("canvas") ??
-    playerVideoContainer?.getElementsByTagName("video") ??
-    [];
+    const videoElements =
+      playerVideoContainer?.getElementsByTagName("canvas") ??
+      playerVideoContainer?.getElementsByTagName("video") ??
+      [];
     const playerVideoElement =
       videoElements.length > 0 ? videoElements[0] : null;
 
