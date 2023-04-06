d3f3774eb3c7f678073e17344ee5cba5b3cf73f3
CJ <72030708+Teda1@users.noreply.github.com>
2023-04-05 21:41:52 -0500

    added random sort order

--- ui/v2.5/src/utils/dlnaVideoSort.ts
+++ ui/v2.5/src/utils/dlnaVideoSort.ts
@@ -1,6 +1,7 @@
 export enum VideoSortOrder {
   Created_At = "created_at",
   Date = "date",
+  Random = "random",
   Title = "title",
   Updated_At = "updated_at",
 }
@@ -10,6 +11,7 @@ export const defaultVideoSort = VideoSortOrder.Title;
 export const videoSortOrderIntlMap = new Map<VideoSortOrder, string>([
   [VideoSortOrder.Created_At, "created_at"],
   [VideoSortOrder.Date, "date"],
+  [VideoSortOrder.Random, "random"],
   [VideoSortOrder.Title, "title"],
   [VideoSortOrder.Updated_At, "updated_at"],
 ]);
