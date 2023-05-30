c10a224201935bdc1b0ab33fc0745ff5b3268837
Nodude <75137537+NodudeWasTaken@users.noreply.github.com>
2023-05-30 19:08:06 +0200

    Moved isSafari out of the filter loop. Added isDirect as a function.

--- ui/v2.5/src/components/ScenePlayer/ScenePlayer.tsx
+++ ui/v2.5/src/components/ScenePlayer/ScenePlayer.tsx
@@ -477,31 +477,31 @@ export const ScenePlayer: React.FC<IScenePlayerProps> = ({
     };
     player.mobileUi(mobileUiOptions);
 
+    function isDirect(src: URL) {
+        return src.pathname.endsWith("/stream") ||
+           src.pathname.endsWith("/stream.mpd") ||
+           src.pathname.endsWith("/stream.m3u8");
+    }
+
     const { duration } = file;
     const sourceSelector = player.sourceSelector();
+    const isSafari = UAParser().browser.name?.includes("Safari");
     sourceSelector.setSources(
       scene.sceneStreams
         .filter((stream) => {
           const src = new URL(stream.url);
-          const isFileTranscode =
-            src.pathname.endsWith("/stream.mp4") ||
-            src.pathname.endsWith("/stream.webm");
-          const isSafari = UAParser().browser.name?.includes("Safari");
+          const isFileTranscode = !isDirect(src);
 
           return !(isFileTranscode && isSafari);
         })
         .map((stream) => {
           const src = new URL(stream.url);
-          const isDirect =
-            src.pathname.endsWith("/stream") ||
-            src.pathname.endsWith("/stream.mpd") ||
-            src.pathname.endsWith("/stream.m3u8");
 
           return {
             src: stream.url,
             type: stream.mime_type ?? undefined,
             label: stream.label ?? undefined,
-            offset: !isDirect,
+            offset: !isDirect(src),
             duration,
           };
         })
