1cca7699c092cd9bc76e39f6ec9b3f40201b3f21
DingDongSoLong4 <99329275+DingDongSoLong4@users.noreply.github.com>
2023-06-02 03:33:13 +0200

    Update unsupported modifiers

--- ui/v2.5/src/models/list-filter/criteria/tags.ts
+++ ui/v2.5/src/models/list-filter/criteria/tags.ts
@@ -1,14 +1,17 @@
 import { CriterionModifier } from "src/core/generated-graphql";
 import { CriterionOption, IHierarchicalLabeledIdCriterion } from "./criterion";
 
-// TODO: should be modifierOptions for all CriterionOptions below but
-// the Equals modifier only works for the "tags" and "sceneTags" criterion.
-// Replace all modifierOptions below when the Equals modifier works
-// everywhere and add the NotEquals modifier here when that works.
 const modifierOptions = [
   CriterionModifier.IncludesAll,
   CriterionModifier.Includes,
   CriterionModifier.Equals,
+  CriterionModifier.IsNull,
+  CriterionModifier.NotNull,
+];
+
+const withoutEqualsModifierOptions = [
+  CriterionModifier.IncludesAll,
+  CriterionModifier.Includes,
   CriterionModifier.IsNull,
   CriterionModifier.NotNull,
 ];
@@ -33,36 +36,21 @@ export const PerformerTagsCriterionOption = new CriterionOption({
   messageID: "performerTags",
   type: "performerTags",
   parameterName: "performer_tags",
-  modifierOptions: [
-    CriterionModifier.IncludesAll,
-    CriterionModifier.Includes,
-    CriterionModifier.IsNull,
-    CriterionModifier.NotNull,
-  ],
+  modifierOptions: withoutEqualsModifierOptions,
   defaultModifier,
 });
 export const ParentTagsCriterionOption = new CriterionOption({
   messageID: "parent_tags",
   type: "parentTags",
   parameterName: "parents",
-  modifierOptions: [
-    CriterionModifier.IncludesAll,
-    CriterionModifier.Includes,
-    CriterionModifier.IsNull,
-    CriterionModifier.NotNull,
-  ],
+  modifierOptions: withoutEqualsModifierOptions,
   defaultModifier,
 });
 export const ChildTagsCriterionOption = new CriterionOption({
   messageID: "sub_tags",
   type: "childTags",
   parameterName: "children",
-  modifierOptions: [
-    CriterionModifier.IncludesAll,
-    CriterionModifier.Includes,
-    CriterionModifier.IsNull,
-    CriterionModifier.NotNull,
-  ],
+  modifierOptions: withoutEqualsModifierOptions,
   defaultModifier,
 });
 
