aceaabd90135c97512124336f7b6e81d760f4a89
Herpes3000 <61895715+Herpes3000@users.noreply.github.com>
2023-06-20 17:55:56 +0300

    Migration to add the tables scenes_odates and scenes_playdates and populate them
    Migration creates two new tables in the SQL to track the dates of plays and o's and then populates them with info from the 'o_counter', 'play_count' and 'last_played_at' columns from 'scenes'. The most recent playdate utilises the 'last_played_at' date, all other dates default to 2020-02-24 (the first release of Stash v0.1.0)

---
+++ pkg/sqlite/migrations/47_manual_history.up.sql
@@ -0,0 +1,51 @@
+CREATE TABLE `scenes_playdates` (
+  `id` integer not null primary key autoincrement,
+  `scene_id` integer,
+  `playdate` datetime not null,
+  foreign key(`scene_id`) references `scenes`(`id`)
+);
+
+CREATE TABLE `scenes_odates` (
+  `id` integer not null primary key autoincrement,
+  `scene_id` integer,
+  `odate` datetime not null,
+  foreign key(`scene_id`) references `scenes`(`id`)
+);
+
+WITH max_play_count AS (
+  SELECT MAX(play_count) AS max_count
+  FROM scenes
+)
+, numbers AS (
+  SELECT 1 AS n
+  FROM max_play_count
+  UNION ALL
+  SELECT n + 1
+  FROM numbers
+  WHERE n < (SELECT max_count FROM max_play_count)
+)
+INSERT INTO scenes_playdates (scene_id, playdate)
+SELECT scenes.id, 
+       CASE 
+         WHEN numbers.n = 1 THEN scenes.last_played_at
+         ELSE '2020-02-24'
+       END AS playdate
+FROM scenes
+JOIN numbers
+WHERE numbers.n <= scenes.play_count;
+
+WITH numbers AS (
+  SELECT 1 AS n
+  UNION ALL
+  SELECT n + 1
+  FROM numbers
+  WHERE n < (SELECT MAX(o_counter) FROM scenes)
+)
+INSERT INTO scenes_odates (scene_id, odate)
+SELECT scenes.id, 
+       CASE 
+         WHEN numbers.n <= scenes.o_counter THEN '2020-02-24'
+       END AS odate
+FROM scenes
+CROSS JOIN numbers
+WHERE numbers.n <= scenes.o_counter;
