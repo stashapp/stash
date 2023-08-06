91ef32adafa2610786c444532c5bb6fa94b5ef78
CJ <tedabed@gmail.com>
2023-08-06 18:11:26 -0500

    updated apple.ts code to use UAParser

--- ui/v2.5/src/utils/apple.ts
+++ ui/v2.5/src/utils/apple.ts
@@ -1,6 +1,11 @@
+import UAParser from "ua-parser-js";
+
 export function isPlatformUniquelyRenderedByApple() {
+  // OS name on iPads show up as iOS or Max OS depending on the browser.
+  const isiOS = UAParser().os.name?.includes("iOS");
+  const isMacOS = UAParser().os.name?.includes("Mac OS");
+  const isSafari = UAParser().browser.name?.includes("Safari");
   return (
-    /(ipad)/i.test(navigator.userAgent) ||
-    /(macintosh.*safari)/i.test(navigator.userAgent)
+    isiOS || (isMacOS && isSafari)
   );
 }
