4883d15745f527c393fe4f959f7bca107fe072f9
DingDongSoLong4 <99329275+DingDongSoLong4@users.noreply.github.com>
2023-08-12 17:23:23 +0200

    Replace deprecated substr()

--- ui/v2.5/src/utils/text.ts
+++ ui/v2.5/src/utils/text.ts
@@ -157,15 +157,15 @@ const fileSizeFractionalDigits = (unit: Unit) => {
 };
 
 const secondsToTimestamp = (seconds: number) => {
-  let ret = new Date(seconds * 1000).toISOString().substr(11, 8);
+  let ret = new Date(seconds * 1000).toISOString().substring(11, 19);
 
   if (ret.startsWith("00")) {
     // strip hours if under one hour
-    ret = ret.substr(3);
+    ret = ret.substring(3);
   }
   if (ret.startsWith("0")) {
     // for duration under a minute, leave one leading zero
-    ret = ret.substr(1);
+    ret = ret.substring(1);
   }
   return ret;
 };
