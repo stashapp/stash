b1aca0950afe5327c94ed304cfe3a4b1f23f7617
its-josh4 <74079536+its-josh4@users.noreply.github.com>
2023-09-10 14:54:03 -0700

    Improve error reporting when moving a transcoded file fails

--- pkg/fsutil/file.go
+++ pkg/fsutil/file.go
@@ -59,9 +59,9 @@ func SafeMove(src, dst string) error {
 	err := os.Rename(src, dst)
 
 	if err != nil {
-		err = CopyFile(src, dst)
-		if err != nil {
-			return err
+		copyErr := CopyFile(src, dst)
+		if copyErr != nil {
+			return fmt.Errorf("copying file failed during SaveMove with %w; renaming file failed previously with %w", copyErr, err)
 		}
 
 		err = os.Remove(src)
