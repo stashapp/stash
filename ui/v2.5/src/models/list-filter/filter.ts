1d39a39b26707657312a76473be265b3e95d0bb4
yoshnopa <usingusenet@protonmail.com>
2023-09-26 22:23:27 +0200

    Default Filter Links & Details // Fix Errors on subpages with undefined filter

--- ui/v2.5/src/models/list-filter/filter.ts
+++ ui/v2.5/src/models/list-filter/filter.ts
@@ -489,6 +489,7 @@ const recursiveRenameToSnakeCase = (
 
 export const useDefaultLinkFilter = (mode: GQL.FilterMode) => {
   let filter = new ListFilterModel(mode, undefined);
+  filter.sortBy = "name";
   let { configuration: config } = useContext(ConfigurationContext);
   if (config?.ui?.linkFilters?.[mode.toLowerCase()]) {
     let linkFilter = recursiveRenameToSnakeCase(
@@ -504,6 +505,5 @@ export const useDefaultLinkFilter = (mode: GQL.FilterMode) => {
     filter.currentPage = 1;
     filter.randomSeed = -1;
   }
-  console.log(filter);
   return filter;
 };
