b99d16b71261c07bc204b9fcae90c78671e091e0
its-josh4 <74079536+its-josh4@users.noreply.github.com>
2023-10-22 14:20:41 -0700

    Some cosmetic fixes from #4106 (#4236)

--- internal/desktop/systray_nixes.go
+++ internal/desktop/systray_nixes.go
@@ -1,10 +1,11 @@
-//go:build !windows && !darwin
-// +build !windows,!darwin
+//go:build (!windows && !darwin) || !cgo
 
 package desktop
 
 func startSystray(shutdownHandler ShutdownHandler, favicon FaviconProvider) {
-	// The systray is not available on linux because the required libraries (libappindicator3 and gtk+3.0)
+	// The systray is not available on Linux because the required libraries (libappindicator3 and gtk+3.0)
 	// are not able to be statically compiled. Technically, the systray works perfectly fine when dynamically
 	// linked, but we cannot distribute it for compatibility reasons.
+	// Additionally, the systray package requires CGo so the dependency cannot be used if building with
+	// CGo disabled.
 }
