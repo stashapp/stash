86938fde833fb1567f1cc2c74a9bd270e8130e94
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2024-01-09 11:39:00 +1100

    Don't bail on error when scraping all (#4442)

--- ui/v2.5/src/utils/errors.ts
+++ ui/v2.5/src/utils/errors.ts
@@ -2,3 +2,18 @@ import { ApolloError } from "@apollo/client";
 
 export const apolloError = (error: unknown) =>
   error instanceof ApolloError ? error.message : "";
+
+export function errorToString(error: unknown) {
+  let message;
+  if (error instanceof Error) {
+    message = error.message;
+  }
+  if (!message) {
+    message = String(error);
+  }
+  if (!message) {
+    message = "Unknown error";
+  }
+
+  return message;
+}
