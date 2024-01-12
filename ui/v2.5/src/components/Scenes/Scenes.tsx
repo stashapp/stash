7a149633330fa96e732cc541385f67a78a43abc2
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2024-01-12 19:02:28 +1100

    Use new page in /scenes

--- ui/v2.5/src/components/Scenes/Scenes.tsx
+++ ui/v2.5/src/components/Scenes/Scenes.tsx
@@ -5,8 +5,8 @@ import { useTitleProps } from "src/hooks/title";
 import { PersistanceLevel } from "../List/ItemList";
 import { lazyComponent } from "src/utils/lazyComponent";
 import { useScrollToTopOnMount } from "src/hooks/scrollToTop";
-import ScenesPage from "./ScenesPage";
 
+const ScenesPage = lazyComponent(() => import("./ScenesPage"));
 const SceneList = lazyComponent(() => import("./SceneList"));
 const SceneMarkerList = lazyComponent(() => import("./SceneMarkerList"));
 const Scene = lazyComponent(() => import("./SceneDetails/Scene"));
@@ -36,8 +36,8 @@ const SceneRoutes: React.FC = () => {
     <>
       <Helmet {...titleProps} />
       <Switch>
-        <Route exact path="/scenes" component={Scenes} />
-        <Route exact path="/scenes/proto" component={ScenesPage} />
+        <Route exact path="/scenes" component={ScenesPage} />
+        <Route exact path="/scenes/old" component={Scenes} />
         <Route exact path="/scenes/markers" component={SceneMarkers} />
         <Route exact path="/scenes/new" component={SceneCreate} />
         <Route path="/scenes/:id" component={Scene} />
