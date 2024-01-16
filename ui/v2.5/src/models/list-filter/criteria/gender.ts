75099b38a81624981f2999c7e3ed057778792086
OFP <134338272+ofpcode@users.noreply.github.com>
2024-01-15 22:47:33 -0500

    Update Performer gender filter (#4419)
    * Update Performer gender filter
    
    Updated the Performer gender filter to allow selection of multiple genders (`IS`) or performers with no gender specified (`IS NULL`).
    
    * Add default modifier for circumcised
    * Handle existing saved filters
    ---------
    Co-authored-by: WithoutPants <53250216+WithoutPants@users.noreply.github.com>

--- ui/v2.5/src/models/list-filter/criteria/gender.ts
+++ ui/v2.5/src/models/list-filter/criteria/gender.ts
@@ -1,23 +1,54 @@
-import { GenderCriterionInput } from "src/core/generated-graphql";
+import {
+  CriterionModifier,
+  GenderCriterionInput,
+  GenderEnum,
+} from "src/core/generated-graphql";
 import { genderStrings, stringToGender } from "src/utils/gender";
-import { CriterionOption, StringCriterion } from "./criterion";
+import {
+  CriterionOption,
+  IEncodedCriterion,
+  MultiStringCriterion,
+} from "./criterion";
 
 export const GenderCriterionOption = new CriterionOption({
   messageID: "gender",
   type: "gender",
   options: genderStrings,
+  modifierOptions: [
+    CriterionModifier.Includes,
+    CriterionModifier.Excludes,
+    CriterionModifier.IsNull,
+    CriterionModifier.NotNull,
+  ],
+  defaultModifier: CriterionModifier.Includes,
   makeCriterion: () => new GenderCriterion(),
 });
 
-export class GenderCriterion extends StringCriterion {
+export class GenderCriterion extends MultiStringCriterion {
   constructor() {
     super(GenderCriterionOption);
   }
 
   protected toCriterionInput(): GenderCriterionInput {
+    const value = this.value.map((v) => stringToGender(v)) as GenderEnum[];
+
     return {
-      value: stringToGender(this.value),
+      value_list: value,
       modifier: this.modifier,
     };
+  }
+
+  public setFromEncodedCriterion(
+    encodedCriterion: IEncodedCriterion<string[]>
+  ) {
+    // backwards compatibility - if the value is a string, convert it to an array
+    if (typeof encodedCriterion.value === "string") {
+      encodedCriterion = {
+        ...encodedCriterion,
+        value: [encodedCriterion.value],
+      };
+    }
+
+    super.setFromEncodedCriterion(encodedCriterion);
   }
 }
