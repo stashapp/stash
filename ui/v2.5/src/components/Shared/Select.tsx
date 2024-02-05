5bf42bed1ff6a1a60d9613ac486356f7a9bcde9e
CJ <72030708+cj12312021@users.noreply.github.com>
2024-02-05 17:18:24 -0600

    Improve list table readability (#4497)
    * fixes mandatory columns bug and consistency issues

--- ui/v2.5/src/components/Shared/Select.tsx
+++ ui/v2.5/src/components/Shared/Select.tsx
@@ -843,7 +843,7 @@ export const ListSelect = <T extends {}>(props: IListSelect<T>) => {
 };
 
 type DisableOption = Option & {
-  disabled?: boolean;
+  isDisabled?: boolean;
 };
 
 interface ICheckBoxSelectProps {
@@ -861,7 +861,7 @@ export const CheckBoxSelect: React.FC<ICheckBoxSelectProps> = ({
     <reactSelectComponents.Option {...props}>
       <input
         type="checkbox"
-        disabled={props.data.disabled}
+        disabled={props.isDisabled}
         checked={props.isSelected}
         onChange={() => null}
         className="mr-1"
