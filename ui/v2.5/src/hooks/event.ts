5d6b87e9e9e6c6ddaa52279a22ca639fa54e9db4
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2024-02-06 12:56:29 +1100

    Lint

--- ui/v2.5/src/hooks/event.ts
+++ ui/v2.5/src/hooks/event.ts
@@ -1,8 +1,4 @@
 class StashEvent extends EventTarget {
-  constructor() {
-    super();
-  }
-
   dispatch(event: string, id?: string, data?: object) {
     this.dispatchEvent(
       new CustomEvent(`stash:${event}${id ? `:${id}` : ""}`, {
