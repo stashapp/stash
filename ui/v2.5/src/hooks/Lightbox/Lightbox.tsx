a6a7b4bacc7a0b0780651b1db76b276dc3a96401
yoshnopa <usingusenet@protonmail.com>
2024-02-09 22:29:41 +0100

    Bugfix: No Image Upscale for Clips

--- ui/v2.5/src/hooks/Lightbox/Lightbox.tsx
+++ ui/v2.5/src/hooks/Lightbox/Lightbox.tsx
@@ -855,6 +855,8 @@ export const LightboxComponent: React.FC<IProps> = ({
                 {i >= currentIndex - 1 && i <= currentIndex + 1 ? (
                   <LightboxImage
                     src={image.paths.image ?? ""}
+                    width={image.visual_files?.[0]?.width ?? 0}
+                    height={image.visual_files?.[0]?.height ?? 0}
                     displayMode={displayMode}
                     scaleUp={lightboxSettings?.scaleUp ?? false}
                     scrollMode={
