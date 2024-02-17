04ac3a6ae998b6cc1c31663f10bc22547b504bf1
QxxxGit <thatsprettycool@protonmail.com>
2024-02-17 04:08:30 -0500

    Update image detail page

--- ui/v2.5/src/components/Images/ImageDetails/ImageDetailPanel.tsx
+++ ui/v2.5/src/components/Images/ImageDetails/ImageDetailPanel.tsx
@@ -1,4 +1,6 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
+import { Button } from "react-bootstrap";
+import { Icon } from "src/components/Shared/Icon";
 import { Link } from "react-router-dom";
 import * as GQL from "src/core/generated-graphql";
 import TextUtils from "src/utils/text";
@@ -9,49 +11,66 @@ import { RatingSystem } from "src/components/Shared/Rating/RatingSystem";
 import { sortPerformers } from "src/core/performers";
 import { FormattedDate, FormattedMessage, useIntl } from "react-intl";
 import { objectTitle } from "src/core/files";
+import {
+  faChevronDown,
+  faChevronUp
+} from "@fortawesome/free-solid-svg-icons";
+
 interface IImageDetailProps {
   image: GQL.ImageDataFragment;
 }
 
 export const ImageDetailPanel: React.FC<IImageDetailProps> = (props) => {
   const intl = useIntl();
+  const [collapsed, setCollapsed] = useState<boolean>(true);
 
   const file = useMemo(
     () => (props.image.files.length > 0 ? props.image.files[0] : undefined),
     [props.image]
   );
 
-  function renderDetails() {
+  function getCollapseButtonIcon() {
+    return collapsed ? faChevronDown : faChevronUp;
+  }
+
+  function maybeRenderDetails() {
     if (!props.image.details) return;
     return (
-      <>
-        <h6>
-          <FormattedMessage id="details" />:{" "}
-        </h6>
-        <p className="pre">{props.image.details}</p>
-      </>
+      <div className="row details-description">
+        <div className="col-12">
+          <h5>
+            <FormattedMessage id="details" />
+            <Button
+              className="minimal expand-collapse"
+              onClick={() => setCollapsed(!collapsed)}
+            >
+              <Icon className="fa-fw" icon={getCollapseButtonIcon()} />
+            </Button>
+          </h5>
+          <p className={`pre ${collapsed ? 'collapsed' : ''}`}>{props.image.details}</p>
+        </div>
+      </div>
     );
   }
 
-  function renderTags() {
+  function maybeRenderTags() {
     if (props.image.tags.length === 0) return;
     const tags = props.image.tags.map((tag) => (
       <TagLink key={tag.id} tag={tag} linkType="image" />
     ));
     return (
-      <>
-        <h6>
-          <FormattedMessage
-            id="countables.tags"
-            values={{ count: props.image.tags.length }}
-          />
-        </h6>
-        {tags}
-      </>
+      <div className="row details-tags">
+        <div className="col-12">
+          <h5>
+            <FormattedMessage id="tags" />
+          </h5>
+          {tags}
+        </div>
+      </div>
     );
   }
 
-  function renderPerformers() {
+  function maybeRenderPerformers() {
     if (props.image.performers.length === 0) return;
     const performers = sortPerformers(props.image.performers);
     const cards = performers.map((performer) => (
@@ -63,120 +82,134 @@ export const ImageDetailPanel: React.FC<IImageDetailProps> = (props) => {
     ));
 
     return (
-      <>
-        <h6>
-          <FormattedMessage
-            id="countables.performers"
-            values={{ count: props.image.performers.length }}
-          />
-        </h6>
-        <div className="row justify-content-center image-performers">
-          {cards}
+      <div className="row details-performers">
+        <div className="col-12">
+          <h5>
+            <FormattedMessage id="performers" />
+          </h5>
+          <div className="row image-performers">
+            {cards}
+          </div>
         </div>
-      </>
+      </div>
     );
   }
 
-  function renderGalleries() {
+  function maybeRenderGalleries() {
     if (props.image.galleries.length === 0) return;
     const galleries = props.image.galleries.map((gallery) => (
       <GalleryLink key={gallery.id} gallery={gallery} />
     ));
     return (
-      <>
-        <h6>
-          <FormattedMessage
-            id="countables.galleries"
-            values={{ count: props.image.galleries.length }}
-          />
-        </h6>
-        {galleries}
-      </>
+      <div className="row details-galleries">
+        <div className="col-12">
+          <h5>
+            <FormattedMessage
+              id="countables.galleries"
+              values={{ count: props.image.galleries.length }}
+            />
+          </h5>
+          {galleries}
+        </div>
+      </div>
     );
   }
-
-  // filename should use entire row if there is no studio
-  const imageDetailsWidth = props.image.studio ? "col-9" : "col-12";
-
+  
   return (
-    <>
-      <div className="row">
-        <div className={`${imageDetailsWidth} col-xl-12 image-details`}>
+    <div className="col-xl-12 details-display">
+      <div className="details-basic">
+        <div className="row">
           <div className="image-header d-xl-none">
             <h3>
               <TruncatedText text={objectTitle(props.image)} />
             </h3>
           </div>
-          {props.image.date ? (
+        </div>
+        <div className="row">
+          <div className="col-6">
             <h5>
-              <FormattedDate
-                value={props.image.date}
-                format="long"
-                timeZone="utc"
-              />
+              <FormattedMessage id="studio" />
             </h5>
-          ) : undefined}
-          {props.image.rating100 ? (
-            <h6>
-              <FormattedMessage id="rating" />:{" "}
-              <RatingSystem value={props.image.rating100} disabled />
-            </h6>
-          ) : (
-            ""
-          )}
-
-          {renderGalleries()}
-          {file?.width && file?.height ? (
-            <h6>
-              <FormattedMessage id="resolution" />:{" "}
-              {TextUtils.resolution(file.width, file.height)}
-            </h6>
-          ) : (
-            ""
-          )}
-          {
-            <h6>
-              {" "}
-              <FormattedMessage id="created_at" />:{" "}
-              {TextUtils.formatDateTime(intl, props.image.created_at)}{" "}
-            </h6>
-          }
-          {
-            <h6>
-              <FormattedMessage id="updated_at" />:{" "}
-              {TextUtils.formatDateTime(intl, props.image.updated_at)}{" "}
-            </h6>
-          }
-          {props.image.code && (
-            <h6>
-              <FormattedMessage id="scene_code" />: {props.image.code}{" "}
-            </h6>
-          )}
-          {props.image.photographer && (
-            <h6>
-              <FormattedMessage id="photographer" />: {props.image.photographer}{" "}
-            </h6>
-          )}
-        </div>
-        {props.image.studio && (
-          <div className="col-3 d-xl-none">
-            <Link to={`/studios/${props.image.studio.id}`}>
-              <img
-                src={props.image.studio.image_path ?? ""}
-                alt={`${props.image.studio.name} logo`}
-                className="studio-logo float-right"
-              />
-            </Link>
+            {props.image.studio?.name ? (
+              <h6>
+                <Link to={`/studios/${props.image.studio.id}`}>
+                  <TruncatedText text={props.image.studio.name} />
+                </Link>
+              </h6>
+            ) : (<h6>&nbsp;</h6>)}
           </div>
-        )}
-      </div>
-      <div className="row">
-        <div className="col-12">
-          {renderDetails()}
-          {renderTags()}
-          {renderPerformers()}
+          <div className="col-6">
+            <h5>
+              <FormattedMessage id="date" />
+            </h5>
+            {props.image.date ? (
+              <h6>
+                <FormattedDate
+                  value={props.image.date}
+                  format="long"
+                  timeZone="utc"
+                />
+              </h6>
+            ) : (<h6>&nbsp;</h6>)}
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-6">
+            <h5>
+              <FormattedMessage id="scene_code" />
+            </h5>
+            {props.image.code ? (
+              <h6>
+                {props.image.code}
+              </h6>
+            ) : (<h6>&nbsp;</h6>)}
+          </div>
+          <div className="col-6">
+            <h5>
+              <FormattedMessage id="rating" />
+            </h5>
+            <RatingSystem value={props.image.rating100 ?? undefined} disabled />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-6">
+            <h5>
+              <FormattedMessage id="photographer" />
+            </h5>
+            {props.image.photographer ? (
+              <h6>
+                {props.image.photographer}
+              </h6>
+            ) : (<h6>&nbsp;</h6>)}
+          </div>
+          <div className="col-6">
+            <h5>
+              <FormattedMessage id="dimensions" />
+            </h5>
+            {file?.width && file?.height ? (
+              <h6>
+                {file.width}x{file.height}
+              </h6>
+            ) : (<h6>&nbsp</h6>)}
+          </div>
         </div>
       </div>
-    </>
+      {maybeRenderGalleries()}
+      {maybeRenderTags()}
+      {maybeRenderDetails()}
+      {maybeRenderPerformers()}
+      <div className="row details-extra">
+        <div className="col-12">
+          <h6>
+            <FormattedMessage id="created_at" />:{" "}
+            {TextUtils.formatDateTime(intl, props.image.created_at)}
+          </h6>
+          <h6>
+            <FormattedMessage id="updated_at" />:{" "}
+            {TextUtils.formatDateTime(intl, props.image.updated_at)}
+          </h6>
+        </div>
+      </div>
+    </div>
   );
 };
