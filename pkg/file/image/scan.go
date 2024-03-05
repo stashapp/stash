df57a5c6e88ab63db370d2431b87427aaec28eed
dogwithakeyboard <gdkjhsahdka@protonmail.com>
2024-03-05 10:11:00 +0000

    Add bmp to image codec list

--- pkg/file/image/scan.go
+++ pkg/file/image/scan.go
@@ -69,7 +69,7 @@ func (d *Decorator) Decorate(ctx context.Context, fs models.FS, f models.File) (
 
 	isClip := true
 	// This list is derived from ffmpegImageThumbnail in pkg/image/thumbnail. If one gets updated, the other should be as well
-	for _, item := range []string{"png", "mjpeg", "webp"} {
+	for _, item := range []string{"png", "mjpeg", "webp", "bmp"} {
 		if item == probe.VideoCodec {
 			isClip = false
 		}
