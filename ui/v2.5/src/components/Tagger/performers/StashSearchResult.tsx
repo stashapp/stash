347ceaed97f842cc47072e21f0e41cf9ec198cbd
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2024-05-02 15:05:20 +1000

    Clean duplicate aliases when creating from performer tagger

--- ui/v2.5/src/components/Tagger/performers/StashSearchResult.tsx
+++ ui/v2.5/src/components/Tagger/performers/StashSearchResult.tsx
@@ -18,6 +18,18 @@ interface IStashSearchResultProps {
   excludedPerformerFields: string[];
 }
 
+// #4596 - remove any duplicate aliases or aliases that are the same as the performer's name
+function cleanAliases(currentName: string, aliases: string[]) {
+  const ret: string[] = [];
+  aliases.forEach((alias) => {
+    if (alias !== currentName && !ret.includes(alias)) {
+      ret.push(alias);
+    }
+  });
+
+  return ret;
+}
+
 const StashSearchResult: React.FC<IStashSearchResultProps> = ({
   performer,
   stashboxPerformers,
@@ -41,6 +53,10 @@ const StashSearchResult: React.FC<IStashSearchResultProps> = ({
 
     if (input.stash_ids?.length) {
       input.stash_ids = mergeStashIDs(performer.stash_ids, input.stash_ids);
+    }
+
+    if (input.alias_list) {
+      input.alias_list = cleanAliases(performer.name, input.alias_list);
     }
 
     const updateData: GQL.PerformerUpdateInput = {
