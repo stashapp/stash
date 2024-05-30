9cc7b4032b1f9f1b7c6acdd4d314e1cae931d1d1
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2024-05-30 14:22:26 +1000

    Pass absolute config path to plugins

--- pkg/plugin/plugins.go
+++ pkg/plugin/plugins.go
@@ -91,7 +91,7 @@ type PluginSetting struct {
 type ServerConfig interface {
 	GetHost() string
 	GetPort() int
-	GetConfigPath() string
+	GetConfigPathAbs() string
 	HasTLSConfig() bool
 	GetPluginsPath() string
 	GetDisabledPlugins() []string
@@ -249,7 +249,7 @@ func (c Cache) makeServerConnection(ctx context.Context) common.StashServerConne
 		Host:          c.config.GetHost(),
 		Port:          c.config.GetPort(),
 		SessionCookie: cookie,
-		Dir:           c.config.GetConfigPath(),
+		Dir:           c.config.GetConfigPathAbs(),
 	}
 
 	if c.config.HasTLSConfig() {
