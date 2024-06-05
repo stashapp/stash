00d15a7429f9e7c02760b21117cde9003b37f780
Nodude <75137537+NodudeWasTaken@users.noreply.github.com>
2024-06-06 00:42:33 +0200

    Fix hw transcoding not detecting filtering errors

--- pkg/ffmpeg/stream_transcode.go
+++ pkg/ffmpeg/stream_transcode.go
@@ -186,7 +186,7 @@ func (o TranscodeOptions) makeStreamArgs(sm *StreamManager) Args {
 
 	codec := o.FileGetCodec(sm, maxTranscodeSize)
 
-	fullhw := sm.config.GetTranscodeHardwareAcceleration() && sm.encoder.hwCanFullHWTranscode(sm.context, o.VideoFile, codec)
+	fullhw := sm.config.GetTranscodeHardwareAcceleration() && sm.encoder.hwCanFullHWTranscode(sm.context, codec, o.VideoFile, maxTranscodeSize)
 	args = sm.encoder.hwDeviceInit(args, codec, fullhw)
 	args = append(args, extraInputArgs...)
 
