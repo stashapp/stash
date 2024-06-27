e116775d60329a13d8eeda84e8de75f40879b57c
dogwithakeyboard <128322708+dogwithakeyboard@users.noreply.github.com>
2024-06-27 01:12:39 +0100

    Check for null disambiguation on validate (#5019)

--- pkg/performer/validate_test.go
+++ pkg/performer/validate_test.go
@@ -15,6 +15,9 @@ func nameFilter(n string) *models.PerformerFilterType {
 			Value:    n,
 			Modifier: models.CriterionModifierEquals,
 		},
+		Disambiguation: &models.StringCriterionInput{
+			Modifier: models.CriterionModifierIsNull,
+		},
 	}
 }
 
@@ -41,13 +44,6 @@ func TestValidateName(t *testing.T) {
 		newName     = "new name"
 		newDisambig = "new disambiguation"
 	)
-	// existing1 := models.Performer{
-	// 	Name: name1,
-	// }
-	// existing2 := models.Performer{
-	// 	Name:           name2,
-	// 	Disambiguation: disambig,
-	// }
 
 	pp := 1
 	findFilter := &models.FindFilterType{
