e16a5d2dfa95f1848b4aadc87216a62127867aad
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2024-07-03 21:20:31 +1000

    Add filter to scraper menu

--- ui/v2.5/src/components/Shared/ScraperMenu.tsx
+++ ui/v2.5/src/components/Shared/ScraperMenu.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import { Dropdown } from "react-bootstrap";
 import { FormattedMessage, useIntl } from "react-intl";
 import { Icon } from "./Icon";
 import { stashboxDisplayName } from "src/utils/stashbox";
 import { ScraperSourceInput, StashBox } from "src/core/generated-graphql";
 import { faSyncAlt } from "@fortawesome/free-solid-svg-icons";
+import { ClearableInput } from "./ClearableInput";
+
+const minFilteredScrapers = 5;
 
 export const ScraperMenu: React.FC<{
   toggle: React.ReactNode;
@@ -22,13 +25,43 @@ export const ScraperMenu: React.FC<{
   onReloadScrapers,
 }) => {
   const intl = useIntl();
+  const [filter, setFilter] = useState("");
+
+  const filteredStashboxes = useMemo(() => {
+    if (!stashBoxes) return [];
+    if (!filter) return stashBoxes;
+
+    return stashBoxes.filter((s) =>
+      s.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  }, [stashBoxes, filter]);
+
+  const filteredScrapers = useMemo(() => {
+    if (!filter) return scrapers;
+
+    return scrapers.filter(
+      (s) =>
+        s.name.toLowerCase().includes(filter.toLowerCase()) ||
+        s.id.toLowerCase().includes(filter.toLowerCase())
+    );
+  }, [scrapers, filter]);
 
   return (
-    <Dropdown title={intl.formatMessage({ id: "actions.scrape_query" })}>
+    <Dropdown
+      className="scraper-menu"
+      title={intl.formatMessage({ id: "actions.scrape_query" })}
+    >
       <Dropdown.Toggle variant={variant}>{toggle}</Dropdown.Toggle>
 
       <Dropdown.Menu>
-        {stashBoxes?.map((s, index) => (
+        {(stashBoxes?.length ?? 0) + scrapers.length > minFilteredScrapers && (
+          <ClearableInput
+            placeholder={`${intl.formatMessage({ id: "filter" })}...`}
+            value={filter}
+            setValue={setFilter}
+          />
+        )}
+        {filteredStashboxes.map((s, index) => (
           <Dropdown.Item
             key={s.endpoint}
             onClick={() =>
@@ -40,7 +73,7 @@ export const ScraperMenu: React.FC<{
             {stashboxDisplayName(s.name, index)}
           </Dropdown.Item>
         ))}
-        {scrapers.map((s) => (
+        {filteredScrapers.map((s) => (
           <Dropdown.Item
             key={s.name}
             onClick={() => onScraperClicked({ scraper_id: s.id })}
