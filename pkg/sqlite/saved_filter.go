03066d20ae6f0a02e37d174fe904737f5a2240dd
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2024-07-03 12:36:12 +1000

    Fix group saved filter querying

--- pkg/sqlite/saved_filter.go
+++ pkg/sqlite/saved_filter.go
@@ -228,10 +228,21 @@ func (qb *SavedFilterStore) getMany(ctx context.Context, q *goqu.SelectDataset) 
 func (qb *SavedFilterStore) FindByMode(ctx context.Context, mode models.FilterMode) ([]*models.SavedFilter, error) {
 	// SELECT * FROM %s WHERE mode = ? AND name != ? ORDER BY name ASC
 	table := qb.table()
-	sq := qb.selectDataset().Prepared(true).Where(
-		table.Col("mode").Eq(mode),
-		table.Col("name").Neq(savedFilterDefaultName),
-	).Order(table.Col("name").Asc())
+
+	// TODO - querying on groups needs to include movies
+	// remove this when we migrate to remove the movies filter mode in the database
+	var whereClause exp.Expression
+
+	if mode == models.FilterModeGroups || mode == models.FilterModeMovies {
+		whereClause = goqu.Or(
+			table.Col("mode").Eq(models.FilterModeGroups),
+			table.Col("mode").Eq(models.FilterModeMovies),
+		)
+	} else {
+		whereClause = table.Col("mode").Eq(mode)
+	}
+
+	sq := qb.selectDataset().Prepared(true).Where(whereClause).Order(table.Col("name").Asc())
 	ret, err := qb.getMany(ctx, sq)
 
 	if err != nil {
