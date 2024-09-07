058de216deb15def348db2d4d0e9fd22c3b21e7f
Gykes <zeniiigmaa@gmail.com>
2024-09-07 15:54:02 -0700

    Updating Reload Scrapers formatting
    Per convo with people on Discord. I have updated the Reload Scrapers UI. It now adds a button if the filter box appears and then the button extends and takes up the whole space if the filter box does not exist.
    
    The CSS is... janky at best. Not too great with it so opinions and potential fixes would be greatly appriciated.

--- ui/v2.5/src/components/Shared/ScraperMenu.tsx
+++ ui/v2.5/src/components/Shared/ScraperMenu.tsx
@@ -1,6 +1,6 @@
 import React, { useMemo, useState } from "react";
-import { Dropdown } from "react-bootstrap";
-import { FormattedMessage, useIntl } from "react-intl";
+import { Dropdown, Button } from "react-bootstrap";
+import { useIntl } from "react-intl";
 import { Icon } from "./Icon";
 import { stashboxDisplayName } from "src/utils/stashbox";
 import { ScraperSourceInput, StashBox } from "src/core/generated-graphql";
@@ -47,56 +47,79 @@ export const ScraperMenu: React.FC<{
   }, [scrapers, filter]);
 
   return (
-    <Dropdown
-      className="scraper-menu"
-      title={intl.formatMessage({ id: "actions.scrape_query" })}
-    >
-      <Dropdown.Toggle variant={variant}>{toggle}</Dropdown.Toggle>
+    <div className="scraper-menu">
+      <Dropdown
+        className="d-inline"
+        title={intl.formatMessage({ id: "actions.reload_scrapers" })}
+      >
+        <Dropdown.Toggle variant={variant}>{toggle}</Dropdown.Toggle>
 
-      <Dropdown.Menu>
-        <Dropdown.Item onClick={() => onReloadScrapers()}>
-          <span className="fa-icon">
-            <Icon icon={faSyncAlt} />
-          </span>
-          <span>
-            <FormattedMessage id="actions.reload_scrapers" />
-          </span>
-        </Dropdown.Item>
+        <Dropdown.Menu>
+          <div className="filter-container d-flex align-items-center px-2">
+            <div className="input-button-wrapper d-flex w-100">
+              {(stashBoxes?.length ?? 0) + scrapers.length >
+              minFilteredScrapers ? (
+                <>
+                  {/* Filter box is visible: render input + normal button */}
+                  <ClearableInput
+                    className="filter-input"
+                    placeholder={`${intl.formatMessage({ id: "filter" })}...`}
+                    value={filter}
+                    setValue={setFilter}
+                  />
+                  <Button
+                    variant="outline-primary"
+                    onClick={onReloadScrapers}
+                    className="refresh-button"
+                  >
+                    <Icon icon={faSyncAlt} />
+                  </Button>
+                </>
+              ) : (
+                <>
+                  {/* No filter: Stretch button to full width */}
+                  <Button
+                    variant="outline-primary"
+                    onClick={onReloadScrapers}
+                    className="refresh-button full-width-button"
+                  >
+                    <Icon icon={faSyncAlt} />
+                    <span className="ml-2">
+                      {intl.formatMessage({ id: "actions.reload_scrapers" })}
+                    </span>
+                  </Button>
+                </>
+              )}
+            </div>
+          </div>
 
-        {(stashBoxes?.length ?? 0) + scrapers.length > minFilteredScrapers && (
-          <ClearableInput
-            placeholder={`${intl.formatMessage({ id: "filter" })}...`}
-            value={filter}
-            setValue={setFilter}
-          />
-        )}
+          {filteredStashboxes.map((s, index) => (
+            <Dropdown.Item
+              key={s.endpoint}
+              onClick={() =>
+                onScraperClicked({
+                  stash_box_endpoint: s.endpoint,
+                })
+              }
+            >
+              {stashboxDisplayName(s.name, index)}
+            </Dropdown.Item>
+          ))}
 
-        {filteredStashboxes.map((s, index) => (
-          <Dropdown.Item
-            key={s.endpoint}
-            onClick={() =>
-              onScraperClicked({
-                stash_box_endpoint: s.endpoint,
-              })
-            }
-          >
-            {stashboxDisplayName(s.name, index)}
-          </Dropdown.Item>
-        ))}
+          {filteredStashboxes.length > 0 && filteredScrapers.length > 0 && (
+            <Dropdown.Divider />
+          )}
 
-        {filteredStashboxes.length > 0 && filteredScrapers.length > 0 && (
-          <Dropdown.Divider />
-        )}
-
-        {filteredScrapers.map((s) => (
-          <Dropdown.Item
-            key={s.name}
-            onClick={() => onScraperClicked({ scraper_id: s.id })}
-          >
-            {s.name}
-          </Dropdown.Item>
-        ))}
-      </Dropdown.Menu>
-    </Dropdown>
+          {filteredScrapers.map((s) => (
+            <Dropdown.Item
+              key={s.name}
+              onClick={() => onScraperClicked({ scraper_id: s.id })}
+            >
+              {s.name}
+            </Dropdown.Item>
+          ))}
+        </Dropdown.Menu>
+      </Dropdown>
+    </div>
   );
 };
