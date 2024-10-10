6452ea60a5988f2168d164c47ad2c74e895dcd25
its-josh4 <74079536+its-josh4@users.noreply.github.com>
2024-10-09 19:28:35 -0700

    Add some unit tests

--- pkg/sliceutil/collections_test.go
+++ pkg/sliceutil/collections_test.go
@@ -1,6 +1,7 @@
 package sliceutil
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -63,6 +64,82 @@ func TestSliceSame(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			got := SliceSame(tt.a, tt.b)
 			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func TestAppendUniques(t *testing.T) {
+	type args struct {
+		vs    []int
+		toAdd []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "append to empty slice",
+			args: args{
+				vs:    []int{},
+				toAdd: []int{1, 2, 3},
+			},
+			want: []int{1, 2, 3},
+		},
+		{
+			name: "append all unique values",
+			args: args{
+				vs:    []int{1, 2, 3},
+				toAdd: []int{4, 5, 6},
+			},
+			want: []int{1, 2, 3, 4, 5, 6},
+		},
+		{
+			name: "append with some duplicates",
+			args: args{
+				vs:    []int{1, 2, 3},
+				toAdd: []int{3, 4, 5},
+			},
+			want: []int{1, 2, 3, 4, 5},
+		},
+		{
+			name: "append all duplicates",
+			args: args{
+				vs:    []int{1, 2, 3},
+				toAdd: []int{1, 2, 3},
+			},
+			want: []int{1, 2, 3},
+		},
+		{
+			name: "append to nil slice",
+			args: args{
+				vs:    nil,
+				toAdd: []int{1, 2, 3},
+			},
+			want: []int{1, 2, 3},
+		},
+		{
+			name: "append empty slice",
+			args: args{
+				vs:    []int{1, 2, 3},
+				toAdd: []int{},
+			},
+			want: []int{1, 2, 3},
+		},
+		{
+			name: "append nil to slice",
+			args: args{
+				vs:    []int{1, 2, 3},
+				toAdd: nil,
+			},
+			want: []int{1, 2, 3},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := AppendUniques(tt.args.vs, tt.args.toAdd); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("AppendUniques() = %v, want %v", got, tt.want)
+			}
 		})
 	}
 }
