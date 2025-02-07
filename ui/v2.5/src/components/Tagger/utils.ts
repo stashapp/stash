232b3a216c2cafd98fcb6fb28836e0c3ee185604
javstash <admin@javstash.org>
2025-02-07 01:11:22 -0500

    update utils.ts

--- ui/v2.5/src/components/Tagger/utils.ts
+++ ui/v2.5/src/components/Tagger/utils.ts
@@ -76,9 +76,9 @@ const parseString = (input: string): string => {
       output.slice(yyyymmdd + 10).replace(/-/g, " ")
     );
 
-  const javcode = output.search(javcodeRegex);
+  const javcode_index = output.search(javcodeRegex);
   if (javcode_index !== -1) {
-    javcode_length = output.match(javcodeRegex)[1].length;
+    const javcode_length = output.match(javcodeRegex)[1].length;
     return (
       output.slice(0, javcode_index).replace(/-/g, " ") +
       output.slice(javcode_index, javcode_index + javcode_length) +
