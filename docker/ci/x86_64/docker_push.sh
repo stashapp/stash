59014f14cada3c41d3b3b85e5563e58fcc4a1499
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2025-02-28 17:56:32 +1100

    Revert "Add docker labels to have update tools be able to pull changelog/rele…" (#5688)
    This reverts commit 661e9eba5170195d20009888bca7fa5180e7d88d.

--- docker/ci/x86_64/docker_push.sh
+++ docker/ci/x86_64/docker_push.sh
@@ -2,10 +2,6 @@
 
 DOCKER_TAGS=""
 
-# usage: ./docker_push.sh <version> <tag1> <tag2> ...
-STASH_VERSION="$1"
-shift
-
 for TAG in "$@"
 do
 	DOCKER_TAGS="$DOCKER_TAGS -t stashapp/stash:$TAG"
@@ -14,10 +10,5 @@ done
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 # must build the image from dist directory
-docker buildx build \
-  --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 \
-  --build-arg "STASH_VERSION=${STASH_VERSION}" \
-  --push "${DOCKER_TAGS}" \
-  --file docker/ci/x86_64/Dockerfile \
-  dist/
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 --push $DOCKER_TAGS -f docker/ci/x86_64/Dockerfile dist/
 
