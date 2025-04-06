134c87443b7dc09b3bda987512e8cdf89142be9a
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2025-04-06 20:50:45 +1000

    Add icon to error message

--- ui/v2.5/src/components/Shared/ErrorMessage.tsx
+++ ui/v2.5/src/components/Shared/ErrorMessage.tsx
@@ -1,6 +1,8 @@
+import { faWarning } from "@fortawesome/free-solid-svg-icons";
 import React, { ReactNode } from "react";
 import { Alert } from "react-bootstrap";
 import { FormattedMessage } from "react-intl";
+import { Icon } from "./Icon";
 
 interface IProps {
   message?: React.ReactNode;
@@ -13,7 +15,10 @@ export const ErrorMessage: React.FC<IProps> = (props) => {
   return (
     <div className="ErrorMessage-container">
       <Alert variant="danger" className="ErrorMessage">
-        <Alert.Heading className="ErrorMessage-header">{message}</Alert.Heading>
+        <Alert.Heading className="ErrorMessage-header">
+          <Icon icon={faWarning} />
+          {message}
+        </Alert.Heading>
         <div className="ErrorMessage-content code">{error}</div>
       </Alert>
     </div>
