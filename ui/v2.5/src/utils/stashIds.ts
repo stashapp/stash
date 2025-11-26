6e76e7c217dd8a4cad0c8c75d393666e094e318d
Gykes <24581046+Gykes@users.noreply.github.com>
2025-11-26 14:06:36 -0800

    lint

--- ui/v2.5/src/utils/stashIds.ts
+++ ui/v2.5/src/utils/stashIds.ts
@@ -1,4 +1,4 @@
- export const getStashIDs = (
+export const getStashIDs = (
   ids?: { stash_id: string; endpoint: string; updated_at: string }[]
 ) =>
   (ids ?? []).map(({ stash_id, endpoint, updated_at }) => ({
