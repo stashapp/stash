597cb34adef6e51b52a2f8284ee6585bf3b8e424
feederbox826 <feederbox826@users.noreply.github.com>
2025-11-27 16:59:00 -0500

    add WakeLockSentinel
    prevents screen from sleeping ONLY in secure contexts (localhost, https)
    
    closes #2884

---
+++ ui/v2.5/src/components/ScenePlayer/wake-sentinel.ts
@@ -0,0 +1,59 @@
+import videojs, { VideoJsPlayer } from "video.js";
+
+class WakeSentinelPlugin extends videojs.getPlugin("plugin") {
+  public wakeLock: WakeLockSentinel | null = null;
+  public wakeLockFail: boolean = false;
+  constructor(player: VideoJsPlayer) {
+    super(player);
+
+    // listen for visibility change events
+    document.addEventListener("visibilitychange", async () => {
+      if (document.visibilityState === "visible") {
+        // reacquire the wake lock when the page becomes visible
+        await this.acquireWakeLock(false);
+      }
+    });
+
+    player.ready(async () => {
+      player.addClass("vjs-wake-sentinel");
+      await this.acquireWakeLock(true);
+    });
+
+    player.on("play", () => {
+      this.acquireWakeLock(false);
+    });
+
+    player.on("pause", () => {
+      this.wakeLock?.release();
+    });
+  }
+
+  private async acquireWakeLock(log = false): Promise<void> {
+    // if wake lock failed, don't even try
+    if (this.wakeLockFail) return
+    // check for wake lock on startup
+    if ("wakeLock" in navigator) {
+      try {
+        this.wakeLock = await navigator.wakeLock.request("screen");
+        if (log) console.log("Screen Wake Lock is active!");
+      } catch (err) {
+        if (log) console.error("Failed to obtain Screen Wake Lock:", err);
+        this.wakeLockFail = true;
+      }
+    } else {
+      if (log) console.warn("Screen Wake Lock API not supported.");
+      this.wakeLockFail = true;
+    }
+  }
+}
+
+videojs.registerPlugin("wakeSentinel", WakeSentinelPlugin);
+
+/* eslint-disable @typescript-eslint/naming-convention */
+declare module "video.js" {
+  interface VideoJsPlayer {
+    wakeSentinel: () => WakeSentinelPlugin;
+  }
+}
+
+export default WakeSentinelPlugin;
