b5a546d8bc65d301964e4082ec56792574eaec8e
Gykes <24581046+Gykes@users.noreply.github.com>
2025-12-21 20:54:00 -0800

    PerformerCreateInputs update

--- pkg/models/performer.go
+++ pkg/models/performer.go
@@ -224,6 +224,8 @@ type PerformerCreateInput struct {
 	PenisLength    *float64        `json:"penis_length"`
 	Circumcised    *CircumisedEnum `json:"circumcised"`
 	CareerLength   *string         `json:"career_length"`
+	CareerStart    *int            `json:"career_start"`
+	CareerEnd      *int            `json:"career_end"`
 	Tattoos        *string         `json:"tattoos"`
 	Piercings      *string         `json:"piercings"`
 	Aliases        *string         `json:"aliases"`
@@ -263,6 +265,8 @@ type PerformerUpdateInput struct {
 	PenisLength    *float64        `json:"penis_length"`
 	Circumcised    *CircumisedEnum `json:"circumcised"`
 	CareerLength   *string         `json:"career_length"`
+	CareerStart    *int            `json:"career_start"`
+	CareerEnd      *int            `json:"career_end"`
 	Tattoos        *string         `json:"tattoos"`
 	Piercings      *string         `json:"piercings"`
 	Aliases        *string         `json:"aliases"`
