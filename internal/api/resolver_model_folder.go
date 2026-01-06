dd1c89e6f1824214f4595882e036433ca9ec6099
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2026-01-06 15:39:33 +1100

    Add basename field to folder

--- internal/api/resolver_model_folder.go
+++ internal/api/resolver_model_folder.go
@@ -2,10 +2,15 @@ package api
 
 import (
 	"context"
+	"path/filepath"
 
 	"github.com/stashapp/stash/internal/api/loaders"
 	"github.com/stashapp/stash/pkg/models"
 )
+
+func (r *folderResolver) Basename(ctx context.Context, obj *models.Folder) (string, error) {
+	return filepath.Base(obj.Path), nil
+}
 
 func (r *folderResolver) ParentFolder(ctx context.Context, obj *models.Folder) (*models.Folder, error) {
 	if obj.ParentFolderID == nil {
