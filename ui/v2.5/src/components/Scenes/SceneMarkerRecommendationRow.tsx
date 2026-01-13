45df8647219b8d6b36bd64bfa86be7db8fba72bb
ValkyrJS <valkyr.photos@gmail.com>
2026-01-13 17:09:06 +0000

    Patched SceneMarkerRecommendationRow component

--- ui/v2.5/src/components/Scenes/SceneMarkerRecommendationRow.tsx
+++ ui/v2.5/src/components/Scenes/SceneMarkerRecommendationRow.tsx
@@ -7,6 +7,7 @@ import { getSlickSliderSettings } from "src/core/recommendations";
 import { RecommendationRow } from "../FrontPage/RecommendationRow";
 import { FormattedMessage } from "react-intl";
 import { SceneMarkerCard } from "./SceneMarkerCard";
+import { PatchComponent } from "src/patch";
 
 interface IProps {
   isTouch: boolean;
@@ -14,46 +15,51 @@ interface IProps {
   header: string;
 }
 
-export const SceneMarkerRecommendationRow: React.FC<IProps> = (props) => {
-  const result = useFindSceneMarkers(props.filter);
-  const cardCount = result.data?.findSceneMarkers.count;
+export const SceneMarkerRecommendationRow: React.FC<IProps> = PatchComponent(
+  "SceneMarkerRecommendationRow",
+  (props) => {
+    const result = useFindSceneMarkers(props.filter);
+    const cardCount = result.data?.findSceneMarkers.count;
 
-  if (!result.loading && !cardCount) {
-    return null;
-  }
+    if (!result.loading && !cardCount) {
+      return null;
+    }
 
-  return (
-    <RecommendationRow
-      className="scene-marker-recommendations"
-      header={props.header}
-      link={
-        <Link to={`/scenes/markers?${props.filter.makeQueryParameters()}`}>
-          <FormattedMessage id="view_all" />
-        </Link>
-      }
-    >
-      <Slider
-        {...getSlickSliderSettings(
-          cardCount ? cardCount : props.filter.itemsPerPage,
-          props.isTouch
-        )}
+    return (
+      <RecommendationRow
+        className="scene-marker-recommendations"
+        header={props.header}
+        link={
+          <Link to={`/scenes/markers?${props.filter.makeQueryParameters()}`}>
+            <FormattedMessage id="view_all" />
+          </Link>
+        }
       >
-        {result.loading
-          ? [...Array(props.filter.itemsPerPage)].map((i) => (
-              <div
-                key={`_${i}`}
-                className="scene-marker-skeleton skeleton-card"
-              ></div>
-            ))
-          : result.data?.findSceneMarkers.scene_markers.map((marker, index) => (
-              <SceneMarkerCard
-                key={marker.id}
-                marker={marker}
-                index={index}
-                zoomIndex={1}
-              />
-            ))}
-      </Slider>
-    </RecommendationRow>
-  );
-};
+        <Slider
+          {...getSlickSliderSettings(
+            cardCount ? cardCount : props.filter.itemsPerPage,
+            props.isTouch
+          )}
+        >
+          {result.loading
+            ? [...Array(props.filter.itemsPerPage)].map((i) => (
+                <div
+                  key={`_${i}`}
+                  className="scene-marker-skeleton skeleton-card"
+                ></div>
+              ))
+            : result.data?.findSceneMarkers.scene_markers.map(
+                (marker, index) => (
+                  <SceneMarkerCard
+                    key={marker.id}
+                    marker={marker}
+                    index={index}
+                    zoomIndex={1}
+                  />
+                )
+              )}
+        </Slider>
+      </RecommendationRow>
+    );
+  }
+);
