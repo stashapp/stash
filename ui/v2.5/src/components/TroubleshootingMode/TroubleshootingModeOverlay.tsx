0bef736f9d389d44a740a6c215275bb54db3554e
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2026-01-27 11:41:22 +1100

    Change styling of troubleshooting mode to red-bordered overlay. Position message at bottom right.

--- ui/v2.5/src/components/TroubleshootingMode/TroubleshootingModeOverlay.tsx
+++ ui/v2.5/src/components/TroubleshootingMode/TroubleshootingModeOverlay.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Alert, Button } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import { FormattedMessage } from "react-intl";
 import { faBug } from "@fortawesome/free-solid-svg-icons";
 import { Icon } from "src/components/Shared/Icon";
@@ -14,22 +14,15 @@ export const TroubleshootingModeOverlay: React.FC = () => {
 
   return (
     <div className="troubleshooting-mode-overlay">
-      <Alert variant="danger" className="mb-0">
-        <div className="d-flex justify-content-between align-items-center">
-          <span>
-            <Icon icon={faBug} className="mr-2" />
-            <FormattedMessage id="config.ui.troubleshooting_mode.overlay_message" />
-          </span>
-          <Button
-            variant="danger"
-            size="sm"
-            onClick={disable}
-            disabled={isLoading}
-          >
-            <FormattedMessage id="config.ui.troubleshooting_mode.exit" />
-          </Button>
-        </div>
-      </Alert>
+      <div className="troubleshooting-mode-alert">
+        <span>
+          <Icon icon={faBug} className="mr-2" />
+          <FormattedMessage id="config.ui.troubleshooting_mode.overlay_message" />
+        </span>
+        <Button variant="link" onClick={disable} disabled={isLoading}>
+          <FormattedMessage id="config.ui.troubleshooting_mode.exit" />
+        </Button>
+      </div>
     </div>
   );
 };
