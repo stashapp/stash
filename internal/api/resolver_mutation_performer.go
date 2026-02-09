8d92efdccfda272cbc1451d671d1633f54c11756
Gykes <24581046+Gykes@users.noreply.github.com>
2026-02-09 00:44:37 -0800

    parse deprecated career_length input in mutation resolvers

--- internal/api/resolver_mutation_performer.go
+++ internal/api/resolver_mutation_performer.go
@@ -52,9 +52,14 @@ func (r *mutationResolver) PerformerCreate(ctx context.Context, input models.Per
 	newPerformer.FakeTits = translator.string(input.FakeTits)
 	newPerformer.PenisLength = input.PenisLength
 	newPerformer.Circumcised = input.Circumcised
-	newPerformer.CareerLength = translator.string(input.CareerLength)
 	newPerformer.CareerStart = input.CareerStart
 	newPerformer.CareerEnd = input.CareerEnd
+	// if career_start/career_end not provided, parse deprecated career_length
+	if newPerformer.CareerStart == nil && newPerformer.CareerEnd == nil && input.CareerLength != nil {
+		start, end, _ := performer.ParseCareerLength(*input.CareerLength)
+		newPerformer.CareerStart = start
+		newPerformer.CareerEnd = end
+	}
 	newPerformer.Tattoos = translator.string(input.Tattoos)
 	newPerformer.Piercings = translator.string(input.Piercings)
 	newPerformer.Favorite = translator.bool(input.Favorite)
@@ -263,9 +268,19 @@ func performerPartialFromInput(input models.PerformerUpdateInput, translator cha
 	updatedPerformer.FakeTits = translator.optionalString(input.FakeTits, "fake_tits")
 	updatedPerformer.PenisLength = translator.optionalFloat64(input.PenisLength, "penis_length")
 	updatedPerformer.Circumcised = translator.optionalString((*string)(input.Circumcised), "circumcised")
-	updatedPerformer.CareerLength = translator.optionalString(input.CareerLength, "career_length")
-	updatedPerformer.CareerStart = translator.optionalInt(input.CareerStart, "career_start")
-	updatedPerformer.CareerEnd = translator.optionalInt(input.CareerEnd, "career_end")
+	// prefer career_start/career_end over deprecated career_length
+	if translator.hasField("career_start") || translator.hasField("career_end") {
+		updatedPerformer.CareerStart = translator.optionalInt(input.CareerStart, "career_start")
+		updatedPerformer.CareerEnd = translator.optionalInt(input.CareerEnd, "career_end")
+	} else if translator.hasField("career_length") && input.CareerLength != nil {
+		start, end, _ := performer.ParseCareerLength(*input.CareerLength)
+		if start != nil {
+			updatedPerformer.CareerStart = models.NewOptionalInt(*start)
+		}
+		if end != nil {
+			updatedPerformer.CareerEnd = models.NewOptionalInt(*end)
+		}
+	}
 	updatedPerformer.Tattoos = translator.optionalString(input.Tattoos, "tattoos")
 	updatedPerformer.Piercings = translator.optionalString(input.Piercings, "piercings")
 	updatedPerformer.Favorite = translator.optionalBool(input.Favorite, "favorite")
@@ -421,9 +436,19 @@ func (r *mutationResolver) BulkPerformerUpdate(ctx context.Context, input BulkPe
 	updatedPerformer.FakeTits = translator.optionalString(input.FakeTits, "fake_tits")
 	updatedPerformer.PenisLength = translator.optionalFloat64(input.PenisLength, "penis_length")
 	updatedPerformer.Circumcised = translator.optionalString((*string)(input.Circumcised), "circumcised")
-	updatedPerformer.CareerLength = translator.optionalString(input.CareerLength, "career_length")
-	updatedPerformer.CareerStart = translator.optionalInt(input.CareerStart, "career_start")
-	updatedPerformer.CareerEnd = translator.optionalInt(input.CareerEnd, "career_end")
+	// prefer career_start/career_end over deprecated career_length
+	if translator.hasField("career_start") || translator.hasField("career_end") {
+		updatedPerformer.CareerStart = translator.optionalInt(input.CareerStart, "career_start")
+		updatedPerformer.CareerEnd = translator.optionalInt(input.CareerEnd, "career_end")
+	} else if translator.hasField("career_length") && input.CareerLength != nil {
+		start, end, _ := performer.ParseCareerLength(*input.CareerLength)
+		if start != nil {
+			updatedPerformer.CareerStart = models.NewOptionalInt(*start)
+		}
+		if end != nil {
+			updatedPerformer.CareerEnd = models.NewOptionalInt(*end)
+		}
+	}
 	updatedPerformer.Tattoos = translator.optionalString(input.Tattoos, "tattoos")
 	updatedPerformer.Piercings = translator.optionalString(input.Piercings, "piercings")
 
