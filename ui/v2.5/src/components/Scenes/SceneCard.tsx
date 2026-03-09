3fca6e85cf583bc6a09115f786064b098ff8bb59
smith113-p <205463041+smith113-p@users.noreply.github.com>
2026-03-09 14:30:42 -0400

    Show scene resolution and duration in tagger
    A scene's duration and resolution is often useful to ensure you have
    found the right scene. This PR adds the same resolution/duration
    overlay from the grid view to the tagger view.
    
    Bug: #4482

--- ui/v2.5/src/components/Scenes/SceneCard.tsx
+++ ui/v2.5/src/components/Scenes/SceneCard.tsx
@@ -352,6 +352,29 @@ const SceneCardOverlays = PatchComponent(
   }
 );
 
+interface ISceneSpecsOverlay {
+  scene: GQL.SlimSceneDataFragment;
+}
+
+export const SceneSpecsOverlay: React.FC<ISceneSpecsOverlay> = ({ scene }) => {
+  if (!scene.files.length) return null;
+  let file = scene.files[0];
+  return (
+    <div className="scene-specs-overlay">
+      <span className="overlay-filesize extra-scene-info">
+        <FileSize size={file.size} />
+      </span>
+      <span className="overlay-resolution">
+        {" "}
+        {TextUtils.resolution(file.width, file.height)}
+      </span>
+      <span className="overlay-duration">
+        {TextUtils.secondsToTimestamp(file.duration)}
+      </span>
+    </div>
+  );
+};
+
 const SceneCardImage = PatchComponent(
   "SceneCard.Image",
   (props: ISceneCardProps) => {
@@ -363,35 +386,6 @@ const SceneCardImage = PatchComponent(
       () => (props.scene.files.length > 0 ? props.scene.files[0] : undefined),
       [props.scene]
     );
-
-    function maybeRenderSceneSpecsOverlay() {
-      return (
-        <div className="scene-specs-overlay">
-          {file?.size !== undefined ? (
-            <span className="overlay-filesize extra-scene-info">
-              <FileSize size={file.size} />
-            </span>
-          ) : (
-            ""
-          )}
-          {file?.width && file?.height ? (
-            <span className="overlay-resolution">
-              {" "}
-              {TextUtils.resolution(file?.width, file?.height)}
-            </span>
-          ) : (
-            ""
-          )}
-          {(file?.duration ?? 0) >= 1 ? (
-            <span className="overlay-duration">
-              {TextUtils.secondsToTimestamp(file?.duration ?? 0)}
-            </span>
-          ) : (
-            ""
-          )}
-        </div>
-      );
-    }
 
     function maybeRenderInteractiveSpeedOverlay() {
       return (
@@ -432,7 +426,7 @@ const SceneCardImage = PatchComponent(
           disabled={props.selecting}
         />
         <RatingBanner rating={props.scene.rating100} />
-        {maybeRenderSceneSpecsOverlay()}
+        <SceneSpecsOverlay scene={props.scene} />
         {maybeRenderInteractiveSpeedOverlay()}
       </>
     );
