78f0266861c88967dce1be8d0f1c1d1767a5be6e
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2026-03-15 16:03:13 +1100

    Make compatible with other os

--- pkg/file/error_nonwindows.go
+++ pkg/file/error_nonwindows.go
@@ -1,12 +1,9 @@
+//go:build linux || darwin || !windows
+// +build linux darwin !windows
+
 package file
-
-import (
-	"errors"
-
-	"golang.org/x/sys/windows"
-)
 
 // IsBadNetworkError checks if the error is a "bad network name" error, which can occur when a network share is disconnected.
 func IsBadNetworkError(err error) bool {
-	return errors.Is(err, windows.ERROR_BAD_NETPATH)
+	return false
 }
---
+++ pkg/file/error_windows.go
@@ -0,0 +1,15 @@
+//go:build windows
+// +build windows
+
+package file
+
+import (
+	"errors"
+
+	"golang.org/x/sys/windows"
+)
+
+// IsBadNetworkError checks if the error is a "bad network name" error, which can occur when a network share is disconnected.
+func IsBadNetworkError(err error) bool {
+	return errors.Is(err, windows.ERROR_BAD_NETPATH)
+}
