682737c3ee7b5c5b3b7f865babb9794d18e25ed3
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2026-03-16 11:08:51 +1100

    Simplify - treat any error as missing folder

--- pkg/file/error_nonwindows.go
+++
@@ -1,9 +0,0 @@
-//go:build linux || darwin || !windows
-// +build linux darwin !windows
-
-package file
-
-// IsBadNetworkError checks if the error is a "bad network name" error, which can occur when a network share is disconnected.
-func IsBadNetworkError(err error) bool {
-	return false
-}
--- pkg/file/folder_rename_detect.go
+++ pkg/file/folder_rename_detect.go
@@ -2,7 +2,6 @@ package file
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"io/fs"
 
@@ -161,10 +160,7 @@ func (s *Scanner) detectFolderMove(ctx context.Context, file ScannedFile) (*mode
 					continue
 				}
 
-				// treat bad network path error as missing folder
-				if !errors.Is(err, fs.ErrNotExist) && !IsBadNetworkError(err) {
-					return fmt.Errorf("checking for parent folder %q: %w", pf.Path, err)
-				}
+				// treat any error as missing folder
 
 				// parent folder is missing, possible candidate
 				// count the total number of files in the existing folder
