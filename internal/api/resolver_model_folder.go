750697c7ecdafccb8ad9cd6c72a8d56a030260fd
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2026-03-24 12:43:06 +1100

    Add short cuts when only getting zip/folder ids

--- internal/api/resolver_model_folder.go
+++ internal/api/resolver_model_folder.go
@@ -17,13 +17,29 @@ func (r *folderResolver) ParentFolder(ctx context.Context, obj *models.Folder) (
 		return nil, nil
 	}
 
+	if r.idOnly(ctx) {
+		return &models.Folder{ID: *obj.ParentFolderID}, nil
+	}
+
 	return loaders.From(ctx).FolderByID.Load(*obj.ParentFolderID)
+}
+
+func foldersFromIDs(ids []models.FolderID) []*models.Folder {
+	ret := make([]*models.Folder, len(ids))
+	for i, id := range ids {
+		ret[i] = &models.Folder{ID: id}
+	}
+	return ret
 }
 
 func (r *folderResolver) ParentFolders(ctx context.Context, obj *models.Folder) ([]*models.Folder, error) {
 	ids, err := loaders.From(ctx).FolderParentFolderIDs.Load(obj.ID)
 	if err != nil {
 		return nil, err
+	}
+
+	if r.idOnly(ctx) {
+		return foldersFromIDs(ids), nil
 	}
 
 	var errs []error
@@ -37,11 +53,26 @@ func (r *folderResolver) SubFolders(ctx context.Context, obj *models.Folder) ([]
 		return nil, err
 	}
 
+	if r.idOnly(ctx) {
+		return foldersFromIDs(ids), nil
+	}
+
 	var errs []error
 	ret, errs := loaders.From(ctx).FolderByID.LoadAll(ids)
 	return ret, firstError(errs)
 }
 
 func (r *folderResolver) ZipFile(ctx context.Context, obj *models.Folder) (*BasicFile, error) {
+	// shortcut for id only queries
+	if r.idOnly(ctx) {
+		if obj.ZipFileID == nil {
+			return nil, nil
+		}
+
+		return &BasicFile{
+			BaseFile: &models.BaseFile{ID: *obj.ZipFileID},
+		}, nil
+	}
+
 	return zipFileResolver(ctx, obj.ZipFileID)
 }
