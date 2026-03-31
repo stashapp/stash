8559b6de773ee59d6733aa14ec609555370b59fe
feederbox826 <feederbox826@users.noreply.github.com>
2026-03-31 01:38:05 -0400

    bump ua-parser-js

--- ui/v2.5/src/utils/apple.ts
+++ ui/v2.5/src/utils/apple.ts
@@ -1,4 +1,4 @@
-import UAParser from "ua-parser-js";
+import { UAParser } from 'ua-parser-js';
 
 export function isPlatformUniquelyRenderedByApple() {
   // OS name on iPads show up as iOS or Max OS depending on the browser.
