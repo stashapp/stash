cf87f50fe0bfa6fe07fa1b9ff898ae2603dfb4b5
feederbox826 <feederbox826@users.noreply.github.com>
2026-03-31 02:05:23 -0400

    bump polyfills

--- ui/v2.5/src/polyfills.ts
+++ ui/v2.5/src/polyfills.ts
@@ -1,25 +1,26 @@
-import { shouldPolyfill as shouldPolyfillCanonicalLocales } from "@formatjs/intl-getcanonicallocales/should-polyfill";
-import { shouldPolyfill as shouldPolyfillLocale } from "@formatjs/intl-locale/should-polyfill";
-import { shouldPolyfill as shouldPolyfillNumberformat } from "@formatjs/intl-numberformat/should-polyfill";
-import { shouldPolyfill as shouldPolyfillPluralRules } from "@formatjs/intl-pluralrules/should-polyfill";
+/* eslint-disable import/extensions */
+import { shouldPolyfill as shouldPolyfillCanonicalLocales } from "@formatjs/intl-getcanonicallocales/should-polyfill.js";
+import { shouldPolyfill as shouldPolyfillLocale } from "@formatjs/intl-locale/should-polyfill.js";
+import { shouldPolyfill as shouldPolyfillNumberformat } from "@formatjs/intl-numberformat/should-polyfill.js";
+import { shouldPolyfill as shouldPolyfillPluralRules } from "@formatjs/intl-pluralrules/should-polyfill.js";
 
 // needed for older safari versions
 import "event-target-polyfill";
 
 async function checkPolyfills() {
   if (shouldPolyfillCanonicalLocales()) {
-    await import("@formatjs/intl-getcanonicallocales/polyfill");
+    await import("@formatjs/intl-getcanonicallocales/polyfill.js");
   }
   if (shouldPolyfillLocale()) {
-    await import("@formatjs/intl-locale/polyfill");
+    await import("@formatjs/intl-locale/polyfill.js");
   }
   if (shouldPolyfillNumberformat()) {
-    await import("@formatjs/intl-numberformat/polyfill");
+    await import("@formatjs/intl-numberformat/polyfill.js");
     await import("@formatjs/intl-numberformat/locale-data/en");
     await import("@formatjs/intl-numberformat/locale-data/en-GB");
   }
   if (shouldPolyfillPluralRules()) {
-    await import("@formatjs/intl-pluralrules/polyfill");
+    await import("@formatjs/intl-pluralrules/polyfill.js");
     await import("@formatjs/intl-pluralrules/locale-data/en");
   }
 }
