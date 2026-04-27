a0e3ab22f9ad2718c3715567ed3f068603e4aa65
Bob <241886672+bob12224@users.noreply.github.com>
2026-04-26 20:43:56 -0700

    Removing unused functions

--- internal/api/resolver_query_find_audio.go
+++ internal/api/resolver_query_find_audio.go
@@ -9,7 +9,6 @@ import (
 
 	"github.com/99designs/gqlgen/graphql"
 
-	"github.com/stashapp/stash/pkg/audio"
 	"github.com/stashapp/stash/pkg/models"
 )
 
@@ -36,39 +35,6 @@ func (r *queryResolver) FindAudio(ctx context.Context, id *string, checksum *str
 		}
 
 		return err
-	}); err != nil {
-		return nil, err
-	}
-
-	return audio, nil
-}
-
-func (r *queryResolver) FindAudioByHash(ctx context.Context, input AudioHashInput) (*models.Audio, error) {
-	var audio *models.Audio
-
-	if err := r.withReadTxn(ctx, func(ctx context.Context) error {
-		qb := r.repository.Audio
-		if input.Checksum != nil {
-			audios, err := qb.FindByChecksum(ctx, *input.Checksum)
-			if err != nil {
-				return err
-			}
-			if len(audios) > 0 {
-				audio = audios[0]
-			}
-		}
-
-		if audio == nil && input.Oshash != nil {
-			audios, err := qb.FindByOSHash(ctx, *input.Oshash)
-			if err != nil {
-				return err
-			}
-			if len(audios) > 0 {
-				audio = audios[0]
-			}
-		}
-
-		return nil
 	}); err != nil {
 		return nil, err
 	}
@@ -143,96 +109,6 @@ func (r *queryResolver) FindAudios(
 		}
 
 		return nil
-	}); err != nil {
-		return nil, err
-	}
-
-	return ret, nil
-}
-
-func (r *queryResolver) FindAudiosByPathRegex(ctx context.Context, filter *models.FindFilterType) (ret *FindAudiosResultType, err error) {
-	if err := r.withReadTxn(ctx, func(ctx context.Context) error {
-
-		audioFilter := &models.AudioFilterType{}
-
-		if filter != nil && filter.Q != nil {
-			audioFilter.Path = &models.StringCriterionInput{
-				Modifier: models.CriterionModifierMatchesRegex,
-				Value:    "(?i)" + *filter.Q,
-			}
-		}
-
-		// make a copy of the filter if provided, nilling out Q
-		var queryFilter *models.FindFilterType
-		if filter != nil {
-			f := *filter
-			queryFilter = &f
-			queryFilter.Q = nil
-		}
-
-		fields := graphql.CollectAllFields(ctx)
-
-		result, err := r.repository.Audio.Query(ctx, models.AudioQueryOptions{
-			QueryOptions: models.QueryOptions{
-				FindFilter: queryFilter,
-				Count:      slices.Contains(fields, "count"),
-			},
-			AudioFilter:   audioFilter,
-			TotalDuration: slices.Contains(fields, "duration"),
-			TotalSize:     slices.Contains(fields, "filesize"),
-		})
-		if err != nil {
-			return err
-		}
-
-		audios, err := result.Resolve(ctx)
-		if err != nil {
-			return err
-		}
-
-		ret = &FindAudiosResultType{
-			Count:    result.Count,
-			Audios:   audios,
-			Duration: result.TotalDuration,
-			Filesize: result.TotalSize,
-		}
-
-		return nil
-	}); err != nil {
-		return nil, err
-	}
-
-	return ret, nil
-}
-
-func (r *queryResolver) ParseAudioFilenames(ctx context.Context, filter *models.FindFilterType, config models.AudioParserInput) (ret *AudioParserResultType, err error) {
-	repo := audio.NewFilenameParserRepository(r.repository)
-	parser := audio.NewFilenameParser(filter, config, repo)
-
-	if err := r.withReadTxn(ctx, func(ctx context.Context) error {
-		result, count, err := parser.Parse(ctx)
-
-		if err != nil {
-			return err
-		}
-
-		ret = &AudioParserResultType{
-			Count:   count,
-			Results: result,
-		}
-
-		return nil
-	}); err != nil {
-		return nil, err
-	}
-
-	return ret, nil
-}
-
-func (r *queryResolver) AllAudios(ctx context.Context) (ret []*models.Audio, err error) {
-	if err := r.withReadTxn(ctx, func(ctx context.Context) error {
-		ret, err = r.repository.Audio.All(ctx)
-		return err
 	}); err != nil {
 		return nil, err
 	}
