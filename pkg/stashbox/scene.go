db4eabea8148f002783c43792931137c7ec31375
Gykes <24581046+Gykes@users.noreply.github.com>
2026-05-03 15:32:28 -0700

    New: Allow Description and Alias on Tag Creation in Scene Tagger (#6872)

--- pkg/stashbox/scene.go
+++ pkg/stashbox/scene.go
@@ -204,11 +204,7 @@ func (c Client) sceneFragmentToScrapedScene(ctx context.Context, s *graphql.Scen
 	}
 
 	for _, t := range s.Tags {
-		st := &models.ScrapedTag{
-			Name:         t.Name,
-			RemoteSiteID: &t.ID,
-		}
-		ss.Tags = append(ss.Tags, st)
+		ss.Tags = append(ss.Tags, tagFragmentToScrapedTag(*t))
 	}
 
 	return ss, nil
