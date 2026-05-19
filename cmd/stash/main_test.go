04a9b78cb569d501c285234a738cbd1aadcec9f0
slick-daddy <gokalpselamet98@hotmail.com>
2026-05-20 00:30:38 +0300

    test: replace empty TestStub with real tests for exitError, recoverPanic, initLogTemp

--- cmd/stash/main_test.go
+++ cmd/stash/main_test.go
@@ -1,7 +1,64 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
 
-func TestStub(t *testing.T) {
+	"github.com/stashapp/stash/internal/log"
+	"github.com/stashapp/stash/pkg/logger"
+)
 
+func TestExitError(t *testing.T) {
+	origCode := exitCode
+	origLogger := logger.Logger
+	defer func() {
+		exitCode = origCode
+		logger.Logger = origLogger
+	}()
+
+	l := log.NewLogger()
+	l.Init("", true, "Error", 0)
+	logger.Logger = l
+
+	exitCode = 0
+	exitError(os.ErrInvalid)
+	if exitCode != 1 {
+		t.Errorf("exitError should set exitCode to 1, got %d", exitCode)
+	}
+}
+
+func TestRecoverPanic(t *testing.T) {
+	origCode := exitCode
+	origLogger := logger.Logger
+	defer func() {
+		exitCode = origCode
+		logger.Logger = origLogger
+	}()
+
+	l := log.NewLogger()
+	l.Init("", true, "Error", 0)
+	logger.Logger = l
+
+	exitCode = 0
+	func() {
+		defer recoverPanic()
+		panic("test panic")
+	}()
+	if exitCode != 1 {
+		t.Errorf("recoverPanic should set exitCode to 1 after a panic, got %d", exitCode)
+	}
+}
+
+func TestInitLogTemp(t *testing.T) {
+	origLogger := logger.Logger
+	defer func() { logger.Logger = origLogger }()
+
+	logger.Logger = nil
+	l := initLogTemp()
+	if l == nil {
+		t.Fatal("initLogTemp should return a non-nil logger")
+	}
+	if logger.Logger == nil {
+		t.Error("initLogTemp should set the global logger.Logger")
+	}
 }
