a851cc9c08d892f9fbedc6c1586f288248371d9a
ghostmountain3 <jermhernan359@gmail.com>
2026-05-19 22:22:00 -0700

    Run Docker images as non-root

--- docker/ci/x86_64/docker_push.sh
+++ docker/ci/x86_64/docker_push.sh
@@ -10,5 +10,7 @@ done
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 # must build the image from dist directory
+mkdir -p dist/scripts
+cp scripts/docker-entrypoint.sh dist/scripts/docker-entrypoint.sh
+chmod 555 dist/scripts/docker-entrypoint.sh
 docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 --push $DOCKER_TAGS -f docker/ci/x86_64/Dockerfile dist/
-
