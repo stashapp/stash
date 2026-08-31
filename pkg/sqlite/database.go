76e6a6ab16a0a19eedf055f67ba5f95dc8678540
smith113-p <205463041+smith113-p@users.noreply.github.com>
2026-08-31 13:06:28 -0400

    add comment

--- pkg/sqlite/database.go
+++ pkg/sqlite/database.go
@@ -236,6 +236,7 @@ func (db *Database) Close() error {
 
 func (db *Database) open(disableForeignKeys bool, writable bool) (*sqlx.DB, error) {
 	// https://github.com/mattn/go-sqlite3
+	// 5s timeout is the default.
 	url := "file:" + db.dbPath + "?_journal=WAL&_sync=NORMAL&_busy_timeout=5000"
 	if !disableForeignKeys {
 		url += "&_fk=true"
